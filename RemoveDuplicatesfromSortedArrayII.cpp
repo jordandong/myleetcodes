f4822ec98584ee5f1f4a9eaabf55e9237045c603
jordandong <jordandong@gmail.com>
2013-12-11 13:21:19 -0800

    20131211

--- RemoveDuplicatesfromSortedArrayII.cpp
+++ RemoveDuplicatesfromSortedArrayII.cpp
@@ -12,6 +12,31 @@
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
+        unordered_map<int, int> mp;
+        int i=0;
+        int j=0;
+        int sum = 0;
+        while(j<n){
+            if(mp.find(A[j])==mp.end()){
+                mp[A[j]]=1;
+                A[i++]=A[j++];
+                sum++;
+            }else if(mp[A[j]]>=2){
+                j++;
+            }else{
+                mp[A[j]]++;
+                A[i++]=A[j++];
+                sum++;
+            }
+        }
+        return sum;
+    }
+};
+
+
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         if(n<3)
