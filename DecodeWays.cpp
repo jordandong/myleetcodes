ae042ed1b69b40965750eb2e9b82832d62d676e0
jordandong <jordandong@gmail.com>
2013-12-11 14:45:27 -0800

    20131211v5

--- DecodeWays.cpp
+++ DecodeWays.cpp
@@ -16,6 +16,32 @@
 // The number of ways decoding "12" is 2.
 //============================================================================
 
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        if(!n)
+            return 0;
+        if(s[0]=='0')
+            return 0;
+            
+        int dp[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=1; i<n; i++){
+            if(s[i]!='0')
+                dp[i+1] = dp[i];
+            else
+                dp[i+1] = 0;
+        
+            if(s[i-1]=='1' || (s[i-1]=='2' && s[i]<'7'))
+                dp[i+1]+=dp[i-1];
+        }
+        return dp[n];
+    }
+};
+
+
 #include <iostream>
 using namespace std;
 
