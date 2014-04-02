257e77cd5779548cb3ab02200370c366e06bc7f1
jordandong <jordandong@gmail.com>
2014-04-01 23:54:16 -0700

    20140402

--- ImplementStrStr.cpp
+++ ImplementStrStr.cpp
@@ -5,6 +5,59 @@
 // or null if needle is not part of haystack.
 //============================================================================
 
+class Solution {
+public:
+    char *strStr(char *haystack, char *needle) {
+        if (!haystack||!needle)
+            return haystack;
+        int l_h = strlen(haystack);
+        int l_n = strlen(needle);
+        if(l_n == 0)
+            return haystack;
+        if(l_h == 0)
+            return NULL;
+
+        int pos = KMP(haystack, needle);
+        return pos==-1?NULL:haystack+pos;
+    }
+    
+    void cal_next(vector<int> &next, char* needle){
+        int l_n = strlen(needle);
+        int k = -1;
+        int j = 0;
+        next[0] = -1;
+        while(j < l_n - 1){
+            if(k==-1 || *(needle+k) == *(needle+j)){
+                next[++j] = ++k;
+            }else{
+                k = next[k];
+            }
+        }
+    }
+    
+    int KMP(char *haystack, char *needle){
+        int l_h = strlen(haystack);
+        int l_n = strlen(needle);
+        vector<int> next(l_n, 0);
+        cal_next(next, needle);
+        int i_h = 0;
+        int i_n = 0;
+        while(i_h<l_h && i_n<l_n){
+            if(i_n == -1 || *(needle+i_n) == *(haystack+i_h)){
+                i_n++;
+                i_h++;
+            }else{
+                i_n = next[i_n];
+            }
+        }
+        return i_n==l_n?i_h-i_n:-1;
+    }
+};
+
+
+
+
+
 #include <cstring>
 
 class Solution {
