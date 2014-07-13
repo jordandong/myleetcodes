bf7904e7dda241220f3a1317915961d04be95c7d
jordandong <jordandong@gmail.com>
2014-07-12 20:10:25 -0700

    20140712v2

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -6,6 +6,25 @@
 // The order of elements can be changed. It doesn't matter what you leave
 // beyond the new length.
 //============================================================================
+
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        int i = 0;
+        int j = n-1;
+        while(i<=j){
+            if(A[i] == elem){
+                swap(A[i], A[j--]);
+            }else{
+                i++;
+            }
+        }
+        return i;
+    }
+};
+
+
+
 class Solution {
 public:
     int removeElement(int A[], int n, int elem) {
