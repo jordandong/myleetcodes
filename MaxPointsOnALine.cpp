956b258497a60e684b7bb05ec30292e42cdd1cd9
jordandong <jordandong@gmail.com>
2014-07-23 12:45:52 -0700

    20140723v1

--- MaxPointsOnALine.cpp
+++ MaxPointsOnALine.cpp
@@ -11,6 +11,41 @@ Given n points on a 2D plane, find the maximum number of points that lie on the 
  *     Point(int a, int b) : x(a), y(b) {}
  * };
  */
+ 
+//O(n^2) slope version
+class Solution {
+public:
+    int maxPoints(vector<Point> &points) {
+	    unordered_map<float, int> statistic;
+	
+	    int maxNum = 0;
+	    for (int i = 0; i< points.size(); i++){
+		    statistic.clear();
+		    statistic[INT_MIN] = 0; // for processing duplicate point
+		    int duplicate = 1;
+		    for (int j = 0; j<points.size(); j++){
+			if (j == i)
+				continue;
+			if (points[j].x == points[i].x && points[j].y == points[i].y){// count duplicate
+				duplicate++;
+				continue;
+		    	}
+		    	float key = (points[j].x - points[i].x) == 0 ?
+			    		INT_MAX : // key for vertical line
+				    	(float) (points[j].y - points[i].y) / (points[j].x - points[i].x);
+		    	statistic[key]++;
+		    }
+		    for (unordered_map<float, int>::iterator it = statistic.begin(); it != statistic.end(); ++it){
+			    if (it->second + duplicate >maxNum){
+				    maxNum = it->second + duplicate;
+		    	}
+		    }
+	    } 
+	    return maxNum;
+    }
+};
+ 
+ 
 
 //O(n^2), hash line
 /*hash func can be used eariler, then use the hash value as Key
