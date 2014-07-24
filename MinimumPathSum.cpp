657742dd70df0a02c3165b20061413ca4545141d
jordandong <jordandong@gmail.com>
2014-07-24 13:40:18 -0700

    20140724v2

--- MinimumPathSum.cpp
+++ MinimumPathSum.cpp
@@ -5,11 +5,30 @@
 // Note: You can only move either down or right at any point in time.
 //============================================================================
 
-#include <vector>
-#include <queue>
-#include <climits>
-#include <iostream>
-using namespace std;
+//optimize the space use
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        int m = grid.size();
+        if(m==0)
+            return 0;
+        int n = grid[0].size();
+        if(n==0)
+            return 0;
+            
+        vector<int> dp(n, INT_MAX/2);
+        dp[0]=0;
+        for(int i=0; i<m; i++){
+            dp[0]+=grid[i][0];
+            for(int j=1; j<n; j++){
+                dp[j]=min(dp[j-1], dp[j]) + grid[i][j];
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
+
 
 class Solution {
 public:
@@ -37,10 +56,6 @@ public:
         return dp[M-1][N-1];
     }
 };
-
-int main() {
-    return 0;
-}
 
 
 class Solution {
