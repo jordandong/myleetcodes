0e0d45972cb16cf6677097091217e2211b634567
jordandong <jordandong@gmail.com>
2014-07-24 12:45:09 -0700

    20140724

--- UniquePathsII.cpp
+++ UniquePathsII.cpp
@@ -20,6 +20,31 @@
 // Note: m and n will be at most 100.
 //============================================================================
 
+//optimize space use
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {  
+        int m = obstacleGrid.size();  
+        if(m ==0)
+            return 0;  
+        int n = obstacleGrid[0].size();  
+        if(obstacleGrid[0][0] ==1)
+            return 0;  
+        vector<int> maxV(n,0);  
+        maxV[0] =1;  
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){  
+                if(obstacleGrid[i][j] ==1)  
+                    maxV[j]=0;  
+                else if(j >0)  
+                    maxV[j] = maxV[j-1]+maxV[j];  //dp[i][j] = dp[i][j-1] + dp[i-1][j]
+            }  
+        }  
+        return maxV[n-1];  
+    } 
+};
+
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
