36d07649caf481c55d9935e380ae40ae9185f9e5
jordandong <jordandong@gmail.com>
2014-07-27 19:41:40 -0700

    20140727v3

--- RotateImage.cpp
+++ RotateImage.cpp
@@ -8,6 +8,43 @@
 // Could you do this in-place?
 //============================================================================
 
+/* flip left to right or up and down, then filp based on diag or back diag
+ * 1 2 3     7 8 9    7 4 1
+ * 4 5 6  -> 4 5 6 -> 8 5 2   or 
+ * 7 8 9     1 2 3    9 6 3 
+
+ * 1 2 3     9 6 3    7 4 1
+ * 4 5 6  -> 8 5 2 -> 8 5 2  = clockwise
+ * 7 8 9     7 4 1    9 6 3 
+
+/* 1 2 3     3 2 1    3 6 9
+ * 4 5 6  -> 6 5 4 -> 2 5 8   or 
+ * 7 8 9     9 8 7    1 4 7 
+
+ * 1 2 3     9 6 3    3 6 9
+ * 4 5 6  -> 8 5 2 -> 2 5 8 = anti clockwise
+ * 7 8 9     7 6 1    1 4 7 
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        int start=0, end=m-1;
+        while(start<=end){// swap up and down based on the mid row
+            for(int j=0;j<m;j++)
+                swap(matrix[start][j], matrix[end][j]);
+            start++;
+            end--;
+        }
+        
+        for(int i=0; i<m;i++) //swap based on diag
+            for(int j=i;j<m;j++)
+                swap(matrix[i][j], matrix[j][i]);
+    }
+};
+
+
 class Solution {
 public:
     void rotate(vector<vector<int> > &matrix) {
