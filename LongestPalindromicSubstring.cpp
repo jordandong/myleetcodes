b28fd4cd374fe1fef6501e19b3ae07aab489777c
jordandong <jordandong@gmail.com>
2014-07-29 22:16:50 -0700

    20140729v5

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -4,92 +4,17 @@
 // longest palindromic substring.
 //============================================================================
 
-#include <iostream>
-using namespace std;
-
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        if (n == 0)
-			return "";
-        int maxi = 0;
-        int maxl = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                int start = i - j;
-                int end = i + 1;
-                while (start >= 0 && end < n && s[start] == s[end]){
-                    start--;
-					end++;
-				}
-                start++;
-				end--;
-                int len = end - start + 1;
-                if (len > maxl){
-					maxi = start;
-					maxl = len;
-				}
-            }
-        }
-        return s.substr(maxi, maxl);
-    }
-};
-
-int main() {
-    return 0;
-}
 
 class Solution {
 public:
     string longestPalindrome(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
-        
         int N = s.size();
         if(N==1)
             return s;
         
-        bool dp[N][N+1];
-        int m = 0;
-        int start;
-        for(int i =0; i<N;i++){
-            for(int k=0; k+i<=N; k++){
-                if(k==1||k==0)
-                    dp[i][k] = true;
-                else
-                    dp[i][k] = false;
-            }
-        }
-        
-        for(int k=2; k<=N;k++){
-            for(int i=0; i+k<=N; i++){
-                dp[i][k] = dp[i+1][k-2] && s[i]==s[i+k-1];
-                if(dp[i][k]){
-                    m = max(m, k);
-                    start=i;
-                }
-            }
-        }
-        
-        return s.substr(start, m);
-        
-    }
-};
-
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        
-        int N = s.size();
-        if(N==1)
-            return s;
-        
-        bool dp[N][N];
+        bool dp[N][N]; // dp[i][j] means start from i and end at j
         int m = 0;
         int start;
         for(int i =0; i<N;i++){
@@ -112,11 +37,75 @@ public:
                 }
             }
         }
-        
         return s.substr(start, m);
-        
     }
 };
+
+//brute force
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 0)
+            return "";
+        int maxi = 0;
+        int maxl = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                int start = i - j;
+                int end = i + 1;
+                while (start >= 0 && end < n && s[start] == s[end]){
+                    start--;
+                    end++;
+                }
+                start++;
+                end--;
+                int len = end - start + 1;
+                if (len > maxl){
+                    maxi = start;
+                    maxl = len;
+                }
+            }
+        }
+        return s.substr(maxi, maxl);
+    }
+};
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int N = s.size();
+        if(N==1)
+            return s;
+        
+        bool dp[N][N+1];
+        int m = 0;
+        int start;
+        for(int i =0; i<N;i++){
+            for(int k=0; k+i<=N; k++){
+                if(k==1||k==0)
+                    dp[i][k] = true;
+                else
+                    dp[i][k] = false;
+            }
+        }
+        
+        for(int k=2; k<=N;k++){ // dp[i][k] means start from i and length is k
+            for(int i=0; i+k<=N; i++){
+                dp[i][k] = dp[i+1][k-2] && s[i]==s[i+k-1];
+                if(dp[i][k]){
+                    m = max(m, k);
+                    start=i;
+                }
+            }
+        }
+        return s.substr(start, m);
+    }
+};
+
 
 
 //the below method is not true, since   abcdefgxxiiliigfedcba  v.s. abcdefgiiliixxgfedcba
@@ -135,18 +124,17 @@ public:
         
         for(int i=m-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-				if(s[i]==s[j])
-                	dp[j+1] = dp[j]+ (s[i]==s[j]);
-				else
-					dp[j+1] = 0;
+                if(s[i]==s[j])
+                    dp[j+1] = dp[j]+ (s[i]==s[j]);
+                else
+                    dp[j+1] = 0;
 
                 if(dp[j+1]>maxl){
-				    maxl=dp[j+1];
-				    maxj=j+1;
-			    }
+                    maxl=dp[j+1];
+                    maxj=j+1;
+                }
             }
         }
-        
         return s.substr(maxj-maxl,maxl);
     }
 };
