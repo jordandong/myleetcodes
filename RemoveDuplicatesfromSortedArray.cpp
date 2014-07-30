3e4e166c127336b1dfefd897c7c24347db325115
jordandong <jordandong@gmail.com>
2014-07-30 13:25:21 -0700

    20140730v11

--- RemoveDuplicatesfromSortedArray.cpp
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -15,6 +15,21 @@
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
+        int k=0, i=0;
+        while(i<n){
+            if(A[i]==A[k]){
+                i++;
+                continue;
+            }
+            A[++k]=A[i++];
+        }
+        return i==k?0:k+1;
+    }
+};
+
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         if(n<2)
@@ -36,18 +51,14 @@ class Solution {
 public:
     int removeDuplicates(int A[], int n) {
         if (n < 2)
-			return n;
+            return n;
         int i = 1, j = 1;
         while (i < n) {
             if (A[i-1] != A[i])
-				A[j++] = A[i++];
+                A[j++] = A[i++];
             else
-				i++;
+                i++;
         }
         return j;
     }
 };
-
-int main() {
-    return 0;
-}
