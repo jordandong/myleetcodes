023fc5ee7fcf48c3e32a7dd12c0ac9f36d155734
jordandong <jordandong@gmail.com>
2014-07-29 22:42:02 -0700

    20140729v6

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -9,61 +9,27 @@
 // is "()()", which has length = 4.
 //============================================================================
 
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int N=s.size();
-        if (N<2)
-			return 0;
-        int res=0;
-        int k=0, l=0;
-        for(int i=0; i<N; i++){
-            if(s[i] == '('){
-				k++;
-				l++;
-			}
-            else{
-				k--;
-				l++;
-			}
-            if(k == 0 && l > res)
-				res = l;
-            else if(k < 0){
-				k = 0;
-				l = 0;
-			}
+        stack<int> stk;
+        int size = s.length();
+        int maxL = 0;
+        for(int i=0; i<size; i++){
+            if(s[i]=='('){
+                stk.push(i);
+            }else{
+                if(!stk.empty()&&s[stk.top()]=='('){
+                    stk.pop();
+                    maxL=max(maxL, i-(stk.empty()?-1:stk.top()));
+                }else{
+                    stk.push(i);
+                }
+            }
         }
-		//cannot figure out like "(((((())"
-		//so need to start from the end again
-        k=0;
-		l=0;
-        for(int i=N-1; i>0; i--){
-        	if (s[i] == ')'){
-				k++;
-				l++;
-			}else{
-				k--;
-				l++;
-			}
-            if(k == 0 && l > res)
-				res = l;
-            else if (k < 0){
-				k = 0;
-				l = 0;
-			}
-        }
-        return res;
+        return maxL;
     }
 };
-
-int main() {
-	return 0;
-}
 
 class Solution {
 public:
@@ -82,11 +48,57 @@ public:
                     stk.pop();
                     nMax = max(p - (stk.empty() ? str-1 : stk.top()), nMax);
                 }
-                else stk.push(p);
+                else
+                    stk.push(p);
             }
             p++;
         }
         return nMax;
     }
-        
+};
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int N=s.size();
+        if (N<2)
+            return 0;
+        int res=0;
+        int k=0, l=0;
+        for(int i=0; i<N; i++){
+            if(s[i] == '('){
+                k++;
+                l++;
+            }else{
+                k--;
+                l++;
+            }
+            if(k == 0 && l > res)
+                res = l;
+            else if(k < 0){
+                k = 0;
+                l = 0;
+            }
+        }
+        //cannot figure out like "(((((())"
+        //so need to start from the end again
+        k=0;
+        l=0;
+        for(int i=N-1; i>0; i--){
+            if (s[i] == ')'){
+                k++;
+                l++;
+            }else{
+                k--;
+                l++;
+            }
+            if(k == 0 && l > res)
+                res = l;
+            else if (k < 0){
+                k = 0;
+                l = 0;
+            }
+        }
+        return res;
+    }
 };
