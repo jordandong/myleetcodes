f4642750635b58c27f500c28a34dab0c30ac70f3
jordandong <jordandong@gmail.com>
2014-07-30 00:00:12 -0700

    20140730v2

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -7,62 +7,6 @@
 //
 //============================================================================
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-/**
- * Definition for an interval.
- */
-struct Interval {
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
-};
-
-struct StartIncreasing {
-    bool operator() (const Interval &a, const Interval &b) {
-        return a.start < b.start;
-    }
-} start_increasing;
-
-class Solution {
-public:
-    vector<Interval> merge(vector<Interval> &intervals) {
-        if (intervals.size() < 2)
-			return intervals;
-        sort(intervals.begin(), intervals.end(), start_increasing);
-        vector<Interval> result;
-        Interval curInterval;
-        vector<Interval>::iterator it = intervals.begin();
-        while (it != intervals.end()) {
-            if (it == intervals.begin()) {
-                curInterval = *it;
-            }
-            else if ((*it).start <= curInterval.end && curInterval.start <= (*it).end) {
-                curInterval.start = (curInterval.start > (*it).start) ? (*it).start : curInterval.start;
-                curInterval.end = ((curInterval.end < (*it).end) ? (*it).end : curInterval.end);
-            }
-            else if ((*it).start > curInterval.end) {
-                result.push_back(curInterval);
-                curInterval = *it;
-            }
-            it++;
-        }
-        result.push_back(curInterval);
-        return result;
-    }
-};
-
-int main() {
-    return 0;
-}
-
-
-
-
 /**
  * Definition for an interval.
  * struct Interval {
@@ -74,7 +18,7 @@ int main() {
  */
  
 bool start_increasing(struct Interval a, struct Interval b) {
-        return a.start < b.start;
+    return a.start < b.start;
 }
 
 class Solution {
@@ -101,5 +45,40 @@ public:
         }
         res.push_back(curr);
         return res;
+    }
+};
+
+
+struct StartIncreasing {
+    bool operator() (const Interval &a, const Interval &b) {
+        return a.start < b.start;
+    }
+} start_increasing;
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval> &intervals) {
+        if (intervals.size() < 2)
+            return intervals;
+        sort(intervals.begin(), intervals.end(), start_increasing);
+        vector<Interval> result;
+        Interval curInterval;
+        vector<Interval>::iterator it = intervals.begin();
+        while (it != intervals.end()) {
+            if (it == intervals.begin()) {
+                curInterval = *it;
+            }
+            else if ((*it).start <= curInterval.end && curInterval.start <= (*it).end) {
+                curInterval.start = (curInterval.start > (*it).start) ? (*it).start : curInterval.start;
+                curInterval.end = ((curInterval.end < (*it).end) ? (*it).end : curInterval.end);
+            }
+            else if ((*it).start > curInterval.end) {
+                result.push_back(curInterval);
+                curInterval = *it;
+            }
+            it++;
+        }
+        result.push_back(curInterval);
+        return result;
     }
 };
