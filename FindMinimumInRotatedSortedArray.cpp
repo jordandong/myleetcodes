e545581ab165f08387da26166837de06fcd9df58
jordandong <jordandong@gmail.com>
2014-10-22 20:12:18 -0700

    20141022

--- FindMinimumInRotatedSortedArray.cpp
+++ FindMinimumInRotatedSortedArray.cpp
@@ -12,6 +12,23 @@ public:
         int hi = num.size()-1;
         int mid = -1;
         while(lo < hi){
+            mid = lo + (hi-lo)/2;
+            if(num[mid] >num[hi])
+                lo = mid + 1;
+            else if(num[mid] < num[hi])
+                hi = mid;
+        }
+        return num[lo];
+    }
+};
+ 
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        int lo = 0;
+        int hi = num.size()-1;
+        int mid = -1;
+        while(lo < hi){
             mid = lo + (hi-lo+1)/2;
             if(num[mid] >num[hi])
                 lo = mid + 1;
