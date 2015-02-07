accb87f61017311e5a364b66474594ddf4576539
jordandong <jordandong@gmail.com>
2015-02-06 22:20:43 -0800

    20150206_v3

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -1,49 +1,41 @@
-//============================================================================
-// Reverse Integer
-// Reverse digits of an integer.
-//
-// Example1: x = 123, return 321
-// Example2: x = -123, return -321
-//
-// Have you thought about this?
-// Here are some good questions to ask before coding. Bonus points for you if
-// you have already thought through this!
-//
-// If the integer's last digit is 0, what should the output be? ie, cases such
-// as 10, 100.
-//
-// Did you notice that the reversed integer might overflow? Assume the input
-// is a 32-bit integer, then the reverse of 1000000003 overflows. How should
-// you handle such cases?
-//
-// Throw an exception? Good, but what if throwing an exception is not an
-// option? You would then have to re-design the function (ie, add an extra
-// parameter).
-//============================================================================
+/*
+Reverse digits of an integer.
 
-#include <cassert>
-#include <climits>
-using namespace std;
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+click to show spoilers.
+
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+Update (2014-11-10):
+Test cases had been added to test the overflow behavior.
+*/
 
 class Solution {
 public:
     int reverse(int x) {
-        int sign = 1;
-        if (x < 0){
-        	sign = -sign;
-			x = -x;
-		}
+        bool neg = false;
         long long res = 0;
-        while (x) {
-            res = res * 10 + x % 10;
-            x /= 10;
+        long long y = x;
+        if(y < 0){
+            neg = true;
+            y = -y;
         }
-        if (sign < 0)
-        	return -res;
-        return res;
+        
+        while(y){
+            res = 10*res + y%10;
+            y/=10;
+            if(res > INT_MAX)
+                return 0;
+        }
+        return neg ? -res:res;
     }
 };
-
-int main() {
-	return 0;
-}
