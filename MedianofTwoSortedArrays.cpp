9ac31cac5b17acc382f5700956571a43f0d93a84
jordandong <jordandong@gmail.com>
2015-02-06 22:25:10 -0800

    Update MedianofTwoSortedArrays.cpp

--- MedianofTwoSortedArrays.cpp
+++ MedianofTwoSortedArrays.cpp
@@ -1,8 +1,8 @@
-//============================================================================
-// There are two sorted arrays A and B of size m and n respectively. Find the
-// median of the two sorted arrays. The overall run time complexity should be
-// O(log (m+n)).
-//============================================================================
+/*
+There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Hide Tags Divide and Conquer Array Binary Search
+*/
 
 //T:log(k), k = m+n here
 class Solution {
