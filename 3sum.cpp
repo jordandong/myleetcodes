5f43c1d05f69be59849dae757c16da68e6b2516e
jordandong <jordandong@gmail.com>
2015-02-07 14:19:44 -0800

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,109 +1,62 @@
-//============================================================================
-// Given an array S of n integers, are there elements a, b, c in S such that
-// a + b + c = 0?
-// Find all unique triplets in the array which gives the sum of zero.
-//
-// Note:
-// Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
-// The solution set must not contain duplicate triplets.
-// For example, given array S = {-1 0 1 2 -1 -4},
-//
-// A solution set is:
-// (-1, 0, 1)
-// (-1, -1, 2)
-//============================================================================
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+Note:
+Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+The solution set must not contain duplicate triplets.
+    For example, given array S = {-1 0 1 2 -1 -4},
+
+    A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+Hide Tags Array Two Pointers
+*/
 
 class Solution {
 public:
     vector<vector<int> > threeSum(vector<int> &num) {
-        int N = num.size();
-        sort(num.begin(), num.end());
-        vector<vector<int> > result;
-        for (int k = 0; k < N-2; k++) {
-            int i = k+1;
-            int j = N-1;
-            while (i < j) {
-                int sum = num[i] + num[j] + num[k];
-                if (sum > 0)
-                		j--;
-                else if (sum < 0)
-                		i++;
-                else {
-                    vector<int> triplet;
-                    triplet.push_back(num[i]);
-                    triplet.push_back(num[j]);
-                    triplet.push_back(num[k]);
-                    sort(triplet.begin(), triplet.end());
-                    if (find(result.begin(), result.end(), triplet) == result.end()) {
-                        result.push_back(triplet);
-                    }
-                    i++;
-                    j--;
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+	vector<vector<int> > res;
+	int sz = num.size();
+	if(sz < 3)
+		return res;
+	
+	sort(num.begin(),num.end());
+	for(int i = 0; i < sz - 2; i++){
+            if(num[i] > 0)
+                break;
+            if(i > 0 && num[i] == num[i-1])  
+                continue;
+           
+            int s = i + 1;
+            int e = sz - 1;
+            while(s < e){
+                if(s > i + 1 && num[s] == num[s - 1]){  
+                    s++;  
+                    continue;  
                 }
-            }
-        }
-        return result;
-    }
-};
-
-int main() {
-    return 0;
-}
-
-
-class Solution {
-	public:
-		vector<vector<int> > threeSum(vector<int> &num) {
-			// Start typing your C/C++ solution below
-			// DO NOT write int main() function
-			vector<vector<int> > res;
-			if(num.size()<3)
-				return res;
-			int size = num.size();
-			sort(num.begin(),num.end());
-
-			for(int i=0;i<size-2;i++){
-				if(num[i]>0)
-					break;
-				if (i > 0 && num[i] == num[i-1])  
-					continue;
-
-				int begin =i+1;
-				int end = size-1;
-				while(begin < end){
-					int sum = num[i]+num[begin]+num[end]; 
-					if(sum==0){
-						if (num[begin] == num[begin-1] && begin > i+1){  
-							begin = begin + 1;  
-							continue;  
-						}
-						vector<int> tmp;
-						tmp.push_back(num[i]);
-						tmp.push_back(num[begin]);
-						tmp.push_back(num[end]);
-						res.push_back(tmp);
-						begin++;
-						end--;
-					}
-					else if (sum > 0)   {  
-						end = end - 1;  
-						if (num[end] == num[end+1]){  
-							end = end - 1;  
-						}  
-					}  
-					else  {  
-						begin = begin + 1;  
-						if (num[begin] == num[begin-1])  {  
-							begin = begin + 1;  
-						}  
-					} 
-				}
-			}
-			return res;
+                if(e < sz - 1 && num[e] == num[e + 1]){  
+                    e--;  
+		    continue;
+                }
+	
+		int sum = num[i] + num[s] + num[e]; 
+		if(sum == 0){
+		    vector<int> tmp;
+		    tmp.push_back(num[i]);
+		    tmp.push_back(num[s]);
+		    tmp.push_back(num[e]);
+		    res.push_back(tmp);
+		    s++;
+		    e--;
+		}else if(sum > 0){  
+		    e--;  
+		}else{
+		    s++;
 		}
+            }
+	}
+	return res;
+    }
 };
