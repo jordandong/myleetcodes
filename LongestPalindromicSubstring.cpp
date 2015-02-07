034d50180f0b5ff6dbf7fda300c3e02f3a576c9d
jordandong <jordandong@gmail.com>
2015-02-06 22:25:56 -0800

    Update LongestPalindromicSubstring.cpp

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -1,8 +1,8 @@
-//============================================================================i
-// Given a string S, find the longest palindromic substring in S. You may
-// assume that the maximum length of S is 1000, and there exists one unique
-// longest palindromic substring.
-//============================================================================
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+
+Hide Tags String
+*/
 
 class Solution {
 public:
