49e14a159c84227658c06e3450891bb2e425024a
jordandong <jordandong@gmail.com>
2015-02-06 22:23:53 -0800

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,11 +1,8 @@
-//============================================================================
-// Given a string, find the length of the longest substring without repeating
-// characters. For example,
-//
-// the longest substring without repeating letters for "abcabcbb" is "abc",
-// which the length is 3. For "bbbbb" the longest substring is "b", with the
-// length of 1.
-//============================================================================
+/*
+Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+Hide Tags Hash Table Two Pointers String
+*/
 
 //T:O(n), S:O(1)
 class Solution {
