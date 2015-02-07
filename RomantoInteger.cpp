eb3b73fc7ffbb0bacc18d3df2276b147739c25e4
jordandong <jordandong@gmail.com>
2015-02-07 13:47:57 -0800

    Update RomantoInteger.cpp

--- RomantoInteger.cpp
+++ RomantoInteger.cpp
@@ -1,13 +1,34 @@
-//============================================================================
-// Roman to Integer
-// Given a roman numeral, convert it to an integer.
-//
-// Input is guaranteed to be within the range from 1 to 3999.
-//============================================================================
+/*
+Given a roman numeral, convert it to an integer.
 
-#include <map>
-#include <string>
-using namespace std;
+Input is guaranteed to be within the range from 1 to 3999.
+
+Hide Tags Math String
+*/
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int mp[26];
+        mp['I' - 'A'] = 1;
+        mp['V' - 'A'] = 5;
+        mp['X' - 'A'] = 10;
+        mp['L' - 'A'] = 50;
+        mp['C' - 'A'] = 100;
+        mp['D' - 'A'] = 500;
+        mp['M' - 'A'] = 1000;
+        int res = 0;
+        int i = 0;
+        for(i = 0; i < s.length()-1; i++){
+            if(mp[s[i] - 'A'] < mp[s[i+1] - 'A'])
+                res -= mp[s[i] - 'A'];
+            else
+                res += mp[s[i] - 'A'];
+        }
+        res += mp[s[i] - 'A'];
+        return res;
+    }
+};
 
 class Solution {
 public:
@@ -33,7 +54,3 @@ public:
         return res;
     }
 };
-
-int main() {
-    return 0;
-}
