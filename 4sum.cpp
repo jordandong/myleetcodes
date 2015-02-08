ce5e917fed8d5a88cc9c27193f56ae1f31edbfaa
jordandong <jordandong@gmail.com>
2015-02-08 11:12:01 -0800

    Update 4sum.cpp

--- 4sum.cpp
+++ 4sum.cpp
@@ -1,43 +1,80 @@
-//============================================================================
-// 4Sum
-// Given an array S of n integers, are there elements a, b, c, and d in S such
-// that a + b + c + d = target?
-// Find all unique quadruplets in the array which gives the sum of target.
-// Note:
-// Elements in a quadruplet (a,b,c,d) must be in non-descending order.
-// (ie, a ≤ b ≤ c ≤ d)
-// The solution set must not contain duplicate quadruplets.
-// For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
-// A solution set is:
-// (-1,  0, 0, 1)
-// (-2, -1, 1, 2)
-// (-2,  0, 0, 2)
-//============================================================================
+/*
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
 
-#include <vector>
-#include <map>
-#include <iostream>
-#include <algorithm>
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
 
-using namespace std;
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+Hide Tags Array Hash Table Two Pointers
+*/
 
-struct TwoSum{
+//T: O(N^2)  S: O(N^2)
+class Solution {
 public:
-    TwoSum(int idx1, int idx2, int v) :
-        index1(idx1), index2(idx2), value(v) {}
+    vector<vector<int> > fourSum(vector<int> nums, int target) {
+        // write your code here
+        int sz = nums.size();
+        vector<vector<int> > res;
+        unordered_map<int, vector<pair<int, int> > > data;
+
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < sz; i++)
+            for(int j = i+1; j < sz; j++)
+                data[nums[i]+nums[j]].push_back(make_pair(i,j));
+                
+        for(int i = 0; i < sz - 3; i++){
+            if(i > 0 && nums[i]==nums[i-1])//reduce dup for the 1st element
+                continue;   
+            for(int j = i+1; j < sz - 2; j++){
+                if(j > i+1 && nums[j]==nums[j-1])//reduce dup for the 2nd element
+                    continue;   
+                int left = target - nums[i] - nums[j];
+                if(data.find(left) == data.end())
+                    continue;
+                vector<pair<int, int> > &v = data[left];
+                bool added = false;
+                for(int k = 0; k < v.size(); k++){
+                    if(v[k].first <= j)//reduce the dup across four elements
+                        continue;
+                    if(!added || nums[v[k].first] != res.back()[2]){ //reduce dup for the last two elements
+                        vector<int> sol;
+                        sol.push_back(nums[i]);
+                        sol.push_back(nums[j]);
+                        sol.push_back(nums[v[k].first]);
+                        sol.push_back(nums[v[k].second]);
+                        res.push_back(sol);
+                        added = true;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//T: O(N^2 * (log(N) + N))  S: O(N^2)
+struct TwoSum{
 public:
     int index1;
     int index2;
     int value;
+    TwoSum(int idx1, int idx2, int v) :
+        index1(idx1), index2(idx2), value(v) {}
+
 };
 
 struct increaing {
     bool operator() (TwoSum ts1, TwoSum ts2) {
         if (ts1.value == ts2.value){
             if (ts1.index1 == ts2.index1)
-            		return (ts1.index2 < ts2.index2);
+            	return (ts1.index2 < ts2.index2);
             else
-            		return (ts1.index1 < ts2.index1);
+                return (ts1.index1 < ts2.index1);
         }
         else
             return (ts1.value < ts2.value);
@@ -88,13 +125,13 @@ public:
 
             for (size_t j = begin; j <= end; j++) {
                 if (twoSums[j].index1 == twoSums[i].index1)
-                	continue;
+                    continue;
                 if (twoSums[j].index2 == twoSums[i].index1)
-                	continue;
+                    continue;
                 if (twoSums[j].index1 == twoSums[i].index2)
-                	continue;
+                    continue;
                 if (twoSums[j].index2 == twoSums[i].index2)
-                	continue;
+                    continue;
 
                 vector<int> tmp;
                 tmp.push_back(num[ twoSums[i].index1]);
@@ -111,62 +148,52 @@ public:
     };
 };
 
-int main() {
-    return 0;
-}
-
-
+//brute force, T: O(N^3)  S: O(1)
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         vector<vector<int> > res;
-
-	if(num.size()<4)
-		return res;
-
-	int size = num.size();
-	sort(num.begin(),num.end());
-
-	for(int j=0;j<size-3;j++){
-		if(j>0&&num[j]==num[j-1])
-			continue;
-		for(int i = j+1;i<size-2;i++){
-            		if(i>j+1&&num[i]==num[i-1])
-            			continue;                    
-                	int begin =i+1;
-                	int end = size-1;
-			while(begin < end){
-				int sum =num[j]+num[i]+num[begin]+num[end];
-				if(sum==target){
-                        	if (num[begin] == num[begin-1] && begin > i+1){  
-					begin++;  
-					continue;  
-				}
-				vector<int> tmp;
-                tmp.push_back(num[j]);
-				tmp.push_back(num[i]);
-				tmp.push_back(num[begin]);
-				tmp.push_back(num[end]);
-				res.push_back(tmp);
-                	begin++;
-                        	end--;
-				}
-				else if(sum > target){ 
-                		end--;
-                    while (num[end] == num[end+1]){  
-						end--;  
-					}  
-				}  
-				else{
-					begin++;
-					while(num[begin] == num[begin-1]){  
-						begin++;  
-					}  
-				}
-			}
+        if(num.size() < 4)
+            return res;
+            
+        int size = num.size();
+        sort(num.begin(),num.end());
+        for(int j = 0;j < size - 3; j++){
+            if(j > 0 && num[j] == num[j-1])
+                continue;
+            for(int i = j+1; i < size-2; i++){
+            	if(i > j+1 && num[i] == num[i-1])
+            	    continue;
+            	int begin =i+1;
+            	int end = size-1;
+            	while(begin < end){
+            	    if (num[begin] == num[begin-1] && begin > i+1){
+            	    	begin++;
+            	    	continue;
+            	    }
+            	    if (num[end] == num[end+1] && end < size-1){
+            	    	end--;
+            	    	continue;
+            	    }
+            	    int sum =num[j]+num[i]+num[begin]+num[end];
+            	    if(sum==target){
+            	    	vector<int> tmp;
+            	    	tmp.push_back(num[j]);
+            	    	tmp.push_back(num[i]);
+            	    	tmp.push_back(num[begin]);
+            	    	tmp.push_back(num[end]);
+            	    	res.push_back(tmp);
+            	    	begin++;
+            	    	end--;
+            	    }else if(sum > target){
+            	    	end--;
+            	    }else{
+            	    	begin++;
+            	    }
 		}
+            }
 	}
 	return res;
     }
