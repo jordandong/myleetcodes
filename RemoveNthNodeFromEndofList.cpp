ca19fb80d5f81b6a55b60d795726369553194023
jordandong <jordandong@gmail.com>
2015-02-08 11:22:50 -0800

    Update RemoveNthNodeFromEndofList.cpp

--- RemoveNthNodeFromEndofList.cpp
+++ RemoveNthNodeFromEndofList.cpp
@@ -1,19 +1,23 @@
-//============================================================================
-// Remove Nth Node From End of List
-// Given a linked list, remove the nth node from the end of list and return
-// its head.
-//
-// For example,
-//
-// Given linked list: 1->2->3->4->5, and n = 2.
-//
-// After removing the second node from the end, the linked list becomes
-// 1->2->3->5.
-// Note:
-// Given n will always be valid.
-// Try to do this in one pass.
-//============================================================================
+/*
+Given a linked list, remove the nth node from the end of list and return its head.
+For example,
+   Given linked list: 1->2->3->4->5, and n = 2.
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
 
+Hide Tags Linked List Two Pointers
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
@@ -21,8 +25,8 @@ public:
         new_head->next = head;
         ListNode *fast = new_head;
         ListNode *slow = new_head;
-        while(n>0){
-            fast=fast->next;
+        while(n > 0){
+            fast = fast->next;
             n--;
         }
         
@@ -30,37 +34,27 @@ public:
             fast=fast->next;
             slow=slow->next;
         }
-        slow->next = slow->next->next;
-        return new_head->next;
+        ListNode *tmp = slow->next;
+        slow->next = tmp->next;
+        delete tmp;
+        tmp = new_head->next;
+        delete new_head;
+        return tmp;
     }
-};
-
-
-
-#include <iostream>
-using namespace std;
-
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {};
 };
 
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
-        if (NULL == head)
-        	return head;
-        ListNode *fast = head;
-		ListNode *slow = head;
+    	if (NULL == head)
+    	    return head;
+    	ListNode *fast = head;
+    	ListNode *slow = head;
         int i = 0;
         while (fast != NULL && i < n){
-        	fast = fast->next;
-			i++;
-		}
+            fast = fast->next;
+            i++;
+        }
         if (fast == NULL) {
             head = head->next;
             delete slow;
@@ -76,7 +70,3 @@ public:
         return head;
     }
 };
-
-int main() {
-    return 0;
-}
