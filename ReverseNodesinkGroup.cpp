b49789630bf5cbca39bb3995af572d234a9b3588
jordandong <jordandong@gmail.com>
2015-02-08 20:49:33 -0800

    Update ReverseNodesinkGroup.cpp

--- ReverseNodesinkGroup.cpp
+++ ReverseNodesinkGroup.cpp
@@ -1,23 +1,21 @@
-//============================================================================
-// Reverse Nodes in k-Group
-// Given a linked list, reverse the nodes of a linked list k at a time and
-// return its modified list.
-//
-// If the number of nodes is not a multiple of k then left-out nodes in the
-// end should remain as it is.
-//
-// You may not alter the values in the nodes, only nodes itself may be
-// changed.
-//
-// Only constant memory is allowed.
-//
-// For example,
-// Given this linked list: 1->2->3->4->5
-//
-// For k = 2, you should return: 2->1->4->3->5
-//
-// For k = 3, you should return: 3->2->1->4->5
-//============================================================================
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+Hide Tags Linked List
+*/
 
 /**
  * Definition for singly-linked list.
@@ -36,28 +34,28 @@ public:
 
         int len = 0;
         int rounds = 0;
-        ListNode* cur=head;
+        ListNode* cur = head;
         while(cur){
             len++;
-            cur=cur->next;
+            cur = cur->next;
         }
         rounds = len/k;
-        if(rounds==0)
+        if(rounds == 0)
             return head;
         
         stack<ListNode*> stk;
-        ListNode *rtn=new ListNode(-1);
-        ListNode *rtn_tail=rtn;
-        cur=head;
-        while(rounds>0){
-            for(int i=1; i<=k; i++){
+        ListNode *rtn = new ListNode(-1);
+        ListNode *rtn_tail = rtn;
+        cur = head;
+        while(rounds > 0){
+            for(int i = 1; i <= k; i++){
                 stk.push(cur);
-                cur=cur->next;
+                cur = cur->next;
             }
             while(!stk.empty()){
                 rtn_tail->next = stk.top();
                 stk.pop();
-                rtn_tail=rtn_tail->next;
+                rtn_tail = rtn_tail->next;
             }
             rounds--;
         }
@@ -68,23 +66,21 @@ public:
     }
 };
 
-
 class Solution {
 public:
     ListNode *reverseKGroup(ListNode *head, int k) {
         if(k == 0 || k == 1)
-        	return head;
+            return head;
         	
         ListNode *curNode = head;
         int length = 0;
         while (curNode != NULL){
-        	curNode = curNode->next;
-			length++;
-		}
+            curNode = curNode->next;
+            length++;
+        }
         int multi = length/k;
-        
         if(multi == 0)
-        	return head;
+            return head;
 
         ListNode *preTail = NULL, *curHead = NULL, *curTail = NULL;
         ListNode *preNode, *nextNode;
@@ -98,9 +94,9 @@ public:
                     preNode = curNode;
                 }
                 if(i == 0)
-                	curTail = curNode;
+                    curTail = curNode;
                 if(i == (k - 1))
-                	curHead = curNode;
+                    curHead = curNode;
                 curNode = nextNode;
             }
             if(preTail == NULL)
