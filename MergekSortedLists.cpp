0fe7f29971e02a5c500257f881b9f13a9b80cb03
jordandong <jordandong@gmail.com>
2015-02-08 20:30:34 -0800

    Update MergekSortedLists.cpp

--- MergekSortedLists.cpp
+++ MergekSortedLists.cpp
@@ -1,7 +1,8 @@
-//============================================================================
-// Merge k sorted linked lists and return it as one sorted list.
-// Analyze and describe its complexity.
-//============================================================================
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Hide Tags Divide and Conquer Linked List Heap
+*/
 
 struct min_heap {
     bool operator() (const ListNode* a, const ListNode* b) {
@@ -30,39 +31,5 @@ public:
         curr=head->next;
         delete head;
         return curr;
-    }
-};
-
-
-
-struct greater_listnode {
-    bool operator() (const ListNode* a, const ListNode* b) {
-        return a->val > b->val;
-    }
-};
-
-class Solution {
-public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        priority_queue<ListNode*, vector<ListNode*>, greater_listnode > minHeap;
-        for (vector<ListNode *>::iterator it = lists.begin(); it != lists.end(); it++) {
-            if ((*it) != NULL)
-                minHeap.push(*it);
-        }
-        if (minHeap.empty())
-            return NULL;
-        ListNode *head = minHeap.top();
-        minHeap.pop();
-        ListNode *node = head;
-        if (node->next != NULL)
-            minHeap.push(node->next);
-        while (!minHeap.empty()) {
-            node->next = minHeap.top();
-            minHeap.pop();
-            node = node->next;
-            if (node->next != NULL)
-                minHeap.push(node->next);
-        }
-        return head;
     }
 };
