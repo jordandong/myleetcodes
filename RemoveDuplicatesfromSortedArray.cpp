338c2f5279578e4c4474a549e39fbb11bb21c2b9
jordandong <jordandong@gmail.com>
2015-02-09 17:23:47 -0800

    Update RemoveDuplicatesfromSortedArray.cpp

--- RemoveDuplicatesfromSortedArray.cpp
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -1,29 +1,28 @@
-//============================================================================
-// Remove Duplicates from Sorted Array
-// Given a sorted array, remove the duplicates in place such that each element
-// appear only once and return the new length.
-//
-// Do not allocate extra space for another array, you must do this in place
-// with constant memory.
-//
-// For example,
-// Given input array A = [1,1,2],
-//
-// Your function should return length = 2, and A is now [1,2].
-//============================================================================
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+
+Hide Tags Array Two Pointers
+*/
 
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
         int k=0, i=0;
-        while(i<n){
-            if(A[i]==A[k]){
+        while(i < n){
+            if(A[i] == A[k]){
                 i++;
                 continue;
             }
-            A[++k]=A[i++];
+            A[++k] = A[i++];
         }
-        return i==k?0:k+1;
+        return i==k ? 0:k+1;
     }
 };
 
@@ -32,33 +31,15 @@ public:
     int removeDuplicates(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if(n<2)
+        if(n < 2)
             return n;
-        else{
-            int pos = 1;
-            for(int i=0;i<n-1;i++){
-                if(A[i+1]!=A[i]){
-                    A[pos] = A[i+1];
-                    pos++;
-                }
+        int pos = 1;
+        for(int i = 0; i < n-1; i++){
+            if(A[i+1] != A[i]){
+                A[pos] = A[i+1];
+                pos++;
             }
-            return pos;
         }
-    }
-};
-
-class Solution {
-public:
-    int removeDuplicates(int A[], int n) {
-        if (n < 2)
-            return n;
-        int i = 1, j = 1;
-        while (i < n) {
-            if (A[i-1] != A[i])
-                A[j++] = A[i++];
-            else
-                i++;
-        }
-        return j;
+        return pos;
     }
 };
