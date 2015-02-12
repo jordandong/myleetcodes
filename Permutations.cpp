7eb64f634f02225c1b0487e74b0c7b691a826ce1
jordandong <jordandong@gmail.com>
2015-02-11 22:49:13 -0800

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,63 +1,67 @@
-//============================================================================
-// Given a collection of numbers, return all possible permutations.
-//
-// For example,
-// [1,2,3] have the following permutations:
-// [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
-//============================================================================
+/*
+Given a collection of numbers, return all possible permutations.
 
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 
+Hide Tags Backtracking
+*/
+
+//Without Recursion
 class Solution {
 public:
-    vector<vector<int> > permute(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+    vector<vector<int> > permute(vector<int> &nums) {
         vector<vector<int> > res;
-        perm(res, 0, num);
+        if(nums.size() == 0)
+            return res;
+        sort(nums.begin(), nums.end());
+        res.push_back(nums);
+        while(next(nums)){
+            res.push_back(nums);
+        }
         return res;
     }
     
-    void perm(vector<vector<int> > &res, int begin, vector<int> &num){
-        if(begin==num.size()){
-            res.push_back(num);
-        }
-        else{
-            for(int i=begin; i<num.size(); i++){
-                swap(num[begin], num[i]);
-                perm(res, begin+1, num);
-                swap(num[begin], num[i]);
-            }
-        }
+    bool next(vector<int> &nums){
+        int sz = nums.size();
+        int i = sz - 2;
+        int j = sz - 1;
+        while(i >= 0 && nums[i] >= nums[i+1])
+            i--;
+        if(i == -1)
+            return false;
+        
+        while(j > i && nums[j]<=nums[i])
+            j--;
+        
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i +1, nums.end());
+        return true;
     }
 };
 
-
-
-
-#include <vector>
-using namespace std;
-
+//Recursion
 class Solution {
 public:
-    void permuteHelper(vector<int> num, int i, vector<vector<int> > &result) {
-        if (i == num.size())
-			result.push_back(num);
-        else {
-            for (int j = i; j < num.size(); j++) {
-                swap(num[i], num[j]);
-                permuteHelper(num, i + 1, result);
-                swap(num[i], num[j]);
-            }
+    vector<vector<int> > permute(vector<int> &nums) {
+        vector<vector<int> > res;
+        permuteHelper(nums, 0, res);
+        return res;
+    }
+    
+    void permuteHelper(vector<int> &nums, int start, vector<vector<int> > &res){
+        int sz = nums.size();
+        if(start == sz){
+            if(sz != 0)
+                res.push_back(nums);
+            return;
+        }
+        
+        for(int i = start; i < sz; i++){
+            swap(nums[start], nums[i]);
+            permuteHelper(nums, start + 1, res);
+            swap(nums[i], nums[start]);
         }
     }
-
-    vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int> > result;
-        permuteHelper(num, 0, result);
-        return result;
-    }
 };
-
-int main() {
-	return 0;
-}
