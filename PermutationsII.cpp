9464147d5a5680678631553d85b453be23af4639
jordandong <jordandong@gmail.com>
2015-02-11 22:55:27 -0800

    Update PermutationsII.cpp

--- PermutationsII.cpp
+++ PermutationsII.cpp
@@ -1,91 +1,78 @@
-//============================================================================
-// Permutations II
-// Given a collection of numbers that might contain duplicates, return all
-// possible unique permutations.
-//
-// For example,
-// [1,1,2] have the following unique permutations:
-// [1,1,2], [1,2,1], and [2,1,1].
-//============================================================================
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
+For example,
+[1,1,2] have the following unique permutations:
+[1,1,2], [1,2,1], and [2,1,1].
 
+Hide Tags Backtracking
+*/
+
+//Without Recursion
 class Solution {
 public:
-    vector<vector<int> > permuteUnique(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+    vector<vector<int> > permuteUnique(vector<int> &nums) {
         vector<vector<int> > res;
-        perm(res, 0, num);
+        if(nums.size() == 0)
+            return res;
+        sort(nums.begin(), nums.end());
+        res.push_back(nums);
+        while(next(nums)){
+            res.push_back(nums);
+        }
         return res;
-        
     }
     
-    void perm(vector<vector<int> > &res, int begin, vector<int> &num){
-        if(begin==num.size()){
-            res.push_back(num);
+    bool next(vector<int> &nums){
+        int sz = nums.size();
+        int i = sz - 2;
+        int j = sz - 1;
+        while(i >=0 && nums[i] >= nums[i+1])
+            i--;
+        if(i == -1)
+            return false;
+        
+        while(j>i && nums[j]<=nums[i])
+            j--;
+        
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i +1, nums.end());
+        return true;
+    }
+};
+
+//Recursion
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &nums) {
+        // write your code here
+        vector<vector<int> > res;
+        permuteUniqueHelper(nums, 0, res);
+        return res;
+    }
+    
+    void permuteUniqueHelper(vector<int> &nums, int start, vector<vector<int> > &res){
+        int sz = nums.size();
+        if(start == sz){
+            if(sz != 0)
+                res.push_back(nums);
+            return;
         }
-        else{
-            for(int i=begin; i<num.size(); i++){
-                if(IsSwap(begin, i, num)){
-                    swap(num[begin], num[i]);
-                    perm(res, begin+1, num);
-                    swap(num[begin], num[i]);
-                }
+        
+        for(int i = start; i < sz; i++){
+            if(do_swap(nums, start, i)){
+                swap(nums[start], nums[i]);
+                permuteUniqueHelper(nums, start+1, res);
+                swap(nums[i], nums[start]);
             }
         }
     }
-
-    bool IsSwap(int begin, int curr, vector<int> & num){
-        while(begin<curr){
-            if(num[begin]==num[curr])
+    
+    bool do_swap(vector<int>&nums, int start, int end){
+        while(start < end){
+            if(nums[start++] == nums[end])
                 return false;
-            begin++;
         }
         return true;
-    } 
-};
-
-
-
-#include <vector>
-#include <cassert>
-using namespace std;
-
-class Solution {
-public:
-    void reverse(vector<int> &num, int i, int j) {
-        while(i<j)
-			swap(num[i++], num[j--]);
-    };
-
-    void nextPermutation(vector<int> &num) {
-        int N=num.size();
-        if(N<2)
-			return;
-        int i=N-2;
-        while(i>=0 && num[i]>=num[i+1])
-			i--;
-        if (i == -1) {
-            reverse(num, 0, N - 1);
-            return;
-        }
-        int j=N-1;
-        while(j>i && num[j]<=num[i])
-			j--;
-        swap(num[i], num[j]);
-        reverse(num, i+1, N-1);
-    };
-
-    vector<vector<int> > permuteUnique(vector<int> &num) {
-        vector<vector<int> > result;
-        do {
-            result.push_back(num);
-            nextPermutation(num);
-        } while (!equal(result[0].begin(), result[0].end(), num.begin()));
-        return result;
     }
 };
-
-int main() {
-    return 0;
-}
