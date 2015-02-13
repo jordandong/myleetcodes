53084b4b14e5a761eadf7ea0b70ecd7f4e580d6e
jordandong <jordandong@gmail.com>
2015-02-12 23:08:34 -0800

    Update JumpGame.cpp

--- JumpGame.cpp
+++ JumpGame.cpp
@@ -1,55 +1,44 @@
-//============================================================================
-// Given an array of non-negative integers, you are initially positioned at
-// the first index of the array.
-//
-// Each element in the array represents your maximum jump length at that
-// position.
-//
-// Determine if you are able to reach the last index.
-//
-// For example:
-// A = [2,3,1,1,4], return true.
-//
-// A = [3,2,1,0,4], return false.
-//============================================================================
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
-#include <algorithm>
-using namespace std;
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+A = [3,2,1,0,4], return false.
+
+Hide Tags Array Greedy
+*/
 
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        int i = 0;
-        int next = 0;
-        while (i <= next) {
-            next = max(next, i + A[i]);
-            if (next >= n - 1)
-				return true;
-            i++;
+        if(n == 0)
+            return false;
+
+        int end = 0;
+        for(int start = 0; start <= end; start++){
+            end = max(end, start + A[start]);
+            if(end >= n - 1)
+                return true;
         }
         return false;
     }
 };
 
-int main() {
-    return 0;
-}
-
-
-
-
+//recursion, cannnot pass TLE
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(n<0)
+        if(n < 0)
             return false;
-        else if(n<=1)
+        else if(n <= 1)
             return true;
         else{
-            for(int i=0;i<A[0];i++){
-                if(canJump(A+i+1,n-i-1))
+            for(int i = 0; i < A[0]; i++){
+                if(canJump(A + i + 1, n - i - 1))
                     return true;
             }
         }
