23d17badefddf9a5d59a93788615365a3d3e25d8
jordandong <jordandong@gmail.com>
2015-02-12 22:38:40 -0800

    Update Anagrams.cpp

--- Anagrams.cpp
+++ Anagrams.cpp
@@ -1,67 +1,54 @@
-//============================================================================
-// Given an array of strings, return all groups of strings that are anagrams.
-//
-// Note: All inputs will be in lower-case.
-//============================================================================
+/*
+Given an array of strings, return all groups of strings that are anagrams.
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <algorithm>
-using namespace std;
+Note: All inputs will be in lower-case.
+
+Hide Tags Hash Table String
+*/
 
 class Solution {
 public:
     vector<string> anagrams(vector<string> &strs) {
-        map<string, vector<string> > buffer;
-        for (int i = 0; i < strs.size(); i++) {
-            string copy(strs[i]);
-            sort(copy.begin(), copy.end());
-            buffer[copy].push_back(strs[i]);
-        }
         vector<string> res;
-        map<string, vector<string> >::iterator it = buffer.begin();
-        while (it != buffer.end()) {
-            vector<string> sub = it->second;
-            if (sub.size() > 1)
-                for (int i = 0; i < sub.size(); i++)
-                    res.push_back(sub[i]);
+        map<string, vector<string> > mp;
+        for (int i = 0; i < strs.size(); i++) {
+            string copy = strs[i];
+            sort(copy.begin(), copy.end());
+            mp[copy].push_back(strs[i]);
+        }
+
+        auto it = mp.begin();
+        while (it != mp.end()) {
+            vector<string> &data = it->second;
+            if(data.size() > 1)
+                for(int i = 0; i < data.size(); i++)
+                    res.push_back(data[i]);
             it++;
         }
         return res;
     }
 };
 
-int main() {
-    return 0;
-}
-
-
+//Cannnot pass TLE
 class Solution {
 public:
     vector<string> anagrams(vector<string> &strs) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         map<string, map<char, int> > strCharsMap;
         map<map<char, int>, int> charsCountMap;
+        vector<string> res;
+        
         for(int i = 0; i < strs.size(); ++i) {
-            string s = strs[i];
             map<char, int> map;
-            for(int j = 0; j < s.size(); ++j) {
-                char c = s[j];
-                map[c] += 1;
-            }
-            
-            strCharsMap[s] = map;
+            for(int j = 0; j < strs[i].size(); ++j)
+                map[strs[i][j]] += 1;
+            strCharsMap[strs[i]] = map;
             charsCountMap[map] += 1;
         }
         
-        vector<string> sol;
         for(int i = 0; i < strs.size(); ++i) {
             if(charsCountMap[strCharsMap[strs[i]]] > 1)
-                sol.push_back(strs[i]);
+                res.push_back(strs[i]);
         }
-        return sol;
+        return res;
     }
 };
