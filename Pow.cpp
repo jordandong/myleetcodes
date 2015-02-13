a4a6381173e749f56e66024a9e1d3a98e0f6a02a
jordandong <jordandong@gmail.com>
2015-02-12 22:58:40 -0800

    Update Pow.cpp

--- Pow.cpp
+++ Pow.cpp
@@ -1,55 +1,28 @@
-//============================================================================
-// Pow(x, n)
-// Implement pow(x, n).
-//============================================================================
+/*
+Implement pow(x, n).
+
+Hide Tags Math Binary Search
+*/
 
 class Solution {
 public:
     double pow(double x, int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        bool flag = false;
-        if(n<0){
-            flag=true;
-            n=-n;
-        }
-        
         double res = 1;
-        while(n>0){
-            if(n%2==1)
-                res*=x;
-            n/=2;
-            x*=x;
+        bool sign = false;
+        unsigned int nn;//handle -2147483648
+        if(n < 0){
+            nn = -n;
+            sign = true;
+        }else{
+            nn = n;
         }
         
-        if(flag)
-            return 1/res;
-        else
-            return res;
-    }
-};
-
-
-class Solution {
-public:
-    double pow(double x, int n) {
-        int sign=1;
-        if(n<0){ 
-        	sign=-sign;
-			n = -n;
-		}
-        double tmp=x, res=1.0;
-        for(int t = n; t>0; t/=2){
-            if(t%2==1)
-            	res*=tmp;
-            tmp=tmp*tmp;
+        while(nn){
+            if(nn & 1)
+                res *= x;
+            x *= x;
+            nn = (nn >> 1);
         }
-        if(sign<0)
-        	return 1.0/res;
-        return res;
+        return sign ? 1.0/res : res;
     }
 };
-
-int main() {
-    return 0;
-}
