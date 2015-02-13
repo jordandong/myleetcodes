14336580b31ae9580467ab28806de5ddc3386326
jordandong <jordandong@gmail.com>
2015-02-12 23:46:26 -0800

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -12,6 +12,7 @@ If you have figured out the O(n) solution, try coding another solution using the
 Hide Tags Divide and Conquer Array Dynamic Programming
 */
 
+//T: O(N)
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
@@ -26,5 +27,44 @@ public:
                 sum = 0;
         }
         return res;
+    }
+};
+
+//T: O(N*logN)
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        if(n <= 0)
+            return -1;
+        return maxSubArrayHelper(A, 0, n-1);
+    }
+    
+    int maxSubArrayHelper(int A[], int lo, int hi){
+        /* One element in the array */
+        if (lo == hi) 
+            return A[lo];
+        /* Middle element of the array */   
+        int mid = lo + (hi - lo) / 2;
+        
+        /* find maximum sum crossing to left */
+        int leftMax = A[mid];
+        int sum = 0;
+        for(int i = mid; i >= lo; i--) {
+            sum += A[i];
+            if(sum > leftMax)
+                leftMax = sum;
+        }
+        
+        /* find maximum sum crossing to right */
+        int rightMax = A[mid + 1];
+        sum = 0;
+        for(int i = mid + 1; i <= hi; i++) {
+            sum += A[i];
+            if(sum > rightMax)
+                rightMax = sum;
+        }
+        
+        /* Return the maximum of left side, right side OR across (mid, mid+1) */
+        return max(leftMax + rightMax, max(maxSubArrayHelper(A, lo, mid), maxSubArrayHelper(A, mid + 1, hi)));
     }
 };
