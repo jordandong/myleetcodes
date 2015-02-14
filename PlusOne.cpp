a8bff61cda3117e2d74fda7f81101f26b2cafb9a
jordandong <jordandong@gmail.com>
2015-02-14 13:43:35 -0800

    Update PlusOne.cpp

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -1,56 +1,46 @@
-//============================================================================
-// PlusOne
-// Given a number represented as an array of digits, plus one to the number.
-//============================================================================
+/*
+Given a non-negative number represented as an array of digits, plus one to the number.
 
-#include <vector>
-#include <algorithm>
-using namespace std;
+The digits are stored such that the most significant digit is at the head of the list.
 
+Hide Tags Array Math
+*/
+
+//T:O(N + N)
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int len = digits.size();
+        if(len < 1)
+            return digits;
+        
+        int carry = 1;
+        for(int i = len - 1; i >= 0; i--){
+            int sum = carry + digits[i];
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if(carry > 0)
+            digits.insert(digits.begin(), carry);
+        
+        return digits;
+    }
+};
+
+//T: O(2 * N/2)
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
         reverse(digits.begin(), digits.end());
         int carry = 1;
-        for (int i = 0; i < digits.size(); i++) {
-            int sum = digits[i] + carry;
-            digits[i] = sum % 10;
-            carry = sum / 10;
-            if (carry == 0)
-				break;
+        for(auto &e : digits){
+            int c = (e + carry)/10;
+            e = (e + carry)%10;
+            carry = c;
         }
-        if (carry != 0)
-			digits.push_back(carry);
+        if(carry)
+            digits.push_back(carry);
         reverse(digits.begin(), digits.end());
-        return digits;
-    }
-};
-
-int main() {
-	return 0;
-}
-
-
-class Solution {
-public:
-    vector<int> plusOne(vector<int> &digits) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int len = digits.size();
-        if(len < 1)
-            return digits;
-        
-        int carry=(1 + digits[len-1])/10;
-        digits[len-1]=(digits[len-1] + 1)%10;
-        for(int i= len -2;i>=0;i--){
-            int sum = carry + digits[i];
-            carry = sum/10;
-            digits[i] = sum%10;
-        }
-        if(carry>0){
-            digits.insert(digits.begin(), carry);
-        }
-        
         return digits;
     }
 };
