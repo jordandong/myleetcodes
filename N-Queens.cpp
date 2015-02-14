2086864418139175cc8266daf4be5c8b12b5dee9
jordandong <jordandong@gmail.com>
2015-02-13 22:59:53 -0800

    Update N-Queens.cpp

--- N-Queens.cpp
+++ N-Queens.cpp
@@ -1,49 +1,71 @@
-//============================================================================
-// N-Queens
-//
-// The n-queens puzzle is the problem of placing n queens on an n×n chessboard
-// such that no two queens attack each other.
-//============================================================================
+/*
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+...Q....
+......Q.
+..Q.....
+.......Q
+.Q......
+....Q...
+Q.......
+.....Q..
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+
+Hide Tags Backtracking
+*/
 
 class Solution {
 public:
-    bool isSafe(vector<string> &board, int row, int col) {
-        int N = board.size();
-        int i = row, j = 0;
-        while(j < col)
-            if (board[i][j++] == 'Q')
-                return false;
-        i = row, j = col;
-        while (i >=0 && j >= 0)
-            if (board[i--][j--] == 'Q')
-                return false;
-        i = row, j = col;
-        while (i < N && j >= 0)
-            if (board[i++][j--] == 'Q')
-                return false;
-        return true;
-    };
-
-    void solveNQueensHelper(vector<string> &board, int col, vector<vector<string> > &result) {
-        int N = board.size();
-        if (col == N) {
-            result.push_back(board);
+    vector<vector<string> > solveNQueens(int n) {
+        vector<vector<string> > res;
+        if(n <= 0)
+            return res;
+        vector<string> sol(n, string(n, '.'));
+        int col_Val[n];
+        memset(col_Val, -1, sizeof(int)*n);
+        solveNQueensHelper(n, 0, col_Val, sol, res);
+        return res;
+    }
+    
+    void solveNQueensHelper(int n, int row, int* col_Val, vector<string> &sol, vector<vector<string> > &res){
+        if(row == n){
+            res.push_back(sol);
             return;
         }
-        for(int i = 0; i < N; i++) {
-            if (isSafe(board, i, col)) {
-                board[i][col] = 'Q';
-                solveNQueensHelper(board, col + 1, result);
-                board[i][col] = '.';
+        
+        for(int c = 0; c < n; c++){
+            if(isValid(row, c, col_Val)){
+                col_Val[row] = c;
+                sol[row][c] = 'Q';
+                solveNQueensHelper(n, row + 1, col_Val, sol, res);
+                sol[row][c] = '.';
             }
         }
-    };
-
-    vector<vector<string> > solveNQueens(int n) {
-        vector<vector<string> > result;
-        string row(n, '.');
-        vector<string> board(n, row);
-        solveNQueensHelper(board, 0, result);
-        return result;
+    }
+    
+    bool isValid(int row, int col, int *col_Val){
+        for(int r = row - 1; r >= 0; r--){
+            if(row - r == abs(col - col_Val[r]) || col == col_Val[r])
+                return false;
+        }
+        return true;
     }
 };
