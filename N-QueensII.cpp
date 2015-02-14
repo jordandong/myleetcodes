73ba26950743d9b64493c14146da7efb75e0c471
jordandong <jordandong@gmail.com>
2015-02-13 22:50:00 -0800

    Update N-QueensII.cpp

--- N-QueensII.cpp
+++ N-QueensII.cpp
@@ -1,35 +1,51 @@
-//============================================================================
-// N-Queens II
-// Follow up for N-Queens problem.
-//
-// Now, instead outputting board configurations, return the total number of
-// distinct solutions.
-//============================================================================
+/*
+Follow up for N-Queens problem.
+
+Now, instead outputting board configurations, return the total number of distinct solutions.
+
+...Q....
+......Q.
+..Q.....
+.......Q
+.Q......
+....Q...
+Q.......
+.....Q..
+
+Hide Tags Backtracking
+*/
 
 class Solution {
 public:
-    bool isSafe(int board[], int n, int row, int col) {
-        for (int j = 0; j < col; j++)
-            if (board[j] == row || abs(board[j] - row) == col - j)
-                return false;
-        return true;
+    int totalNQueens(int n) {
+        if(n <= 0)
+            return 0;
+        int total = 0;
+        int col_Val[n];
+        memset(col_Val, -1, sizeof(int)*n);
+        totalNQueensHelper(n, 0, col_Val, total);
+        return total;
     }
-
-    void totalNQueensHelper(int board[], int n, int col, int &count) {
-        if (col == n)
-            count++;
-        for (int row = 0; row < n; row++) {
-            if (isSafe(board, n, row, col)) {
-                board[col] = row;
-                totalNQueensHelper(board, n, col+1, count);
+    
+    void totalNQueensHelper(int n, int row, int* col_Val, int &total){
+        if(row == n){
+            total++;
+            return;
+        }
+        
+        for(int c = 0; c < n; c++){
+            if(isValid(row, c, col_Val)){
+                col_Val[row] = c;
+                totalNQueensHelper(n, row + 1, col_Val, total);
             }
         }
-    };
-
-    int totalNQueens(int n) {
-        int count = 0;
-        int board[n];
-        totalNQueensHelper(board, n, 0, count);
-        return count;
-    };
+    }
+    
+    bool isValid(int row, int col, int *col_Val){
+        for(int r = row - 1; r >= 0; r--){
+            if(row - r == abs(col - col_Val[r]) || col == col_Val[r])
+                return false;
+        }
+        return true;
+    }
 };
