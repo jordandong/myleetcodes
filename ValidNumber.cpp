992c873ce1e04083519fcd1ad5479e34b2e4d427
jordandong <jordandong@gmail.com>
2015-02-14 13:20:00 -0800

    Update ValidNumber.cpp

--- ValidNumber.cpp
+++ ValidNumber.cpp
@@ -1,68 +1,100 @@
-//============================================================================
-// Valid Number
-// Validate if a given string is numeric.
-//
-// Some examples:
-// "0" => true
-// " 0.1 " => true
-// "abc" => false
-// "1 a" => false
-// "2e10" => true
-// Note: It is intended for the problem statement to be ambiguous. You should
-// gather all requirements up front before implementing one.
-//============================================================================
+/*
+Validate if a given string is numeric.
 
-#include <iostream>
-#include <cstring>
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
 
-using namespace std;
+Update (2015-02-10):
+The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+
+Hide Tags Math String
+*/
 
 class Solution {
 public:
-    bool isNumber(const char* s) {
+    bool isNumber(string s) {
+        int start = 0;
+        int end = s.length() - 1;
         // trim leading/trailing spaces
-        while (*s != '\0' && isspace(*s))
-        	s++;
-        if (*s == '\0')
-        	return false;
-        const char *e = s + strlen(s) - 1;
-        while (e > s && isspace(*e))
-        	e--;
+        while(start <= end && s[start] == ' ')
+            start++;
+        if(start > end)
+            return false;
+        while(s[end] == ' ')
+            end--;
+
         // skip leading +/-
-        if (*s == '+' || *s == '-')
-        	s++;
+        if (s[start]== '+' || s[start] == '-')
+            start++;
 
         bool num = false; // is a digit
         bool dot = false; // is a '.'
         bool exp = false; // is a 'e'
-        while (s != e + 1) {
-            if (*s >= '0' && *s <= '9') {
+        while(start <= end) {
+            if (s[start] >= '0' && s[start] <= '9') {
                 num = true;
-            }
-            else if (*s == '.') {
+            }else if (s[start] == '.') {
                 if(exp || dot)
-                	return false;
+                    return false;
                 dot = true;
-            }
-            else if (*s == 'e') {
+            }else if (s[start] == 'e') {
                 if(exp || num == false)
-                	return false;
-                exp = true;
+                    return false;
+                exp = true; 
                 num = false;
-            }
-            else if (*s == '+' || *s == '-') {
-                if (*(s-1) != 'e')
-                	return false;
-            }
-            else {
+            }else if (s[start] == '+' || s[start] == '-') {
+                if (s[start - 1] != 'e')
+                    return false;
+            }else {
                 return false;
             }
-            s++;
+            start++;
         }
         return num;
     }
 };
 
-int main() {
-   return 0;
+bool isNumber(const char* s) {
+    // trim leading/trailing spaces
+    while(*s != '\0' && *s == ' ')
+        s++;
+    if(*s == '\0')
+        return false;
+    const char *e = s + strlen(s) - 1;
+    while(*e == ' ')
+        e--;
+
+    // skip leading +/-
+    if (*s == '+' || *s == '-')
+        s++;
+
+    bool num = false; // is a digit
+    bool dot = false; // is a '.'
+    bool exp = false; // is a 'e'
+    while(s <= e) {
+        if (*s >= '0' && *s <= '9') {
+            num = true;
+        }else if (*s == '.') {
+            if(exp || dot)
+                return false;
+            dot = true;
+        }else if (*s == 'e') {
+            if(exp || num == false)
+                return false;
+            exp = true;
+            num = false;
+        }else if (*s == '+' || *s == '-') {
+            if (*(s-1) != 'e')
+                return false;
+        }else {
+            return false;
+        }
+        s++;
+    }
+    return num;
 }
