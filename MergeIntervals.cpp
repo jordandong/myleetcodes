7a5302744734ffe98d619bdd014c525febc53025
jordandong <jordandong@gmail.com>
2015-02-14 12:03:25 -0800

    Update MergeIntervals.cpp

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -1,11 +1,12 @@
-//============================================================================
-// Given a collection of intervals, merge all overlapping intervals.
-//
-// For example,
-// Given [1,3],[2,6],[8,10],[15,18],
-// return [1,6],[8,10],[15,18].
-//
-//============================================================================
+/*
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+
+Hide Tags Array Sort
+*/
 
 /**
  * Definition for an interval.
@@ -17,68 +18,28 @@
  * };
  */
  
-bool start_increasing(struct Interval a, struct Interval b) {
-    return a.start < b.start;
+bool interval_cmp(Interval &a, Interval &b){
+     return a.start < b.start;
 }
-
-class Solution {
-public:
-
-    vector<Interval> merge(vector<Interval> &intervals) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<Interval> res;
-        int len = intervals.size();
-        if(!len)
-            return res;
-        sort(intervals.begin(), intervals.end(), start_increasing);
-        struct Interval curr = intervals[0];
-        for(int i=1;i<len;i++){
-            if(intervals[i].start<=curr.end){
-                curr.start = curr.start<intervals[i].start?curr.start:intervals[i].start;
-                curr.end = curr.end>intervals[i].end?curr.end:intervals[i].end;
-            }
-            else{
-                res.push_back(curr);
-                curr = intervals[i];
-            }    
-        }
-        res.push_back(curr);
-        return res;
-    }
-};
-
-
-struct StartIncreasing {
-    bool operator() (const Interval &a, const Interval &b) {
-        return a.start < b.start;
-    }
-} start_increasing;
-
+ 
 class Solution {
 public:
     vector<Interval> merge(vector<Interval> &intervals) {
-        if (intervals.size() < 2)
+        if(intervals.size() < 2)
             return intervals;
-        sort(intervals.begin(), intervals.end(), start_increasing);
-        vector<Interval> result;
-        Interval curInterval;
-        vector<Interval>::iterator it = intervals.begin();
-        while (it != intervals.end()) {
-            if (it == intervals.begin()) {
-                curInterval = *it;
+        vector<Interval> res;
+        sort(intervals.begin(), intervals.end(), interval_cmp);
+        Interval cur = intervals[0];
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i].start <= cur.end){
+                cur.start = min(intervals[i].start, cur.start);
+                cur.end = max(intervals[i].end, cur.end);
+                continue;
             }
-            else if ((*it).start <= curInterval.end && curInterval.start <= (*it).end) {
-                curInterval.start = (curInterval.start > (*it).start) ? (*it).start : curInterval.start;
-                curInterval.end = ((curInterval.end < (*it).end) ? (*it).end : curInterval.end);
-            }
-            else if ((*it).start > curInterval.end) {
-                result.push_back(curInterval);
-                curInterval = *it;
-            }
-            it++;
+            res.push_back(cur);
+            cur = intervals[i];
         }
-        result.push_back(curInterval);
-        return result;
+        res.push_back(cur);
+        return res;
     }
 };
