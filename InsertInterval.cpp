48ebe36654ac5d8a44609a5e26bfea108a45b834
jordandong <jordandong@gmail.com>
2015-02-14 12:23:26 -0800

    Update InsertInterval.cpp

--- InsertInterval.cpp
+++ InsertInterval.cpp
@@ -1,58 +1,47 @@
-//============================================================================
-// Given a set of non-overlapping intervals, insert a new interval into the
-// intervals (merge if necessary).
-//
-// You may assume that the intervals were initially sorted according to their
-// start times.
-//
-// Example 1:
-// Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
-//
-// Example 2:
-// Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as
-// [1,2],[3,10],[12,16].
-//
-// This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
-//============================================================================
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 
-#include <iostream>
-#include <vector>
-using namespace std;
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+
+Hide Tags Array Sort
+*/
 
 /**
  * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
  */
-struct Interval {
-    int start;
-    int end;
-public:
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
-};
-
 class Solution {
 public:
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        vector<Interval> result;
-        bool merge_finished = false;
-        for (vector<Interval>::iterator it = intervals.begin(); it != intervals.end(); it++){
-            if ((*it).start <= newInterval.end && newInterval.start <= (*it).end){
-                newInterval.start = (newInterval.start > (*it).start) ? (*it).start : newInterval.start;
-                newInterval.end = ((newInterval.end < (*it).end) ? (*it).end : newInterval.end);
+        vector<Interval> res;
+        bool inserted = false;
+        for(int i = 0; i < intervals.size(); i++){
+            if(newInterval.end >= intervals[i].start && newInterval.start <= intervals[i].end){
+                newInterval.start = min(newInterval.start, intervals[i].start);
+                newInterval.end = max(newInterval.end, intervals[i].end);
                 continue;
             }
-            if (!merge_finished && (*it).start > newInterval.end) {
-                result.push_back(newInterval);
-                merge_finished = true;
+            if(!inserted && intervals[i].start > newInterval.end){
+                res.push_back(newInterval);
+                inserted = true;
             }
-            result.push_back(*it);
+            res.push_back(intervals[i]);
         }
-        if (!merge_finished)
-			result.push_back(newInterval);
-        return result;
+        if(!inserted)
+            res.push_back(newInterval);
+        return res;
     }
 };
-
-int main() {
-    return 0;
-}
