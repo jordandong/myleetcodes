f9f38965c5d20b61937d4ad6cb87b499a83ec167
jordandong <jordandong@gmail.com>
2015-02-15 14:27:44 -0800

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,53 +1,42 @@
-//============================================================================
-// Subsets
-// Given a set of distinct integers, S, return all possible subsets.
-//
-// Note:
-// Elements in a subset must be in non-descending order.
-// The solution set must not contain duplicate subsets.
-// For example,
-// If S = [1,2,3], a solution is:
-//
-// [
-//  [3],
-//  [1],
-//  [2],
-//  [1,2,3],
-//  [1,3],
-//  [2,3],
-//  [1,2],
-//  []
-// ]
-//============================================================================
+/*
+Given a set of distinct integers, S, return all possible subsets.
 
-#include <vector>
-#include <algorithm>
-using namespace std;
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+Hide Tags Array Backtracking Bit Manipulation
+*/
 
 class Solution {
 public:
-    vector<vector<int> > subsets(vector<int> &S) {
-        int N = S.size();
-        int max = 1 << N;
-        vector<vector<int> > res;
-        for (int i = 0; i < max; i++) {
-            vector<int> sub;
-            int k = i;
-            int j = 0;
-            while (k > 0) {
-                if (k & 0x01)
-                	sub.push_back(S[j]);
-                k >>= 1;
-                j++;
-            }
-            sort(sub.begin(), sub.end());
-            res.push_back(sub);
-        }
-        sort(res.begin(), res.end());
-        return res;
+    vector<vector<int> > subsets(vector<int> &nums) {
+    	int n = nums.size();
+    	int mx = 1 << n;
+    	vector<vector<int> > res;
+        sort(nums.begin(), nums.end());
+    	for(int i = 0; i < mx; i++){
+    	    vector<int> sol;
+    	    for(int shift = 0; shift < n; shift++){
+    	        if((i>>shift) & 0x1){
+    	            sol.push_back(nums[shift]);
+    	        }
+    	    }
+    	    res.push_back(sol);
+    	}
+    	return res;
     }
 };
-
-int main() {
-    return 0;
-}
