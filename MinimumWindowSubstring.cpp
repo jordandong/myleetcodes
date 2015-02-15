8eaea8887bd70a7cb1bc3ede70cd224f56cd02f5
jordandong <jordandong@gmail.com>
2015-02-15 14:08:25 -0800

    Update MinimumWindowSubstring.cpp

--- MinimumWindowSubstring.cpp
+++ MinimumWindowSubstring.cpp
@@ -1,19 +1,18 @@
-//============================================================================
-// Given a string S and a string T, find the minimum window in S which will
-// contain all the characters in T in complexity O(n).
-//
-// For example,
-// S = "ADOBECODEBANC"
-// T = "ABC"
-// Minimum window is "BANC".
-//
-// Note:
-// If there is no such window in S that covers all characters in T, return the
-// empty string "".
-//
-// If there are multiple such windows, you are guaranteed that there will
-// always be only one unique minimum window in S.
-//============================================================================
+/*
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+For example,
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC".
+
+Note:
+If there is no such window in S that covers all characters in T, return the emtpy string "".
+
+If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+
+Hide Tags Hash Table Two Pointers String
+*/
 
 class Solution {
 public:
@@ -21,20 +20,20 @@ public:
         int needToFind[256] = {0};
         int hasFound[256] = {0};
         int count = 0;
-        int minL = S.size()+1;
+        int minL = S.size() + 1;
         int start = 0;
         for (int i = 0; i < T.size(); i++)
             needToFind[T[i]]++;
 
-        for (int begin = 0, end = 0; end<S.size(); end++) {
+        for (int begin = 0, end = 0; end < S.size(); end++) {
             if (needToFind[S[end]] == 0)
                 continue;
             hasFound[S[end]]++;
             if(hasFound[S[end]] <= needToFind[S[end]])
                 count++;
 
-            if (count == T.size()) {
-                while (needToFind[S[begin]] == 0 || hasFound[S[begin]] > needToFind[S[begin]]){
+            if (count == T.size()) { // always true after getting entered the firs time
+                while(needToFind[S[begin]] == 0 || hasFound[S[begin]] > needToFind[S[begin]]){
                     if (hasFound[S[begin]] > needToFind[S[begin]])
                         hasFound[S[begin]]--;
                     begin++;
@@ -47,43 +46,6 @@ public:
                 }
             }
         }
-        return minL==S.size()+1?"":S.substr(start, minL);
-    }
-};
-
-
-class Solution {
-public:
-    string minWindow(string S, string T) {
-        int needToFind[256] = {0};
-        int hasFound[256] = {0};
-        int count = 0;
-        for (int i = 0; i < T.size(); i++)
-            needToFind[(int)T[i]]++;
-
-        string res = "";
-        int min = INT_MAX;
-        for (int begin = 0, end = 0; end < S.size(); end++) {
-            if (needToFind[(int)S[end]] == 0)
-                continue;
-            hasFound[(int)S[end]]++;
-            if (hasFound[(int)S[end]] <= needToFind[(int)S[end]])
-               count++;
-
-            if (count == T.size()) {
-                while (needToFind[(int)S[begin]] == 0 || hasFound[(int)S[begin]] > needToFind[(int)S[begin]]){
-                    if (hasFound[(int)S[begin]] > needToFind[(int)S[begin]])
-                        hasFound[(int)S[begin]]--;
-                    begin++;
-                }
-
-                int len = end - begin + 1;
-                if (len < min) {
-                    min = len;
-                    res = S.substr(begin, len);
-                }
-            }
-        }
-        return res;
+        return minL == S.size() +1 ? "" : S.substr(start, minL);
     }
 };
