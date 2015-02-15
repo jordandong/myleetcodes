34709a0f1f31df7c9b464f70d62d5da970b09b7c
jordandong <jordandong@gmail.com>
2015-02-14 22:03:42 -0800

    Update ClimbingStairs.cpp

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -5,27 +5,30 @@
 // In how many distinct ways can you climb to the top?
 //============================================================================
 
+//T:O(N) S:(logN)
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0 || n==1)
+        if(n == 0 || n == 1)
             return 1;
-        int first_half = n&1?(n-1)>>1:n>>1;
-        int second_half = n&1?(n+1)>>1:n>>1;
+        int first_half = n&1 ? (n-1)>>1 : n>>1;
+        int second_half = n&1 ? (n+1)>>1 : n>>1;
+        //using one step OR two steps from first half to second half
         return climbStairs(first_half)*climbStairs(second_half) + climbStairs(first_half - 1)*climbStairs(second_half - 1); 
     }
 };
 
+//T:O(N) S:O(1)
 class Solution {
 public:
     int climbStairs(int n) {
         if (n <= 2)
         	return n;
-        int a = 1;
-        int b = 2;
+        int a = 1; //two steps to reach c
+        int b = 2; //one step to reach c
         int c;
         for (int i = 2; i < n; i++) {
-            c = a + b;
+            c = a + b; // from a to c plus from b to c
             a = b;
             b = c;
         }
@@ -33,27 +36,23 @@ public:
     }
 };
 
+//T: O(N) S: O(N)
 class Solution {
 public:
     int climbStairs(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int *arr = new int[n+1];
-        for(int i=0;i<=n;i++)
-            arr[i]=0;
-        return climb(n, arr);
+        int *dp = new int[n+1];
+        memset(dp, 0, sizeof(int)*(n+1));
+        return climbStairsHelper(n, dp);
     }
     
-    int climb(int n, int *arr) {
-        if(n==0)
-            return 1;
-        if(n<0)
-            return 0;
-        
-        if(arr[n]>0)
-            return arr[n];
+    int climbStairsHelper(int n, int *dp) {
+        if(n <= 0)
+            return n==0 ? 1 : 0;
             
-        arr[n] = climb(n-1, arr) + climb(n-2, arr);
-        return arr[n];
+        if(dp[n] > 0)
+            return dp[n];
+            
+        dp[n] = climbStairsHelper(n - 1, dp) + climbStairsHelper(n - 2, dp);
+        return dp[n];
     }
 };
