363466a946d44109c42008f80ddd1f410066ec18
jordandong <jordandong@gmail.com>
2015-02-15 14:14:25 -0800

    Update Combination.cpp

--- Combination.cpp
+++ Combination.cpp
@@ -1,77 +1,43 @@
-//============================================================================
-// Given two integers n and k, return all possible combinations of k numbers
-// out of 1 ... n.
-//
-// For example,
-// If n = 4 and k = 2, a solution is:
-//
-//[
-// [2,4],
-// [3,4],
-// [2,3],
-// [1,2],
-// [1,3],
-// [1,4],
-//]
-//============================================================================
+/*
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
 
-#include <vector>
-using namespace std;
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+Hide Tags Backtracking
+*/
 
 class Solution {
 public:
     vector<vector<int> > combine(int n, int k) {
-        vector<vector<int> > result;
-        result.clear();
-        vector<int> combination;
-        combineHeler(n, k, 1, combination, result);
-        return result;
-    }
-
-    void combineHeler(int n, int k, int i, vector<int> combination, vector<vector<int> > &result) {
-        if (combination.size() == k) {
-            result.push_back(combination);
-            return;
-        }
-        if (i == n + 1)
-  	      	return;
-        combineHeler(n, k, i + 1, combination, result);
-        combination.push_back(i);
-        combineHeler(n, k, i + 1, combination, result);
-    }
-};
-
-int main() {
-    return 0;
-}
-
-
-
-
-class Solution {
-public:
-    vector<vector<int> > combine(int n, int k) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<int> sub;
+        // write your code here
         vector<vector<int> > res;
-        comb(n,1,k,sub,res);
+        vector<int> sol;
+        if(k > n)
+            return res;
+        combineHelper(1, n, k, sol, res);
         return res;
     }
     
-    bool comb(int n, int start, int k, vector<int> &sub, vector<vector<int> > &res){
-        if(k==0)
-            return true;
-            
-        for(int i=start; i<=n; i++){
-            sub.push_back(i);
-            if(comb(n,i+1,k-1,sub,res)){
-                res.push_back(sub);
-                sub.pop_back();
-            }else{
-                sub.pop_back();
-            }  
+    void combineHelper(int start, int end, int k, vector<int> &sol, vector<vector<int> > &res){
+        if(k == 0){
+            res.push_back(sol);
+            return;
         }
-        return false;
+        
+        for(int i = start; i <= end; i++){
+            sol.push_back(i);
+            combineHelper(i + 1, end, k - 1, sol, res);
+            sol.pop_back();
+        }
     }
 };
