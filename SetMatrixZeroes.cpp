f16adaf1515cf5f776c9f1e4732e62d7a38b7ac9
jordandong <jordandong@gmail.com>
2015-02-15 13:45:16 -0800

    Update SetMatrixZeroes.cpp

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -1,59 +1,85 @@
-//============================================================================
-// Set Matrix Zeroes
-// Given a m x n matrix, if an element is 0, set its entire row and column to
-// 0. Do it in place.
-//
-// Follow up:
-// Did you use extra space?
-// A straight forward solution using O(mn) space is probably a bad idea.
-// A simple improvement uses O(m + n) space, but still not the best solution.
-// Could you devise a constant space solution?
-//============================================================================
+/*
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+
+click to show follow up.
+
+Follow up:
+Did you use extra space?
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+
+Hide Tags Array
+*/
 
 class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         int m = matrix.size();
+        if(m == 0)
+            return;
         int n = matrix[0].size();
-        bool col[n];
-        int r=0;
-        while(r<n)
-            col[r++]=false;
-            
-        for(int i=0;i<m;i++){
-            int k = 0;
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    col[j]=true;
-                    while(k<=j){
-                        matrix[i][k]=0;
-                        k++;
-                    }
+        if(n == 0)
+            return;
+        bool last_flag = false;
+        bool curr_flag = false;
+
+        for(int i = 0; i < m; i++){
+            curr_flag = false;
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    int r = i;
+                    while(r > 0)
+                        matrix[--r][j] = 0;//reset all row in this col
+                    curr_flag = true;
+                }else{
+                    if(i > 0 && matrix[i-1][j] == 0) //reset if last row is zero
+                        matrix[i][j] = 0;
+                    if(last_flag) //reset last row if last row reset flag is set
+                        matrix[i-1][j] = 0;
                 }
             }
-            if(k!=0){
-                    while(k<n){
-                        matrix[i][k]=0;
-                        k++;
-                    }
-            }
+            last_flag = curr_flag;
         }
         
-        for(int l=0;l<n;l++){
-            if(col[l]){
-                for(int p=0; p<m;p++)
-                    matrix[p][l]=0;
-            }
+        if(last_flag){//reset last row
+            for(int j = 0; j < n; j++)
+                matrix[m-1][j] = 0;
         }
     }
 };
 
-
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool col[n];
+        memset(col, false, sizeof(bool)*n);
+            
+        for(int i = 0; i < m; i++){
+            int k = 0;
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    col[j] = true;
+                    while(k <= j) //set the left part of col j on row i
+                        matrix[i][k++] = 0;
+                }
+            }
+            if(k != 0){ //set the right part of curr row i
+                while(k < n)
+                    matrix[i][k++] = 0;
+            }
+        }
+        
+        for(int c = 0; c < n; c++){
+            if(col[c]){
+                for(int p = 0; p < m; p++)//set col which has zero
+                    matrix[p][c] = 0;
+            }
+        }
+    }
+};
 
 class Solution {
 public:
@@ -76,9 +102,8 @@ public:
         }
         for(int i = 1; i < row; i++) {
             for(int j = 1; j < col; j++) {
-                if(matrix[i][j] == 0) {
-                    matrix[0][j] = matrix[i][0] = 0;
-                }
+                if(matrix[i][j] == 0) 
+                    matrix[0][j] = matrix[i][0] = 0; //using first row/col to record the zeros in beyond (1, 1)
             }
         }
         for(int i = 1; i < row; i++) {
@@ -88,18 +113,12 @@ public:
             }
         }
         if(firstRow) {
-            for(int j = 0; j < col; j++) {
+            for(int j = 0; j < col; j++)
                 matrix[0][j] = 0;
-            }
         }
         if(firstCol) {
-            for(int i = 0; i < row; i++) {
+            for(int i = 0; i < row; i++)
                 matrix[i][0] = 0;
-            }
         }
     }
 };
-
-int main() {
-	return 0;
-}
