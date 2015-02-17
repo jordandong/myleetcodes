0b9401d5720c28c92c680dafc92e99b036b85e1a
jordandong <jordandong@gmail.com>
2015-02-16 23:20:01 -0800

    Update PartitionList.cpp

--- PartitionList.cpp
+++ PartitionList.cpp
@@ -1,72 +1,14 @@
-//============================================================================
-// Partition List
-// Given a linked list and a value x, partition it such that all nodes less
-// than x come before nodes greater than or equal to x.
-//
-// You should preserve the original relative order of the nodes in each of the
-// two partitions.
-//
-// For example,
-// Given 1->4->3->2->5->2 and x = 3,
-// return 1->2->2->4->3->5.
-//============================================================================
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 
-#include <iostream>
-using namespace std;
+You should preserve the original relative order of the nodes in each of the two partitions.
 
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
 
-class Solution {
-public:
-    ListNode *partition(ListNode *head, int x) {
-        ListNode* frontHead = NULL, *frontNode = NULL;
-        ListNode* backHead = NULL, *backNode = NULL;
-        while (head != NULL) {
-            if (head->val < x) {
-                if (frontHead == NULL) {
-                    frontHead = head;
-                    frontNode = head;
-                }
-                else {
-                    frontNode->next = head;
-                    frontNode = frontNode->next;
-                }
-            }
-            else {
-                if (backHead == NULL) {
-                    backHead = head;
-                    backNode = head;
-                }
-                else {
-                    backNode->next = head;
-                    backNode = backNode->next;
-                }
-            }
-            head = head->next;
-        }
-        
-        if (frontNode != NULL)
-        	frontNode->next = backHead;
-        if (backNode != NULL)
-        	backNode->next = NULL;
-        if (frontHead == NULL)
-        	return backHead;
-        return frontHead;
-    }
-};
-
-int main() {
-	return 0;
-}
-
-
+Hide Tags Linked List Two Pointers
+*/
 
 /**
  * Definition for singly-linked list.
@@ -79,42 +21,39 @@ int main() {
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(head==NULL)
-            return NULL;
-            
-        ListNode *point = new ListNode(INT_MIN);
-        ListNode *res = point;
-        
-        point->next=head;
-        while(point->next!=NULL){
-            if(point->next->val<x)
-                point=point->next;
-            else{
-                break;
-            }
-        }
-        
-        ListNode *xp = point;
-        
-        while(xp!=NULL){
-            if(xp->next!=NULL){
-                if(xp->next->val < x){
-                    ListNode* tmp = xp->next;
-                    xp->next=tmp->next;
-                    
-                    tmp->next=point->next;
-                    point->next = tmp;
-                    point=point->next;
-                }
-                else{
-                    xp = xp->next;
+        ListNode* first_head = NULL;
+        ListNode* first_tail = NULL;
+        ListNode* second_head = NULL;
+        ListNode* second_tail = NULL;
+        while(head){
+            if(head->val < x){
+                if(!first_head){
+                    first_head = head;
+                    first_tail = head;
+                }else{
+                    first_tail->next = head;
+                    first_tail = first_tail->next;
                 }
             }else{
-                xp=xp->next;
+                if(!second_head){
+                    second_head = head;
+                    second_tail = head;
+                }else{
+                    second_tail->next = head;
+                    second_tail = second_tail->next;
+                }
             }
+            head = head->next;
         }
-        return res->next;
+
+        if(first_tail)
+            first_tail->next = second_head;
+        else
+            return second_head;
+        
+        if(second_tail)
+            second_tail->next = NULL;
+
+        return first_head;
     }
 };
