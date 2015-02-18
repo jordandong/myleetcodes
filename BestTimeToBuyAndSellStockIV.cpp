e3eb6c9d53e93d1e7d69c33b71505eefe7a3b1d5
jordandong <jordandong@gmail.com>
2015-02-17 17:14:23 -0800

    20150217

---
+++ BestTimeToBuyAndSellStockIV.cpp
@@ -0,0 +1,48 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Credits:
+Special thanks to @Freezen for adding this problem and creating all test cases.
+
+Hide Tags Dynamic Programming
+*/
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        if (n < 2)
+            return 0; 
+            
+        if(k > n/2)
+            return bigK(prices);
+        k = min(k, n/2);//can not pass TLE if don't having bigK
+        
+        vector<vector<int> > dp(2, vector<int>(n, 0));
+        int c = 0;
+        for (int i = 1; i <= k; i++) {
+            int bM = dp[c][0] - prices[0];//last trans No., if buy at index 0, the max sum
+            for (int j = 1; j < n; j++) {
+                dp[c^1][j] = max(dp[c^1][j - 1], prices[j] + bM);
+                //same trans No. at j - 1 OR last trans No. and sell at j
+                bM =  max(bM, dp[c][j] - prices[j]);
+                //last trans No.,if buy at index j OR buy before j, the max sum 
+            }
+            c^=1;
+        }
+        return dp[c][n - 1];
+    }
+
+    int bigK(vector<int> &prices) {
+        int res = 0;
+        for(int i = 1; i < prices.size(); i++)
+            if(prices[i] > prices[i - 1])
+                res += prices[i] - prices[i - 1];
+        return res;
+    }
+};
