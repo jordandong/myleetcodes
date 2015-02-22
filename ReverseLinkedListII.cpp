cbd168c62b9d4e4f8e25058ab431c50f1e371fd5
jordandong <jordandong@gmail.com>
2015-02-21 17:06:03 -0800

    Update ReverseLinkedListII.cpp

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -1,61 +1,54 @@
-//============================================================================
-// Reverse a linked list from position m to n. Do it in-place and in one-pass.
-//
-// For example:
-// Given 1->2->3->4->5->NULL, m = 2 and n = 4,
-//
-// return 1->4->3->2->5->NULL.
-//
-// Note:
-// Given m, n satisfy the following condition:
-// 1 ≤ m ≤ n ≤ length of list.
-//============================================================================
+/*
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
 
-#include <iostream>
-using namespace std;
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+
+Hide Tags Linked List
+*/
 
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
-        if (head == NULL || head->next == NULL)
-        	return head;
-        ListNode *preTail = NULL, *curTail = NULL;
-        ListNode *preNode = NULL, *curNode = head;
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode *preTail = NULL, *curNode = head;
+        ListNode *revHead = NULL, *revTail = NULL; 
         
-        for (int i = 1; i <= n; i++) {
-            if (i == m-1)
+        for(int i = 1; i <= n; i++){
+            if(i ==  m - 1)
             	preTail = curNode;
-            if (i == m)
-            	curTail = curNode;
-            if (i >= m) {
+            if(i == m)
+            	revTail = curNode;
+            if(i >= m) {
                 ListNode* nextNode = curNode->next;
-                curNode->next = preNode;
-                preNode = curNode;
+                curNode->next = revHead;
+                revHead = curNode;
                 curNode = nextNode;
-            }
-            else
-				curNode = curNode->next;
+            }else
+                curNode = curNode->next;
         }
         
-        if (preTail == NULL)
-        	head = preNode;
+        if(preTail == NULL)
+            head = revHead;
         else
-        	preTail->next = preNode;
-        
-        curTail->next = curNode;
+            preTail->next = revHead;
+
+        revTail->next = curNode;
         return head;
     }
 };
-
-int main() {
-    return 0;
-}
