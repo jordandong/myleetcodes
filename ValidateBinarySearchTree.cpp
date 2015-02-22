54bcd5dd51d0ad410e8a8e939a082edb38653a1a
jordandong <jordandong@gmail.com>
2015-02-22 12:58:10 -0800

    Update ValidateBinarySearchTree.cpp

--- ValidateBinarySearchTree.cpp
+++ ValidateBinarySearchTree.cpp
@@ -1,101 +1,75 @@
-//============================================================================
-// Validate Binary Search Tree
-// Given a binary tree, determine if it is a valid binary search tree (BST).
-//
-// Assume a BST is defined as follows:
-//
-// The left subtree of a node contains only nodes with keys less than the
-// node's key. The right subtree of a node contains only nodes with keys
-// greater than the node's key. Both the left and right subtrees must also be
-// binary search trees.
-//============================================================================
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
 
-#include <iostream>
-#include <climits>
-using namespace std;
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
+//pre-order with updating min and max T : O(N)
+class Solution {
+public:
+    bool isValidBST(TreeNode* root){
+        long long mi = (long long)INT_MIN - 1;
+        long long mx = (long long)INT_MAX + 1;
+        return isValidBSTHelper(root, mi, mx);
+    }
+
+    bool isValidBSTHelper(TreeNode* root, long long mi, long long mx) {
+        if(!root)
+            return true;
+        if(mi < root->val && root->val < mx) 
+            return isValidBSTHelper(root->left, mi, root->val) && isValidBSTHelper(root->right, root->val, mx);
+        return false;
+    }
+};
+ 
+//in-order and check if it is no-decreasing, T : O(N)
 class Solution {
 public:
     bool isValidBST(TreeNode *root) {
-        //        return isValidBST1(root);
-        //        return isValidBST2(root);
-        return isValidBST3(root);
+        long long pre_val = (long long)INT_MIN - 1;
+        return isValidBSTHelper(root, pre_val);
     }
-
-    // pre-order walk with looking up min and max
-    // takes O(n*log(n)) time
-    bool isValidBST1(TreeNode* node) {
-        if (node == NULL)
-        	return true;
-        if (maxVal(node->left) < node->val && node->val < minVal(node->right)) {
-            return isValidBST1(node->left) && isValidBST1(node->right);
-        }
-        return false;
-    }
-
-    int minVal(TreeNode* node) {
-        if (node == NULL)
-        	return INT_MAX;
-        while (node->left != NULL)
-        	node = node->left;
-        return node->val;
-    }
-
-    int maxVal(TreeNode* node) {
-        if (node == NULL)
-        	return INT_MIN;
-        while (node->right != NULL)
-        	node = node->right;
-        return node->val;
-    }
-
-    // pre-order walk with updating min and max
-    // takes O(n) time
-    bool isValidBST2(TreeNode* node) {
-        return isValidBSTHelper2(node, INT_MIN, INT_MAX);
-    }
-
-    bool isValidBSTHelper2(TreeNode* node, int min, int max) {
-        if (node == NULL)
-        	return true;
-        if (min  < node->val && node->val < max) {
-            return isValidBSTHelper2(node->left, min, node->val)
-                    && isValidBSTHelper2(node->right, node->val, max);
-        }
-        return false;
-    }
-
-    // in-order walk and check if it is no-decreasing
-    // takes O(n) time
-    bool isValidBST3(TreeNode* node) {
-        int prev = INT_MIN;
-        return isValidBSTHelper3(node, prev);
-    }
-
-    bool isValidBSTHelper3(TreeNode* node, int &prev) {
-        if (node == NULL)
-        	return true;
-        if (isValidBSTHelper3(node->left, prev)) {
-            if (prev < node->val) {
-                prev = node->val;
-                return isValidBSTHelper3(node->right, prev);
-            }
+    
+    bool isValidBSTHelper(TreeNode *root, long long &pre_val){
+        if(!root)
+            return true;
+        bool ret;
+        ret = isValidBSTHelper(root->left, pre_val);
+        if(ret == false || pre_val >= root->val)
             return false;
-        }
-        return false;
+        pre_val = root->val;
+        ret = isValidBSTHelper(root->right, pre_val);
+        return ret;
     }
 };
-
-int main() {
-    return 0;
-}
