125fb81b2462ea530057c189806994d99ebfa244
jordandong <jordandong@gmail.com>
2015-02-21 16:41:59 -0800

    Update SubsetsII.cpp

--- SubsetsII.cpp
+++ SubsetsII.cpp
@@ -1,54 +1,48 @@
-//============================================================================
-// Subsets II
-// Given a collection of integers that might contain duplicates, S, return all
-// possible subsets.
-//
-// Note:
-// Elements in a subset must be in non-descending order.
-// The solution set must not contain duplicate subsets.
-// For example,
-// If S = [1,2,2], a solution is:
-//
-// [
-//  [2],
-//  [1],
-//  [1,2,2],
-//  [2,2],
-//  [1,2],
-//  []
-// ]
-//============================================================================
+/*
+Given a collection of integers that might contain duplicates, S, return all possible subsets.
 
-#include <vector>
-#include <algorithm>
-using namespace std;
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+
+For example,
+If S = [1,2,2], a solution is:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+
+Hide Tags Array Backtracking
+*/
 
 class Solution {
 public:
     vector<vector<int> > subsetsWithDup(vector<int> &S) {
-        int N = S.size();
-        int max = 1 << N;
+        int sz = S.size();
         vector<vector<int> > res;
-        for (int i = 0; i < max; i++) {
-            vector<int> sub;
-            int k = i;
-            int j = 0;
-            while (k > 0) {
-                if (k & 0x01)
-                	sub.push_back(S[j]);
-                k >>= 1;
-                j++;
+        int last_sz = 0;
+        int cur_sz = 0;
+        
+        res.push_back(vector<int>());
+        sort(S.begin(), S.end());
+
+        for(int i = 0; i < sz ; i++){
+            cur_sz = res.size();
+            int start = 0;
+            if(i > 0 && S[i] == S[i-1]){
+                start = last_sz; //Avoild Dup, only apply dup element to the new added solutions
             }
-            sort(sub.begin(), sub.end());
-            if (find(res.begin(), res.end(), sub) == res.end()) {
-                res.push_back(sub);
+            last_sz = cur_sz;
+            for(; start < cur_sz; start++){//add new elements based on all emelemts OR based on last sz
+                vector<int> tmp = res[start];
+                tmp.push_back(S[i]);
+                res.push_back(tmp);
             }
         }
-        sort(res.begin(), res.end());
         return res;
     }
 };
-
-int main() {
-    return 0;
-}
