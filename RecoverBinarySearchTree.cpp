8f49465ae8509ec2779581b922b78a15e04f6440
jordandong <jordandong@gmail.com>
2015-02-22 13:09:13 -0800

    Update RecoverBinarySearchTree.cpp

--- RecoverBinarySearchTree.cpp
+++ RecoverBinarySearchTree.cpp
@@ -1,9 +1,28 @@
-//============================================================================
-// Recover Binary Search Tree
-// Two elements of a binary search tree (BST) are swapped by mistake.
-//
-// Recover the tree without changing its structure.
-//============================================================================
+/*
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -14,77 +33,31 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
- /**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
- 
- 
+
  /* inorder: 1 2 3 7 5 6 4 8 9
-  * first issue 5<7,  so 7(p->l) must be wrong and 5(r) may be wrong
+  * first issue 5<7,  so 7(prev, store at l) must be wrong and 5(r) may be wrong
   * second issue 6>4, so 4(r) must be wrong
   */
 class Solution {
 public:
-    
     void recoverTree(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        TreeNode* l=NULL;
-        TreeNode* r=NULL;
-        TreeNode* p=NULL;
-        rT(root, l, r, p);
+        TreeNode* l = NULL;
+        TreeNode* r = NULL;
+        TreeNode* prev = NULL;
+        recoverTreeHelper(root, l, r, prev);
         swap(l->val, r->val);
     }
     
-    void rT(TreeNode *root, TreeNode* &l, TreeNode* &r, TreeNode* &p){
-        if(root==NULL)
+    void recoverTreeHelper(TreeNode *root, TreeNode* &l, TreeNode* &r, TreeNode* &prev){
+        if(!root)
             return;
-        rT(root->left, l, r, p);
-        if(p!=NULL && root->val<p->val){
-            if(l==NULL)
-                l=p;
-            r=root;
+        recoverTreeHelper(root->left, l, r, prev);
+        if(prev && root->val < prev->val){
+            if(!l)
+                l = prev;
+            r = root;
         }
-        p=root;
-        rT(root->right, l, r, p);
-    }
-};
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    void recoverTree(TreeNode *root) {
-        TreeNode *prev = NULL, *first = NULL, *second = NULL;
-        recoverTreeHelper(root, prev, first, second);
-        swap(first->val, second->val);
-    }
-
-    void recoverTreeHelper(TreeNode *curNode, TreeNode *&preNode, TreeNode *&first, TreeNode *&second) {
-        if (curNode == NULL)
-        	return;
-        recoverTreeHelper(curNode->left, preNode, first, second);
-        if (preNode && preNode->val > curNode->val) {
-            if (first == NULL)
-            	first = preNode;
-            second = curNode;
-        }
-        preNode = curNode;
-        recoverTreeHelper(curNode->right, preNode, first, second);
+        prev = root;
+        recoverTreeHelper(root->right, l, r, prev);
     }
 };
