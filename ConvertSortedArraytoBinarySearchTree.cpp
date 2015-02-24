6c9326b4491b008d7947f7062aa6a2d70a7ae672
jordandong <jordandong@gmail.com>
2015-02-23 21:56:29 -0800

    Update ConvertSortedArraytoBinarySearchTree.cpp

--- ConvertSortedArraytoBinarySearchTree.cpp
+++ ConvertSortedArraytoBinarySearchTree.cpp
@@ -1,48 +1,8 @@
-//============================================================================
-// Convert Sorted Array to Binary Search Tree
-//
-// Given an array where elements are sorted in ascending order, convert it to
-// a height balanced BST.
-//
-//============================================================================
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    TreeNode *sortedArrayToBST(vector<int> &num) {
-        return sortedArrayToBSTHelper(num, 0, num.size() - 1);
-    }
-
-    // pre-order construct tree, takes O(n) time
-    TreeNode* sortedArrayToBSTHelper(vector<int> &num, int start, int end) {
-        if (start > end)
-        	return NULL;
-        int mid = start + (end - start) / 2;
-        TreeNode* node = new TreeNode(num[mid]);
-        node->left = sortedArrayToBSTHelper(num, start, mid - 1);
-        node->right = sortedArrayToBSTHelper(num, mid + 1, end);
-        return node;
-    };
-};
-
-int main() {
-    return 0;
-}
-
-
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -53,22 +13,20 @@ int main() {
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode *sortedArrayToBST(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        return sA(num, 0, num.size()-1);
-        
+        return sortedArrayToBSTHelper(num, 0, num.size() - 1);
     }
-    TreeNode *sA(vector<int> &data, int left, int right){
-        if(left>right)
+    
+    TreeNode* sortedArrayToBSTHelper(vector<int> &num, int s, int e){
+        if(e < s)
             return NULL;
-        
-        int mid = (left + right+1)/2;
-        TreeNode *t = new TreeNode(data[mid]);
-        t->left = sA(data, left, mid-1);
-        t->right = sA(data, mid+1, right);
-        return t;
+        int mid = s + (e - s)/2;
+        TreeNode* node = new TreeNode(num[mid]);
+        node->left = sortedArrayToBSTHelper(num, s, mid - 1);
+        node->right = sortedArrayToBSTHelper(num, mid + 1, e);
+        return node;
     }
 };
