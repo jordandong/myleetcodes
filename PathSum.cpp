ee5ebe176303f559700200bdc6f7824d9eb34dfa
jordandong <jordandong@gmail.com>
2015-02-23 22:29:43 -0800

    Update PathSum.cpp

--- PathSum.cpp
+++ PathSum.cpp
@@ -1,53 +1,19 @@
-//============================================================================
-// Path Sum
-// Given a binary tree and a sum, determine if the tree has a root-to-leaf path
-// such that adding up all the values along the path equals the given sum.
-//
-// For example:
-// Given the below binary tree and sum = 22,
-//              5
-//             / \
-//            4   8
-//           /   / \
-//          11  13  4
-//         /  \      \
-//        7    2      1
-// return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
-//============================================================================
+/*
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
-#include <iostream>
-using namespace std;
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    bool hasPathSum(TreeNode *root, int sum) {
-        return pathSumHelper(root, sum);
-    }
-
-    bool pathSumHelper(TreeNode* node, int sum) {
-        if (node == NULL)
-			return false;
-        sum -= node->val;
-        if (NULL == node->left && NULL == node->right)
-            return (sum == 0);
-        return pathSumHelper(node->left, sum) || pathSumHelper(node->right, sum);
-    }
-};
-
-int main() {
-    return 0;
-}
-
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -61,28 +27,11 @@ int main() {
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(root==NULL)
+        if(!root)
             return false;
-
-        if(root->left!=NULL){
-            if(hasPathSum(root->left, sum - root->val))
-                return true;
-        }
-        
-        if(root->right!=NULL){
-            if(hasPathSum(root->right, sum - root->val))
-                return true;
-        }
-        
-        if(root->left==NULL&&root->right==NULL){
-            if(sum-root->val==0)
-                return true;
-            else
-                return false;
-        }
-        
-        return false;
+        sum -= root->val;
+        if(!root->left && !root->right)
+            return sum == 0;
+        return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
     }
 };
