6639c3753014f9fbe0ffd17d31399eeaff3c51a2
jordandong <jordandong@gmail.com>
2015-02-23 22:19:42 -0800

    Update MinimumDepthofBinaryTree.cpp

--- MinimumDepthofBinaryTree.cpp
+++ MinimumDepthofBinaryTree.cpp
@@ -1,12 +1,10 @@
-//============================================================================
-// Minimum Depth of Binary Tree
-//
-// Given a binary tree, find its minimum depth.
-//
-// The minimum depth is the number of nodes along the shortest path from the
-// root node down to the nearest leaf node.
-//============================================================================
+/*
+Given a binary tree, find its minimum depth.
 
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -18,7 +16,7 @@
  * };
  */
  
- class Solution {
+class Solution {
 public:
     int minDepth(TreeNode *root) {
         if(!root)
@@ -26,31 +24,8 @@ public:
         int nl = minDepth(root->left);
         int nr = minDepth(root->right);
         if(nl == 0 || nr == 0)
-            return (nl>nr?nl:nr) + 1;
+            return (nl > nr ? nl : nr) + 1;
         else
             return min(nl, nr) + 1;
-    }
-};
- 
- 
-class Solution {
-public:
-    int minDepth(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(root==NULL)
-            return 0;
-        else if(root->left==NULL&&root->right==NULL)
-            return 1;
-        return min(mD(root->left), mD(root->right))+1;
-    }
-    
-    int mD(TreeNode *root){
-        if(root==NULL)
-            return INT_MAX;
-        if(root->left==NULL&&root->right==NULL)
-            return 1;
-            
-        return min(mD(root->left), mD(root->right))+1;
     }
 };
