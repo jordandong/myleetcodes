49a851555e42213a8148c4d14153efeb012103ca
jordandong <jordandong@gmail.com>
2015-02-23 22:02:45 -0800

    Update ConvertSortedListtoBinarySearchTree.cpp

--- ConvertSortedListtoBinarySearchTree.cpp
+++ ConvertSortedListtoBinarySearchTree.cpp
@@ -1,64 +1,8 @@
-//============================================================================
-// Convert Sorted List to Binary Search Tree
-//
-// Given a singly linked list where elements are sorted in ascending order,
-// convert it to a height balanced BST.
-//
-//============================================================================
+/*
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 
-#include <iostream>
-using namespace std;
-
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    TreeNode *sortedListToBST(ListNode *head) {
-        if (head == NULL)
-  	      	return NULL;
-        ListNode* node = head;
-  	      int len = 1;
-        while (NULL != (node = node->next))
-  	      	len++;
-        return sortedListToBSTHelper(head, 0, len - 1);
-    }
-
-    // in-order build tree, takes O(n) time
-    TreeNode* sortedListToBSTHelper(ListNode*& head, int start, int end) {
-        if (start > end)
-  	      	return NULL;
-        int mid = start + (end - start) / 2;
-        TreeNode* leftTree = sortedListToBSTHelper(head, start, mid - 1);
-        TreeNode* root = new TreeNode(head->val);
-        root->left = leftTree;
-        head = head->next;
-        root->right = sortedListToBSTHelper(head, mid + 1, end);
-        return root;
-    };
-};
-
-int main() {
-    return 0;
-}
-
-
+Hide Tags Depth-first Search Linked List
+*/
 
 /**
  * Definition for singly-linked list.
@@ -77,31 +21,30 @@ int main() {
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode *sortedListToBST(ListNode *head) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(head==NULL)
-            return NULL;
-            
-        vector<int> v;
-        while(head!=NULL){
-            v.push_back(head->val);
-            head=head->next;
+        // write your code here
+        int len = 0;
+        ListNode* tmp = head;
+        while(tmp){
+            len++;
+            tmp = tmp->next;
         }
-        
-        return sA(v, 0, v.size()-1);
+        return sortedListToBSTHelper(1, len, head);
     }
     
-    TreeNode *sA(vector<int> &data, int left, int right){
-        if(left>right)
+    TreeNode* sortedListToBSTHelper(int start, int end, ListNode* &head){
+        if(start > end)
             return NULL;
-        
-        int mid = (left + right+1)/2;
-        TreeNode *t = new TreeNode(data[mid]);
-        t->left = sA(data, left, mid-1);
-        t->right = sA(data, mid+1, right);
-        return t;
-    }   
+        int mid = start + (end - start)/2;
+        TreeNode* left = sortedListToBSTHelper(start, mid - 1, head);
+        TreeNode* cur = new TreeNode(head->val);
+        head = head->next;
+        TreeNode* right = sortedListToBSTHelper(mid + 1, end, head);
+        cur->left = left;
+        cur->right = right;
+        return cur;
+    }
 };
