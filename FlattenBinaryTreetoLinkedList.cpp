1271d74cfb9eb4781fd06a029c9b46ce09e75adf
jordandong <jordandong@gmail.com>
2015-02-24 21:53:16 -0800

    Update FlattenBinaryTreetoLinkedList.cpp

--- FlattenBinaryTreetoLinkedList.cpp
+++ FlattenBinaryTreetoLinkedList.cpp
@@ -1,102 +1,91 @@
-//============================================================================
-// Flatten Binary Tree to Linked List
-// Given a binary tree, flatten it to a linked list in-place.
-//
-// For example,
-// Given
-//
-//         1
-//        / \
-//       2   5
-//      / \   \
-//     3   4   6
-// The flattened tree should look like:
-//   1
-//    \
-//     2
-//      \
-//       3
-//        \
-//         4
-//          \
-//           5
-//            \
-//             6
-// Hints:
-// If you notice carefully in the flattened tree, each node's right child
-// points to the next node of a pre-order traversal.
-//============================================================================
+/*
+Given a binary tree, flatten it to a linked list in-place.
 
-#include <iostream>
-#include <stack>
-using namespace std;
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+
+Hints:
+If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        TreeNode* cur = new TreeNode(-1);
+        TreeNode* dummy = cur;
+        flattenHelper(root, cur);
+        delete dummy;
+    }
+    
+    void flattenHelper(TreeNode *root, TreeNode* &cur) {
+        if(!root)
+            return;
+        TreeNode* leftChild = root->left;
+        TreeNode* rightChild = root->right;
+        cur->right = root;
+        cur = cur->right;
+        cur->left = NULL;
+        cur->right = NULL;
+        flattenHelper(leftChild, cur);
+        flattenHelper(rightChild, cur);
+    }
 };
 
 class Solution {
 public:
     void flatten(TreeNode *root) {
-        return flatten1(root);
-		//return flatten2(root);
-    }
-
-    void flatten1(TreeNode *root) {
-        TreeNode* res = NULL;
-        flattenHelper1(root, res);
-    }
-
-    void flattenHelper1(TreeNode *node, TreeNode *&res) {
-        if(node == NULL)
-			return;
-        TreeNode* leftSub = node->left;
-        node->left = NULL;
-        TreeNode* rightSub = node->right;
-        node->right = NULL;
-        if (res == NULL)
-			res = node;
-        else{
-			res->right = node;
-			res = res->right;
-		}
-        flattenHelper1(leftSub, res);
-        flattenHelper1(rightSub, res);
-    }
-
-    void flatten2(TreeNode *root) {
-        if (root == NULL)
-			return;
-        TreeNode *res = NULL;
+        if(!root)
+            return;
+        TreeNode *cur = NULL;
         stack<TreeNode*> stk;
         stk.push(root);
-        while (!stk.empty()) {
-            TreeNode* top = stk.top();
+        while(stk.size()) {
+            TreeNode* t = stk.top();
             stk.pop();
-            if (top->right){
-				stk.push(top->right);
-				top->right = NULL;
-			}
-            if (top->left){
-				stk.push(top->left);
-				top->left = NULL;
-			}
-            if (res == NULL)
-				res = top;
-            else{
-				res->right = top;
-				res = res->right;
-			}
+            if(t->right){
+                stk.push(t->right);
+                t->right = NULL;
+            }
+            if(t->left){
+            	stk.push(t->left);
+            	t->left = NULL;
+            }
+            if(!cur){
+            	cur = t;
+            }else{
+            	cur->right = t;
+            	cur = cur->right;
+            }
         }
     }
 };
-
-int main() {
-    return 0;
-}
