b457face5192b6947ec9ac66f976fe5638060217
jordandong <jordandong@gmail.com>
2015-02-24 23:04:55 -0800

    Update PopulatingNextRightPointersinEachNodeII.cpp

--- PopulatingNextRightPointersinEachNodeII.cpp
+++ PopulatingNextRightPointersinEachNodeII.cpp
@@ -1,113 +1,99 @@
-//============================================================================
-// Populating Next Right Pointers in Each Node II
-// 
-// Follow up for problem "Populating Next Right Pointers in Each Node".
-//
-// What if the given tree could be any binary tree? Would your previous 
-// solution still work?
-//
-// Note:
-//
-// You may only use constant extra space.
-// 
-// For example, given the following perfect binary tree:
-//  "     1     "
-//  "    / \    "
-//  "   2   3   "
-//  "  / \   \  "
-//  " 4  5    7 "
-// After calling your function, the tree should look like:
-//  "      1 -> NULL      "
-//  "     /  \            "
-//  "    2 -> 3 -> NULL   "
-//  "   / \    \          "
-//  "  4-> 5 -> 7 -> NULL "
-//============================================================================
+/*
+Follow up for problem "Populating Next Right Pointers in Each Node".
 
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+
+Hide Tags Tree Depth-first Search
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+ 
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
         if(!root)
             return;
         queue<TreeLinkNode*> q[2];
-        int i=0;
-        q[i].push(root);
-        while(!q[i].empty()){
-            TreeLinkNode* t = q[i].front();
-            q[i].pop();
+        int c = 0;
+        q[c].push(root);
+        while(q[c].size()){
+            TreeLinkNode* t = q[c].front();
+            q[c].pop();
             if(t->left)
-                q[i^1].push(t->left);
+                q[c^1].push(t->left);
             if(t->right)
-                q[i^1].push(t->right);
-            if(q[i].empty()){
+                q[c^1].push(t->right);
+            if(q[c].empty()){
                 t->next = NULL;
-                i^=1;
+                c ^= 1;
             }else{
-                t->next = q[i].front();
+                t->next = q[c].front();
             }
         }
     }
 };
 
-#include <iostream>
-using namespace std;
-/**
- * Definition for binary tree with next pointer.
- */
-struct TreeLinkNode 
-{
-    int val;
-    TreeLinkNode *left, *right, *next;
-    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
-
-class Solution 
-{
+//BFS
+class Solution {
 public:
-    void connect(TreeLinkNode *root)
-    {
-        connect1(root);
+    void connect(TreeLinkNode *root){
+        connectHelper(root);
     }
-
-    void connect1(TreeLinkNode *curNode)
-    {
-        if (!curNode)
+    
+    void connectHelper(TreeLinkNode *root){
+        if(!root)
         	return;
         TreeLinkNode* nextHead = NULL;
-        while (curNode)
-        {
-            if(curNode->left){
-                curNode->left->next = getNextSibling(curNode, true);
-                if (!nextHead)
-                	nextHead = curNode->left;
+        while(root){
+            if(root->left){
+                root->left->next = getNextSibling(root, true);
+                if(!nextHead)
+                	nextHead = root->left;
             }
-            if (curNode->right){
-                curNode->right->next = getNextSibling(curNode, false);
-                if (!nextHead)
-                	nextHead = curNode->right;
+            if (root->right){
+                root->right->next = getNextSibling(root, false);
+                if(!nextHead)
+                    nextHead = root->right;
             }
-            curNode = curNode->next;
+            root = root->next;
         }
-        connect1(nextHead);
+        connectHelper(nextHead);
     }
 
-    TreeLinkNode* getNextSibling(TreeLinkNode *curNode, bool isLeft)
-    {
+    TreeLinkNode* getNextSibling(TreeLinkNode *curNode, bool isLeft){
         if (isLeft)
             if (curNode->right)
             	return curNode->right;
-        while((curNode=curNode->next))
-        {
-            if (curNode->left)
+        while((curNode = curNode->next)){
+            if(curNode->left)
             	return curNode->left;
-            if (curNode->right)
+            if(curNode->right)
             	return curNode->right;
         }
         return NULL;
     }
 };
-
-int main()
-{
-    return 0;
-}
