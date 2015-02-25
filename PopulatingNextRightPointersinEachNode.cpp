8b7a84537b1bf2b82070abe3c55eeae6fb980f9e
jordandong <jordandong@gmail.com>
2015-02-24 22:51:19 -0800

    Update PopulatingNextRightPointersinEachNode.cpp

--- PopulatingNextRightPointersinEachNode.cpp
+++ PopulatingNextRightPointersinEachNode.cpp
@@ -1,102 +1,91 @@
-//============================================================================
-// Populating Next Right Pointers in Each Node
-// 
-// Given a binary tree
-//
-// struct TreeLinkNode {
-//     TreeLinkNode *left;
-//     TreeLinkNode *right;
-//     TreeLinkNode *next;
-// }
-//
-// Populate each next pointer to point to its next right node. If there is no 
-// next right node, the next pointer should be set to NULL.
-//
-// Initially, all next pointers are set to NULL.
-//
-// Note:
-//
-// You may only use constant extra space.
-// You may assume that it is a perfect binary tree (ie, all leaves are at the 
-// same level, and every parent has two children).
-// 
-// For example, given the following perfect binary tree:
-//  "     1     "
-//  "    / \    "
-//  "   2   3   "
-//  "  / \ / \  "
-//  " 4  5 6  7 "
-// After calling your function, the tree should look like:
-//  "      1 -> NULL      "
-//  "     /  \            "
-//  "    2 -> 3 -> NULL   "
-//  "   / \  / \          "
-//  "  4->5->6->7 -> NULL "
-//============================================================================
+/*
+Given a binary tree
 
-#include <iostream>
-using namespace std;
+    struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+For example,
+Given the following perfect binary tree,
+         1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+
+Hide Tags Tree Depth-first Search
+*/
+
 /**
  * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
  */
-struct TreeLinkNode 
-{
-    int val;
-    TreeLinkNode *left, *right, *next;
-    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
-
-class Solution 
-{
+ 
+//DFS
+class Solution {
 public:
-    void connect(TreeLinkNode *root)
-    {
-        //connect1(root);
-        connect2(root);
-    }
-
-    void connect1(TreeLinkNode *node)
-    {
-        if(node)
-			node->next = NULL;
-        connectHelper1(node);
-    }
-
-    // BFS
-    void connectHelper1(TreeLinkNode* node) {
-        if (!node || !(node->left) || !(node->right))
-        	return;
-        
-        TreeLinkNode* curr = node;
-        while (curr) {
-            if(curr->left)
-            	curr->left->next = curr->right;
-            if(curr->right)
-            	curr->right->next = (curr->next) ? curr->next->left : NULL;
-            curr = curr->next;
-        }   
-        connectHelper1(node->left);
-    }   
-
-    void connect2(TreeLinkNode *node)
-    {
-        if (node)
-        	node->next = NULL;
-        connectHelper2(node);
+    void connect(TreeLinkNode *root) {
+        if(!root)
+            return;
+        root->next = NULL;
+        connectHelper(root);
     }
     
-    // DFS
-    void connectHelper2(TreeLinkNode* node) {
-        if (!node || !(node->left) || !(node->right))
-        	return;
-        node->left->next = node->right;
-        node->right->next = (node->next) ? node->next->left: NULL;
-        connectHelper2(node->left);
-        connectHelper2(node->right);
+    void connectHelper(TreeLinkNode *root){
+        if(!root)
+            return;
+        if(root->left){
+            root->left->next = root->right?root->right:NULL;
+        }
+        if(root->right){
+            root->right->next = root->next?root->next->left:NULL;
+        }
+        connectHelper(root->left);
+        connectHelper(root->right);
     }
 };
 
-int main()
-{
-    return 0;    
-}
+//BFS
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(!root)
+            return;
+        root->next = NULL;
+        connectHelper(root);
+    }
+    
+    void connectHelper(TreeLinkNode *root){
+        if(!root->left || !root->right)
+            return;
+        
+        TreeLinkNode* cur = root;
+        while(cur){
+            if(cur->left)
+            	cur->left->next = cur->right;
+            if(cur->right)
+            	cur->right->next = (cur->next) ? cur->next->left : NULL;
+            cur = cur->next;
+        }   
+        connectHelper(root->left);
+    }
+};
