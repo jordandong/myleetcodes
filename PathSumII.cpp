42368a622c611142bae844b9c761db22d07932d9
jordandong <jordandong@gmail.com>
2015-02-24 21:22:24 -0800

    Update PathSumII.cpp

--- PathSumII.cpp
+++ PathSumII.cpp
@@ -1,68 +1,22 @@
-//============================================================================
-// Path Sum II
-// Given a binary tree and a sum, find all root-to-leaf paths where each
-// path's sum equals the given sum.
-//
-// For example:
-// Given the below binary tree and sum = 22,
-//              5
-//             / \
-//            4   8
-//           /   / \
-//          11  13  4
-//         /  \    / \
-//        7    2  5   1
-// return
-// [
-//   [5,4,11,2],
-//   [5,8,4,5]
-// ]
-//============================================================================
+/*
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    vector<vector<int> > pathSum(TreeNode *root, int sum) {
-        vector<int> path;
-        vector<vector<int> > res;
-        pathSumHelper(root, sum, path, res);
-        return res;
-    }
-
-    void pathSumHelper(TreeNode* node, int sum, vector<int> path, vector<vector<int> > &res) {
-        if (node == NULL)
-        	return;
-        sum -= node->val;
-        path.push_back(node->val);
-        
-        if (sum == 0 && NULL == node->left && NULL == node->right) {
-            res.push_back(path);
-            return;
-        }
-        pathSumHelper(node->left, sum, path, res);
-        pathSumHelper(node->right, sum, path, res);
-    }
-};
-
-int main() {
-    return 0;
-}
-
-
-
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -76,36 +30,24 @@ int main() {
 class Solution {
 public:
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        vector<int> sol;
         vector<vector<int> > res;
-        vector<int> path;
-        pS(root, sum, res, path);
+        pathSumHelper(root, sum, sol, res);
         return res;
     }
     
-    void pS(TreeNode *root, int sum, vector<vector<int> > &res, vector<int> &path){
-        if(root==NULL)
+    void pathSumHelper(TreeNode *root, int sum, vector<int> &sol, vector<vector<int> > &res) {
+        if(root == NULL)
             return;
             
-        if(root->left!=NULL){
-            path.push_back(root->val);
-            pS(root->left, sum - root->val, res, path);
-            path.pop_back();
-        }
+        sum -= root->val;
+        sol.push_back(root->val);
         
-        if(root->right!=NULL){
-            path.push_back(root->val);
-            pS(root->right, sum - root->val, res, path);
-            path.pop_back();
-        }
+        if(!root->left && !root->right && sum == 0)
+            res.push_back(sol);
         
-        if(root->left==NULL&&root->right==NULL){
-            if(sum - root->val==0){
-                path.push_back(root->val);
-                res.push_back(path);
-                path.pop_back();
-            }
-        }
+        pathSumHelper(root->left, sum, sol, res);
+        pathSumHelper(root->right, sum, sol, res);
+        sol.pop_back();
     }
 };
