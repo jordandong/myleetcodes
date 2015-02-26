360ecd600a30a428d015d4117199697ceb4a63df
jordandong <jordandong@gmail.com>
2015-02-25 22:11:14 -0800

    Update WordLadder.cpp

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -1,4 +1,5 @@
-/*Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
+/*
+Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
 
 Only one letter can be changed at a time
 Each intermediate word must exist in the dictionary
@@ -13,48 +14,82 @@ return its length 5.
 
 Note:
 Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+
+Hide Tags Breadth-first Search
 */
 
 class Solution {
 public:
     int ladderLength(string start, string end, unordered_set<string> &dict) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function    
-        int size=dict.size();
-        int res=1;
         stack<string> stk[2];
-        unordered_set<string> used;
-        int index=0;
-        stk[index%2].push(start);
-        used.insert(start);
+        int cnt = 1;
+        if(start == end)
+            return cnt;
         
-        while(!stk[index%2].empty()){
-            if(res == size+2)
-                return 0;
-            string current=stk[index%2].top();
-            stk[index%2].pop();
-            int N=current.length();
-            for(int i=0;i<N;i++){
-               for(char s='a';s<='z';s++){
-                    if(current[i]!=s){
-                        string tmp = current;
-                        tmp[i]=s;
-                        if(!tmp.compare(end)){
-                            res++;
-                            return res;
+        stk[cnt%2].push(start);
+        dict.erase(start);//if don't erase dict, then need to record visited words
+        while(stk[cnt%2].size()){
+            string cur_str = stk[cnt%2].top();
+            stk[cnt%2].pop();
+
+            for(int i = 0; i < cur_str.length(); i++){
+                char t = cur_str[i];
+                for(char c = 'a'; c <= 'z'; c++){
+                    if(c != t){
+                        cur_str[i] = c;
+                        if(cur_str == end)
+                            return cnt + 1;
+                        if(dict.find(cur_str) != dict.end()){
+                            stk[(cnt + 1)%2].push(cur_str);
+                            dict.erase(cur_str);//if don't erase dict, then need to record visited words
                         }
-                    
-                        if(dict.find(tmp)!=dict.end()&&used.find(tmp)==used.end()){
-                            stk[(index+1)%2].push(tmp);
-                            used.insert(tmp);
-                        }
+                        cur_str[i] = t;
                     }
                 }
             }
-            if(stk[index%2].empty()){
-                index=index+1;
-                res++;
+            
+            if(stk[cnt%2].size() == 0)
+                cnt++;
+        }
+        return 0;
+    }
+};
+
+class Solution {
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        stack<string> stk[2];
+        int cnt = 1;
+        unordered_map<string, bool> mp;
+        if(start == end)
+            return cnt;
+        
+        stk[cnt%2].push(start);
+        mp[start] = true;
+        while(stk[cnt%2].size()){
+            string cur_str = stk[cnt%2].top();
+            stk[cnt%2].pop();
+
+            for(int i = 0; i < cur_str.length(); i++){
+                char t = cur_str[i];
+                for(char c = 'a'; c <= 'z'; c++){
+                    if(c != t){
+                        cur_str[i] = c;
+                        if(cur_str == end)
+                            return cnt + 1;
+                        if(mp.find(cur_str) == mp.end() && dict.find(cur_str) != dict.end()){
+                            stk[(cnt + 1)%2].push(cur_str);
+                            mp[cur_str] = true;
+                        }
+                        cur_str[i] = t;
+                    }
+                }
             }
+            
+            if(stk[cnt%2].size() == 0)
+                cnt++;
         }
         return 0;
     }
