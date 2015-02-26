18994de8ca684a73aa562f5e8e8c128aa9549593
jordandong <jordandong@gmail.com>
2015-02-25 21:45:31 -0800

    Update BinaryTreeMaximumPathSum.cpp

--- BinaryTreeMaximumPathSum.cpp
+++ BinaryTreeMaximumPathSum.cpp
@@ -1,71 +1,18 @@
-//============================================================================
-// Binary Tree Maximum Path Sum
-// Given a binary tree, find the maximum path sum.
-//
-// The path may start and end at any node in the tree.
-//
-// For example:
-// Given the below binary tree,
-//
-//  "     1     "
-//  "    / \    "
-//  "   2   3   " 
-//  
-//  Return 6.
-//
-//============================================================================
+/*
+Given a binary tree, find the maximum path sum.
 
-#include <iostream>
-using namespace std;
+The path may start and end at any node in the tree.
 
-/**
- * Definition for binary tree
- */ 
-struct TreeNode 
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+For example:
+Given the below binary tree,
 
-class Solution 
-{
-public:
-    int maxPathSum(TreeNode *root) 
-    {
-        if (root == NULL)
-        	return 0;
-        int maxSoFar = root->val;
-        maxPathSumHelper(root, maxSoFar);
-        return maxSoFar;
-    }
+       1
+      / \
+     2   3
+Return 6.
 
-    int maxPathSumHelper(TreeNode *node, int &maxSoFar)
-    {
-        if (node == NULL)
-        	return 0;
-        int leftMax = maxPathSumHelper(node->left, maxSoFar);
-        int rightMax = maxPathSumHelper(node->right, maxSoFar);
-        int maxPath = node->val;
-        if (leftMax > 0)
-        	maxPath += leftMax;
-        if (rightMax > 0)
-        	maxPath += rightMax;
-        if (maxPath > maxSoFar)
-        	maxSoFar = maxPath; 
-        int res = node->val;
-        return max(res, res+max(leftMax, rightMax));
-    }
-};
-
-int main()
-{
-    return 0;
-}
-
-
-
+Hide Tags Tree Depth-first Search
+*/
 
 /**
  * Definition for binary tree
@@ -79,55 +26,20 @@ int main()
 class Solution {
 public:
     int maxPathSum(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int maxval=INT_MIN/3;
-        mPT(root, maxval);
-        return maxval;
-        
+        long long res = (long long)INT_MIN - 1;
+        maxPathSumHelper(root, res);
+        return (int)res;
     }
     
-    int mPT(TreeNode *root, int& maxval) {
-        if(root==NULL)
-            return INT_MIN/3;
-        int left = mPT(root->left, maxval);
-        int right = mPT(root->right, maxval);
-
-        int rsum = right + root->val;
-        int lsum = left + root->val;
-        int sum = right + left + root->val;        
-        maxval = max(max(max(max(max(max(right,left),rsum),lsum),sum),root->val),maxval);
-        return max(max(rsum,lsum),root->val);
+    int maxPathSumHelper(TreeNode *root, long long &res) {
+        if(!root)
+            return 0;
+        int left = maxPathSumHelper(root->left, res);
+        int right = maxPathSumHelper(root->right, res);
+        int sum = root->val;
+        sum += max(left, 0);
+        sum += max(right, 0);
+        res = res > sum ? res : sum;
+        return max(root->val + max(left, right), root->val);
     }
 };
-
-
-class Solution {
-public:
-    int maxPathSum(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int maxval=INT_MIN/2;
-        mPT(root, maxval);
-        return maxval;
-        
-    }
-    
-    int mPT(TreeNode *root, int& maxval) {
-        if(root==NULL)
-            return INT_MIN/2;
-        int left = mPT(root->left, maxval);
-        int right = mPT(root->right, maxval);
-        int sum = root->val;        
-
-	if(left>0)
-		 sum+=left;
-	if(right>0)
-		sum+=right;
-	if(sum>maxval)
-		maxval=sum;
-        
-        return max(root->val+max(left, right), root->val);
-    }
-};
-
