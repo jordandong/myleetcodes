b7da34df9e95c8a07debbeec90217ac00bc3fbe8
jordandong <jordandong@gmail.com>
2015-02-26 20:53:32 -0800

    Update PalindromePartitioning.cpp

--- PalindromePartitioning.cpp
+++ PalindromePartitioning.cpp
@@ -1,4 +1,4 @@
-/**
+/*
 Given a string s, partition s such that every substring of the partition is a palindrome.
 
 Return all possible palindrome partitioning of s.
@@ -10,42 +10,42 @@ Return
     ["aa","b"],
     ["a","a","b"]
   ]
-**/
+  
+Hide Tags Backtracking
+*/
 
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<string> curr;
+        vector<string> sol;
         vector<vector<string> > res;
         if(s.length())
-            partition_one(s, curr, res);
+            partitionHelper(s, 0, sol, res);
         return res;
        
     }
     
-    void partition_one(string s, vector<string> &curr, vector<vector<string> > &res){
-        int len = s.length();
-        if (len==0){
-            res.push_back(curr);
+    void partitionHelper(string &s, int start, vector<string> &sol, vector<vector<string> > &res){
+        if(start == s.length()){
+            res.push_back(sol);
             return;
         }
-        for(int i=1;i<=len;i++){
-            string tmp = s.substr(0,i);
-            if(judge(tmp)){
-                curr.push_back(tmp);
-                partition_one(s.substr(i), curr, res);
-                curr.pop_back();
+
+        for(int i = 1; i <= s.length() - start; i++){
+            string tmp = s.substr(start, i);
+            if(isPalindrome(tmp)){
+                sol.push_back(tmp);
+                partitionHelper(s, start + i, sol, res);
+                sol.pop_back();
             }
         }
     }
     
-    bool judge(string s){
-        int j = s.length()-1;
-        int i=0;
-        while(i<j){
-            if(s[i]!=s[j])
+    bool isPalindrome(string &s){
+        int j = s.length() - 1;
+        int i = 0;
+        while(i < j){
+            if(s[i] != s[j])
                 return false;
             i++;
             j--;
