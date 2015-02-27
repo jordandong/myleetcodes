467b5cc50182a9a0d42d458cc9562df64bb2a6e3
jordandong <jordandong@gmail.com>
2015-02-26 22:06:21 -0800

    Update WordBreak.cpp

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -6,26 +6,25 @@ s = "leetcode",
 dict = ["leet", "code"].
 
 Return true because "leetcode" can be segmented as "leet code".
+
+Hide Tags Dynamic Programming
 */
 
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int size = s.size();
-        bool dp[size+1];
-        for(int i =0; i<=size; i++)
-            dp[i]=false;
+        int sz = s.length();
+        bool dp[sz + 1];//at length i, word break is true/false
+        memset(dp, 0, (sz+1)*sizeof(bool));
         dp[0] = true;
-        
-        for(int j=0; j<size; j++){
-            for(int k=j; k>=0; k--){
-                if(dict.find(s.substr(k,j-k+1))!=dict.end()&&dp[k]){
-                    dp[j+1] = true;
+        for(int i = 0; i < sz; i++){
+            for(int j = i; j >= 0; j--){
+                if(dict.find(s.substr(j, i - j + 1)) != dict.end() && dp[j]==true){
+                    dp[i + 1] = true;
                     break;
                 }
             }
         }
-        return dp[size];
+        return dp[sz];
     }
 };
