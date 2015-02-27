1291e03bc3c1e1bd5c195baf097aeb12fb64475a
jordandong <jordandong@gmail.com>
2015-02-26 22:07:18 -0800

    Update WordBreak.cpp

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -15,11 +15,11 @@ public:
     bool wordBreak(string s, unordered_set<string> &dict) {
         int sz = s.length();
         bool dp[sz + 1];//at length i, word break is true/false
-        memset(dp, 0, (sz+1)*sizeof(bool));
+        memset(dp, 0, (sz + 1)*sizeof(bool));
         dp[0] = true;
         for(int i = 0; i < sz; i++){
             for(int j = i; j >= 0; j--){
-                if(dict.find(s.substr(j, i - j + 1)) != dict.end() && dp[j]==true){
+                if(dict.find(s.substr(j, i - j + 1)) != dict.end() && dp[j]){
                     dp[i + 1] = true;
                     break;
                 }
