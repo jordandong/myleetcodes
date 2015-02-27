739d888dcf7f1e373064c89ea01d0d8e7f2285e6
jordandong <jordandong@gmail.com>
2015-02-26 21:40:37 -0800

    Update SingleNumber.cpp

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -3,16 +3,16 @@ Given an array of integers, every element appears twice except for one. Find tha
 
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Hide Tags Hash Table Bit Manipulation
 */
 
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int rtn = 0;
-        for(int i=0; i<n; i++){
-            rtn = rtn^A[i];
-        }
-        return rtn;
+        int res = 0;
+        for(int i = 0; i < n; i++)
+            res ^= A[i];
+        return res;
     }
 };
