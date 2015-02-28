a35c294b7dc522689087795c74bc85fa937adcdf
jordandong <jordandong@gmail.com>
2015-02-27 22:46:49 -0800

    Update BinaryTreePreorderTraversal.cpp

--- BinaryTreePreorderTraversal.cpp
+++ BinaryTreePreorderTraversal.cpp
@@ -11,6 +11,8 @@ Given binary tree {1,#,2,3},
 return [1,2,3].
 
 Note: Recursive solution is trivial, could you do it iteratively?
+
+Hide Tags Tree Stack
 */
 
 /**
@@ -22,56 +24,56 @@ Note: Recursive solution is trivial, could you do it iteratively?
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode *root) {
-        vector<int> rtn;
+        vector<int> res;
         stack<TreeNode*> stk;
         stk.push(root);
-        while(!stk.empty()){
+        while(stk.size()){
             TreeNode* cur = stk.top();
             stk.pop();
             if(!cur)
                 continue;
-            rtn.push_back(cur->val);
+            res.push_back(cur->val);
             stk.push(cur->right);
             stk.push(cur->left);
         }
-        return rtn;
+        return res;
     }
 };
-
 
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode *root) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        vector<int> s;
-        //preorderHelper(root, s);
-        preorderHelper1(root, s);
-        return s;
-    }
-    
-    void preorderHelper(TreeNode *root, vector<int> &s){
-        if(!root)
-            return;
-        s.push_back(root->val);
-        preorderHelper(root->left, s);
-        preorderHelper(root->right, s);
-    }
-    
-    void preorderHelper1(TreeNode *root, vector<int> &s){
+        vector<int> res;
         stack<TreeNode*> stk;
-        while(!stk.empty() || root){
+        while(stk.size() || root){
             while(root){
-                s.push_back(root->val);
+                res.push_back(root->val);
                 stk.push(root->right);
-                root=root->left;
+                root = root->left;
             }            
             root = stk.top();
             stk.pop();
         }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> res;
+        preorderTraversalHelper(root, res);
+        return res;
+    }
+    
+    void preorderTraversalHelper(TreeNode *root, vector<int> &res){
+        if(!root)
+            return;
+        res.push_back(root->val);
+        preorderTraversalHelper(root->left, res);
+        preorderTraversalHelper(root->right, res);
     }
 };
