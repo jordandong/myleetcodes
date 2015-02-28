c8ba0f30a0b036cb04cce0c2b7975e5d9defd700
jordandong <jordandong@gmail.com>
2015-02-27 22:35:55 -0800

    Update ReorderList.cpp

--- ReorderList.cpp
+++ ReorderList.cpp
@@ -6,6 +6,8 @@ You must do this in-place without altering the nodes' values.
 
 For example,
 Given {1,2,3,4}, reorder it to {1,4,2,3}.
+
+Hide Tags Linked List
 */
 
 /**
@@ -16,22 +18,66 @@ Given {1,2,3,4}, reorder it to {1,4,2,3}.
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- 
- class Solution {
+
+//Non recursion
+class Solution {
 public:
     void reorderList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = head;
+        ListNode* tmp = NULL;
+        
+        while(fast){
+            fast = fast->next;
+            if(fast){
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }//find the header of second half
+        fast = slow->next;//fast is the header of second half
+        slow->next = NULL;
+        slow = NULL;
+
+        while(fast){
+            tmp = fast;
+            fast = fast->next;
+            tmp->next = slow;
+            slow = tmp;
+        }// reverse the second half, slow is the new header of second half
+
+        fast = dummy->next;//fast is the header of the first half
+        dummy->next = NULL;
+        tmp = dummy;
+        while(fast && slow){
+            tmp->next = fast;
+            tmp = tmp->next;
+            fast = fast->next;
+            tmp->next = slow;
+            tmp = tmp->next;
+            slow = slow->next;
+        }//merge two lists
+        if(fast)
+            tmp->next = fast;
+        if(slow)
+            tmp->next = slow;
+        delete dummy;
+    }
+};
+
+//recursion, end to front
+class Solution {
+public:
+    void reorderList(ListNode *head) {
         if(!head || !head->next)
             return;
-        ListNode* slow = head, *fast=head, *first=head;
+        ListNode* slow = head, *fast = head, *first = head;
         while(fast){
-            if(fast->next){
-                fast=fast->next->next;
-                slow = slow ->next;
-            }
-            else{
+            fast = fast->next;
+            if(fast){
                 fast=fast->next;
+                slow = slow ->next;
             }
         }//now slow is the first node in the second half
         reorderListHelper(first, slow);
@@ -40,35 +86,33 @@ public:
     ListNode* reorderListHelper(ListNode* &first, ListNode* second){
         if(!second->next){
             ListNode* tmp = first;
-            first=first->next;
-            tmp->next=second;
+            first = first->next;
+            tmp->next = second;
             return second;	
         }
 
-        ListNode* rtn = reorderListHelper(first, second->next);//rtn is the tail of the list
-        if(first==second){// odd situation
-            second->next=NULL;
-            rtn->next=second;
+        ListNode* res = reorderListHelper(first, second->next);//res is the tail of the list
+        if(first == second){// odd situation
+            second->next = NULL;
+            res->next = second;
             return second;
         }
         
         ListNode* tmp = first;
-        first=first->next;
-        tmp->next=second;
-        second->next=NULL;
-        rtn->next=tmp;
+        first = first->next;
+        tmp->next = second;
+        second->next = NULL;
+        res->next = tmp;
 
         return second;
     }
 };
  
- 
+//recursion, front to end
 class Solution {
 public:
     void reorderList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if(!head || !head->next)
+        if(!head)
             return;
         ListNode* slow = head;
         ListNode* fast = head;
@@ -77,18 +121,16 @@ public:
             if(fast->next){
                 fast=fast->next->next;
                 slow = slow ->next;
-            }
-            else{
+            }else{
                 fast=fast->next;
                 odd = true;
             }
         }
-        
         reorderListHelper(head, slow, odd);
     }
     
     ListNode* reorderListHelper(ListNode* head, ListNode* &mid, bool &odd){
-        if(head==mid){
+        if(head == mid){
             if(odd){
                 ListNode* tmp = mid;
                 mid = mid->next;
@@ -102,43 +144,5 @@ public:
         mid = mid->next;
         head->next->next = tmp;
         return head;
-    }
-};
-
-
-//O(n) spaces, but O(n^2) time, cannot pass the time limit
-class Solution {
-public:
-    void reorderList(ListNode *head) {
-        int num = 0;
-        ListNode *count = head;
-        while(count){
-            count = count->next;
-            num++;
-        }
-
-        bool odd = num%2;
-        int moves = odd?(num-1)/2:(num-2)/2;
-        ListNode *curNode, *bHead;
-        bHead = new ListNode(-1);
-        bHead->next = head;
-        head = NULL;
-        while(moves>=0){
-            curNode = bHead;
-            for(int i = moves; i>0; i--){
-                curNode = curNode->next;
-            }
-            ListNode *tmp = curNode->next;
-            if(odd){
-                curNode->next = tmp->next;
-                tmp->next = head;
-                odd = false;
-            }else{
-                curNode->next = tmp->next->next;
-                tmp->next->next = head;
-            }
-            head = tmp;
-            moves--;
-        }
     }
 };
