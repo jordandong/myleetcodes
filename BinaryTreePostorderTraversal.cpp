194d542314c9c5135f259151ef9129524b45eff5
jordandong <jordandong@gmail.com>
2015-02-27 22:56:41 -0800

    Update BinaryTreePostorderTraversal.cpp

--- BinaryTreePostorderTraversal.cpp
+++ BinaryTreePostorderTraversal.cpp
@@ -11,6 +11,8 @@ Given binary tree {1,#,2,3},
 return [3,2,1].
 
 Note: Recursive solution is trivial, could you do it iteratively?
+
+Hide Tags Tree Stack
 */
 
 /**
@@ -22,32 +24,22 @@ Note: Recursive solution is trivial, could you do it iteratively?
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
- /**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
-        vector<int> rtn;
+        vector<int> res;
         stack<TreeNode*> stk;
         stack<bool> visited;
         stk.push(root);
         visited.push(false);
-        while(!stk.empty()){
-            TreeNode* cur=stk.top();
+        while(stk.size()){
+            TreeNode* cur = stk.top();
             bool v = visited.top();
             stk.pop();
             visited.pop();
             if(!cur || v){
                 if(v)
-                    rtn.push_back(cur->val);
+                    res.push_back(cur->val);
                 continue;
             }
             stk.push(cur);
@@ -57,50 +49,51 @@ public:
             stk.push(cur->left);
             visited.push(false);
         }
-        return rtn;
+        return res;
     }
 };
- 
- 
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        vector<int> s;
-        //postorderHelper(root, s);
-        postorderHelper1(root, s);
-        return s;
-    }
-    
-    void postorderHelper(TreeNode *root, vector<int> &s){
-        if(!root)
-            return;
-        postorderHelper(root->left, s);
-        postorderHelper(root->right, s);
-        s.push_back(root->val);
-    }
-    
-    void postorderHelper1(TreeNode *root, vector<int> &s){
+        vector<int> res;
         stack<TreeNode*> stk;
         set<TreeNode*> st;
-        while(!stk.empty() || root){
+        while(stk.size() || root){
             while(root){
                 stk.push(root);
-                root=root->left;
+                root = root->left;
             }
             
             root = stk.top();
-            while(st.find(root)!=st.end()){
+            while(st.find(root) != st.end()){
                 stk.pop();
-                s.push_back(root->val);
-                if(!stk.empty())
+                res.push_back(root->val);
+                if(stk.size())
                     root = stk.top();
                 else
-                    return;
+                    return res;
             }
             st.insert(root);
             root = root->right;
         }
+        return res;
+    }
+};
+ 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        postorderTraversalHelper(root, res);
+        return res;
+    }
+    
+    void postorderTraversalHelper(TreeNode *root, vector<int> &res){
+        if(!root)
+            return;
+        postorderTraversalHelper(root->left, res);
+        postorderTraversalHelper(root->right, res);
+        res.push_back(root->val);
     }
 };
