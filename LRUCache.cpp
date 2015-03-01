38daea8a2875dfbc1d5032f6b0adb21f2786ef7d
jordandong <jordandong@gmail.com>
2015-03-01 12:09:40 -0800

    Update LRUCache.cpp

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -3,9 +3,16 @@ Design and implement a data structure for Least Recently Used (LRU) cache. It sh
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
 set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Hide Tags Data Structure
 */
 
 class LRUCache{
+private:
+    list<pair<int, int> > entryList; //from Most(head) to Least(tail), in STL it is double-linked list
+    unordered_map<int, list<pair<int, int> >::iterator> entryMap; //for getting, O(1)
+    int max_size; //Total size
+    int cur_size;
 public:
     LRUCache(int capacity) {
         max_size = capacity;
@@ -13,7 +20,7 @@ public:
     }
     
     int get(int key) {
-        if(entryMap.find(key)!=entryMap.end()){
+        if(entryMap.find(key) != entryMap.end()){
             int value = entryMap[key]->second;
             entryList.erase(entryMap[key]);
             entryList.push_front(make_pair(key, value));
@@ -24,8 +31,8 @@ public:
     }
     
     void set(int key, int value) {
-        if(entryMap.find(key)==entryMap.end()){
-            if(cur_size==max_size)
+        if(entryMap.find(key) == entryMap.end()){
+            if(cur_size == max_size)
                 del();
         }else{
             entryList.erase(entryMap[key]);
@@ -38,13 +45,8 @@ public:
     
     void del(){
         cur_size--;
-        list<pair<int, int> >::reverse_iterator it = entryList.rbegin();
+        auto it = entryList.rbegin();
         entryMap.erase(it->first);
         entryList.pop_back();
     }
-private:
-    list<pair<int, int> > entryList; //from Most(head) to Least(tail)
-    unordered_map<int, list<pair<int, int> >::iterator> entryMap; //for getting, O(1)
-    int max_size; //Total size
-    int cur_size;
 };
