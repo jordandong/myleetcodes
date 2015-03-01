14db414c49392f05cbbe5e02adfa681f4b99124c
jordandong <jordandong@gmail.com>
2015-03-01 12:32:17 -0800

    Update SortList.cpp

--- SortList.cpp
+++ SortList.cpp
@@ -1,5 +1,7 @@
 /*
 Sort a linked list in O(n log n) time using constant space complexity.
+
+Hide Tags Linked List Sort
 */
 
 /**
@@ -10,130 +12,46 @@ Sort a linked list in O(n log n) time using constant space complexity.
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- 
- /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
- 
- //Save time by avoiding counting every time
 class Solution {
 public:
     ListNode *sortList(ListNode *head) {
         ListNode *count = head;
         int len = 0;
         while(count){
-            count=count->next;
+            count = count->next;
             len++;
         }
-        return Sort(&head, len);
+        return Partition_Sort(head, len);
     }
     
-    ListNode* Sort(ListNode** head, int len){
+    ListNode* Partition_Sort(ListNode* &head, int len){
         if(len == 0)
             return NULL;
         if(len == 1){
-            ListNode *tmp = *head;
-            *head = (*head)->next;
+            ListNode *tmp = head;
+            head = head->next;
             tmp->next = NULL;
             return tmp;
         }
-        return Merge(Sort(head, len/2), Sort(head, len-len/2));
+        return Merge(Partition_Sort(head, len/2), Partition_Sort(head, len - len/2));
     }
     
     ListNode* Merge(ListNode* l1, ListNode* l2){
-        ListNode *rtn_head = new ListNode(-1);
-        ListNode *cur = rtn_head;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *cur = dummy;
         while(l1 && l2){
-            if(l1->val<l2->val){
+            if(l1->val < l2->val){
                 cur->next = l1;
                 l1 = l1->next;
             }else{
                 cur->next = l2;
                 l2 = l2->next;
             }
-            cur=cur->next;
+            cur = cur->next;
         }
-        cur->next = l1?l1:l2;
-        cur = rtn_head->next;
-        delete rtn_head;
+        cur->next = l1 ? l1 : l2;
+        cur = dummy->next;
+        delete dummy;
         return cur;
-    }
-};
- 
- 
- 
-class Solution {
-public:
-    ListNode *sortList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if(!head||!head->next)
-            return head;
-        
-        ListNode *fast=head;
-        ListNode *slow=head;
-    
-        while(fast){
-            fast=fast->next;
-            if(fast){
-                fast=fast->next;
-                if(fast)
-                    slow=slow->next;
-                else{
-                    ListNode* tmp = slow->next;
-                    slow->next = NULL;
-                    slow = tmp;    
-                }
-            }else{
-                ListNode* tmp = slow->next;
-                slow->next = NULL;
-                slow = tmp;
-            }
-        }
-        
-        return mergeList(sortList(head), sortList(slow));
-    }
-    
-    ListNode *mergeList(ListNode *l1, ListNode *l2){
-        ListNode *new_t = NULL;;
-        ListNode *t = NULL;;
-        while(l1 || l2){
-            if(l1&&l2){
-                if(l1->val < l2->val){
-                    if(!t){
-                        t=l1;
-                        new_t=t;
-                    }else{
-                        t->next=l1;
-                        t=t->next;
-                    }
-                    l1=l1->next;
-                }else{
-                    if(!t){
-                        t=l2;
-                        new_t=t;
-                    }else{
-                        t->next=l2;
-                        t=t->next;
-                    }
-                    l2=l2->next;
-                }
-                continue;
-            }
-            if(!l1&&l2){
-                t->next=l2;
-                break;
-            }
-            if(l1&&!l2){
-                t->next=l1;
-                break;
-            }
-        }
-        return new_t;
     }
 };
