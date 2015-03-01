146288303e3d0c753f6514399f52caf5b8b17805
jordandong <jordandong@gmail.com>
2015-03-01 14:32:14 -0800

    Update ReverseWordsInAString.cpp

--- ReverseWordsInAString.cpp
+++ ReverseWordsInAString.cpp
@@ -5,6 +5,9 @@ For example,
 Given s = "the sky is blue",
 return "blue is sky the".
 
+Update (2015-02-12):
+For C programmers: Try to solve it in-place in O(1) space.
+
 click to show clarification.
 
 Clarification:
@@ -14,8 +17,37 @@ Could the input string contain leading or trailing spaces?
 Yes. However, your reversed string should not contain leading or trailing spaces.
 How about multiple spaces between two words?
 Reduce them to a single space in the reversed string.
+
+Hide Tags String
 */
 
+// T : O(3*N) S : O(1)
+class Solution {
+public:
+    void reverseWords(string &s) {
+        while(s.length() && s.back() == ' ')
+            s.pop_back();
+        while(s.length() && s[0] == ' ')
+            s.erase(0, 1);
+        int i = 0;
+        int j = 0;
+        while(j <= s.length()){
+            if(j == s.length() || s[j] == ' '){
+                if(j > 0 && s[j - 1] == ' '){
+                    s.erase(j, 1);
+                    continue;
+                }else{
+                    reverse(s.begin() + i, s.begin() + j);
+                    i = j + 1;
+                }
+            }
+            j++;
+        }
+        reverse(s.begin(), s.end());
+    }
+};
+
+//T : O(2*N), S : O(N)
 class Solution {
 public:
     void reverseWords(string &s) {
@@ -23,70 +55,25 @@ public:
         stack<char> stk;
         string res;
         
-        for(int i = len-1; i>=0; i--){
-            if(s[i]==' '){
-                if(stk.empty()){
-                    continue;
-                }else{
-                    while(!stk.empty()){
+        for(int i = len - 1; i >= 0; i--){
+            if(s[i] == ' '){
+                if(stk.size()){
+                    while(stk.size()){
                         res.push_back(stk.top());
                         stk.pop();
                     }
-                   res.push_back(' ');
+                    res.push_back(' ');
                 }
             }else{
                 stk.push(s[i]);
             }
         }
-        while(!stk.empty()){
+        while(stk.size()){
             res.push_back(stk.top());
             stk.pop();
         }
-        if(res.back()==' ')
+        if(res.back() == ' ')
             res.pop_back();
         s = res;
-    }
-};
-
-
-
-class Solution {
-public:
-    void reverseWords(string &s) {
-        int first = -1;
-        int second = -1;
-        string r="";
-        for(int i=0; i<s.length(); i++){
-            if(s[i]==' '){
-                if(first==-1 || second ==-1){
-                    continue;
-                }else{
-                    r = " " + r;
-                    r = s.substr(first, second-first+1) + r;
-                    first=-1;
-                    second =-1;
-                }
-            }else{
-                if(first==-1){
-                    first=i;
-                    second=i;
-                }else{
-                    second = i;
-                }
-            }
-                
-        }
-        
-        if(first!=-1 && second !=-1){
-            r = " " + r;
-            r = s.substr(first, second-first+1) + r;
-            first=-1;
-            second =-1;
-        }
-        int size = r.length();
-        if(size >0 && r[size-1]==' ')
-            s = r.substr(0, size-1);
-        else
-            s = r;
     }
 };
