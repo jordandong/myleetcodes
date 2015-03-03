2d485762f7b96c3576ae58162fef0b4d0f24f563
jordandong <jordandong@gmail.com>
2015-03-02 22:43:23 -0800

    Update ExcelSheetColumnTitle.cpp

--- ExcelSheetColumnTitle.cpp
+++ ExcelSheetColumnTitle.cpp
@@ -10,17 +10,21 @@ For example:
     26 -> Z
     27 -> AA
     28 -> AB 
+Credits:
+Special thanks to @ifanchu for adding this problem and creating all test cases.
+
+Hide Tags Math
 */
 class Solution {
 public:
     string convertToTitle(int n) {
         string res = "";
         while(n){
-            int r = (n-1)%26;
+            int r = (n - 1)%26;
             string s = "";
-            s.push_back(r +'A');
-            res= s+res;
-            n=(n-1)/26;
+            s.push_back(r + 'A');
+            res = s + res;
+            n = (n - 1)/26;
         }
         return res;
     }
