0373531c9b2d9d0dc4704f533632000477e1b6d6
jordandong <jordandong@gmail.com>
2015-03-02 22:37:24 -0800

    Update FractionToRecurringDecimal.cpp

--- FractionToRecurringDecimal.cpp
+++ FractionToRecurringDecimal.cpp
@@ -8,6 +8,10 @@ For example,
 Given numerator = 1, denominator = 2, return "0.5".
 Given numerator = 2, denominator = 1, return "2".
 Given numerator = 2, denominator = 3, return "0.(6)".
+Credits:
+Special thanks to @Shangrila for adding this problem and creating all test cases.
+
+Hide Tags Hash Table Math
 */
 
 /*
@@ -36,48 +40,44 @@ public:
         long long r = 0;
         ostringstream out;
         vector<int> cir;
-        unordered_map<int,int> last_r;
+        unordered_map<int, int> last_r;
 
-        if(x==0)
+        if(x == 0)
             return "0";
 
-        if(x<0){
-            x=-x;
-            y=-y;
+        if(x < 0){
+            x = -x;
+            y = -y;
         }
-        if(y<0){
-            y=-y;
+        if(y < 0){
+            y = -y;
             out<<"-";
         }
         
-        out<<(x/y);
-        r=x%y;
-        if(r==0)
+        out<<(x / y);
+        r = x % y;
+        if(r == 0)
             return out.str();
             
         out<< ".";
         
-        for (int i = 0; r&&(last_r.find(r)==last_r.end()); i++){
-            last_r[r] = i;
-            r*=10;
+        for(int i = 0; r && (last_r.find(r) == last_r.end()); i++){
+            last_r[r] = i;//add new remainder index at cir.
+            r *= 10;
             cir.push_back(r/y);
-            r=r%y;
+            r = r%y;
         }
         
         if(r){
-            for (int i = 0; i<last_r[r]; i++){
+            for(int i = 0; i < last_r[r]; i++)
                 out<<cir[i];
-            }
             out<<"(";
-            for (int i = last_r[r]; i<cir.size(); i++){
+            for(int i = last_r[r]; i < cir.size(); i++)
                 out<<cir[i];
-            }
             out<<")";
-        }
-        else {
-            for(int i = 0; i<cir.size(); i++){
+        }else{
+            for(int i = 0; i < cir.size(); i++)
                 out<<cir[i];
-            }
         }
         return out.str();
     }
