498631c1bba6be1dffb8502ecc5cf557df9fdce4
jordandong <jordandong@gmail.com>
2015-03-02 22:55:23 -0800

    Update FactorialTrailingZeroes.cpp

--- FactorialTrailingZeroes.cpp
+++ FactorialTrailingZeroes.cpp
@@ -7,13 +7,25 @@ Note: Your solution should be in logarithmic time complexity.
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int a = 1;
-        int base = pow(5, a);
         int res = 0;
-        while(base<=n){
-            res+=(n/base);
+        int base = 5;
+        while(n){
+            res += (n / base);
+            n /= base;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int a = 1;
+        long long base = pow(5, a);
+        int res = 0;
+        while(base <= n){
+            res += (n / base);
             base = pow(5, ++a);
-            //every 5 has one zero, 5^a has a-1 extra zeros
         }
         return res;
     }
