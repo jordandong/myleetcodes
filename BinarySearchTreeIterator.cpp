9f9bafae9ca1fc94ddf2a150b493f9ebae644f2b
jordandong <jordandong@gmail.com>
2015-03-04 00:30:21 -0800

    Update BinarySearchTreeIterator.cpp

--- BinarySearchTreeIterator.cpp
+++ BinarySearchTreeIterator.cpp
@@ -3,7 +3,12 @@ Implement an iterator over a binary search tree (BST). Your iterator will be ini
 
 Calling next() will return the next smallest number in the BST.
 
-Note: next() and hasNext() should run in O(1) time and uses O(h) memory, where h is the height of the tree.
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+
+Hide Tags Tree Stack
 */
 
 /**
@@ -43,12 +48,56 @@ public:
         TreeNode* t = h.top();
         h.pop();
         if(t->right){
-            t=t->right;
+            t = t->right;
             while(t){
                 h.push(t);
                 t = t->left;
             }
         }
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
+ 
+//modified the tree, O(1) space, not sure why {2, 1, 3} runtime error, it is correct on my local machine
+class BSTIterator {
+private:
+    TreeNode* cur;
+public:
+    BSTIterator(TreeNode *root) {
+        TreeNode* last = NULL;
+        cur = root;
+        preprocess(root, last);
+        while(cur && cur->left)
+            cur = cur->left;
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return cur != NULL;
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* res = cur;
+        cur = cur->right; // always goto next node
+        while(cur && cur->left && cur->left->val > res->val) //if left node is unvisited
+            cur = cur->left;
+        return res->val;
+    }
+    
+    void preprocess(TreeNode *root, TreeNode* &last){
+        if(!root)
+            return;
+        preprocess(root->left, last);
+        if(last && !last->right) // set all the right node to the next node if it is NULL
+            last->right = root;
+        last = root;
+        preprocess(root->right, last);
     }
 };
 
