303e3e25e350c14d41b4b1689296521aad4a81f3
jordandong <jordandong@gmail.com>
2015-03-08 20:41:29 -0700

    Update ReverseBits.cpp

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -31,3 +31,16 @@ public:
         return n;
     }
 };
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int i = 0;
+        uint32_t res = 0;
+        while(i < 32){
+            res <<= 1;
+            res |= ((n>>i++) &1);
+        }
+        return res;
+    }
+};
