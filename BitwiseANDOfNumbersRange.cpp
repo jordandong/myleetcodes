024fe3a2899283856a4921bce380f745c0263b21
jordandong <jordandong@gmail.com>
2015-04-15 18:30:40 -0700

    Update BitwiseANDOfNumbersRange.cpp

--- BitwiseANDOfNumbersRange.cpp
+++ BitwiseANDOfNumbersRange.cpp
@@ -1,4 +1,4 @@
-/8
+/*
 Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
 
 For example, given the range [5, 7], you should return 4.
@@ -15,7 +15,7 @@ public:
         int N = 31;
         int res = 0;
         while(N >= 0){     
-            if(((m>>N) & 0x1) ^ ((n>>N) & 0x1))//check from the highest bits, if different, stop, if equal, set this bits in res
+            if(((m>>N) & 0x1) ^ ((n>>N) & 0x1))//check from the highest bits, if different, stop, if equal, set this bit in res
                 return res;
             res |= (0x1<<N & m);
             N--;
