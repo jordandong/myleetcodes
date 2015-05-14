4af4769aa54d0b77e0a9c7bb24bc96cc877f3094
jordandong <jordandong@gmail.com>
2015-05-13 21:37:49 -0700

    Update CourseSchedule.cpp

--- CourseSchedule.cpp
+++ CourseSchedule.cpp
@@ -35,24 +35,24 @@ public:
             indgree[y]++;
         }
         
-       for (int i = 0; i < numCourses; i++){
-           if (indgree[i] == 0) { //take most dependent first
-               stk.push(i);
-               taken++;
-           }
-       }
+        for (int i = 0; i < numCourses; i++){
+            if (indgree[i] == 0) { //take most dependent first
+                stk.push(i);
+                taken++;
+            }
+        }
        
-       while (stk.size()) {
-           int n = stk.top();
-           stk.pop();
-           for (int i = 0; i < g[n].size(); i++) {
-               indgree[g[n][i]]--; //reduce the indgree after taking one course
-               if (indgree[g[n][i]] == 0) {
-                   stk.push(g[n][i]);
-                   taken++;
-               }
-           }
-       }
-       return taken == numCourses;
+        while (stk.size()) {
+            int n = stk.top();
+            stk.pop();
+            for (int i = 0; i < g[n].size(); i++) {
+                indgree[g[n][i]]--; //reduce the indgree after taking one course
+                if (indgree[g[n][i]] == 0) {
+                    stk.push(g[n][i]);
+                    taken++;
+                }
+            }
+        }
+        return taken == numCourses;
     }
 };
