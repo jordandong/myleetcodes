89f2ff3a65a1d2cec56e4c64791fde5fbede11f8
jordandong <jordandong@gmail.com>
2015-10-26 16:57:58 -0700

    Update SerializeAndDeserializeBinaryTree.cpp

--- SerializeAndDeserializeBinaryTree.cpp
+++ SerializeAndDeserializeBinaryTree.cpp
@@ -67,6 +67,44 @@ private:
     }
 };
 
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serializeHelper(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserializeHelper(in);
+    }
+
+private:
+    void serializeHelper(TreeNode* root, ostringstream& out) {
+        if (root) {
+            out << root->val << ' ';
+            serializeHelper(root->left, out);
+            serializeHelper(root->right, out);
+        } else {
+            out << "# ";
+        }
+    }
+
+    TreeNode* deserializeHelper(istringstream& in) {
+        string val;
+        in >> val;
+        if (val == "#")
+            return NULL;
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserializeHelper(in);
+        root->right = deserializeHelper(in);
+        return root;
+    }
+};
+
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.deserialize(codec.serialize(root));
