eee5a3917c2386192d471f49d90b29d0549e00e3
jordandong <jordandong@gmail.com>
2015-11-27 00:09:55 -0800

    Update SpiralMatrix.cpp

--- SpiralMatrix.cpp
+++ SpiralMatrix.cpp
@@ -40,16 +40,16 @@ public:
             
             int i = 0, j = 0;
             while(j < n - 1)
-        	    res.push_back(matrix[offset + i][offset + j++]);
+                res.push_back(matrix[offset + i][offset + j++]);
             while(i < m - 1)
-        	    res.push_back(matrix[offset + i++][offset + j]);
+                res.push_back(matrix[offset + i++][offset + j]);
             while(j > 0)
-        	    res.push_back(matrix[offset + i][offset + j--]);
+                res.push_back(matrix[offset + i][offset + j--]);
             while(i > 0)
-        	    res.push_back(matrix[offset + i--][offset + j]);
-        	m -= 2;
-        	n -= 2;
-        	offset += 1;
+                res.push_back(matrix[offset + i--][offset + j]);
+            m -= 2;
+            n -= 2;
+            offset += 1;
         }
         return res;
     }
@@ -60,27 +60,27 @@ class Solution {
 public:
     void spiralOrderHelper(vector<vector<int> > &matrix, int M, int N, int k, vector<int> &res) {
         if (M <= 0 || N <= 0)
-        	return;
+            return;
         if (M == 1) {
             for (int j = 0; j < N; j++)
-            	res.push_back(matrix[k][k+j]);
+                res.push_back(matrix[k][k+j]);
             return;
         }
         if (N == 1) {
             for (int i = 0; i < M; i++)
-            	res.push_back(matrix[k+i][k]);
+                res.push_back(matrix[k+i][k]);
             return;
         }
         
         int i = 0, j = 0;
         for (; j < N-1; j++)
-        	res.push_back(matrix[k+i][k+j]);
+            res.push_back(matrix[k+i][k+j]);
         for (; i < M-1; i++)
-        	res.push_back(matrix[k+i][k+j]);
+            res.push_back(matrix[k+i][k+j]);
         for (; j > 0; j--)
-        	res.push_back(matrix[k+i][k+j]);
+            res.push_back(matrix[k+i][k+j]);
         for (; i > 0; i--)
-        	res.push_back(matrix[k+i][k+j]);
+            res.push_back(matrix[k+i][k+j]);
         spiralOrderHelper(matrix, M-2, N-2, k+1, res);
     }
 
@@ -88,10 +88,10 @@ public:
         vector<int> res;
         int M = matrix.size();
         if (M == 0)
-        	return res;
+            return res;
         int N = matrix[0].size();
         if (N == 0)
-        	return res;
+            return res;
         spiralOrderHelper(matrix, M, N, 0, res);
         return res;
     }
