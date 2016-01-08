f2f9b88f976754348a45541a3c96337b502f0254
jordandong <jordandong@gmail.com>
2016-01-07 21:42:22 -0800

    Update PowerOfThree.cpp

--- PowerOfThree.cpp
+++ PowerOfThree.cpp
@@ -10,6 +10,20 @@ Special thanks to @dietpepsi for adding this problem and creating all test cases
 Hide Tags Math
 */
 
+/*
+There exists a pretty fast method for integers of limited size (e.g. 32-bit integers).
+Note that for an integer N that is a power of 3 the following is true:
+    For any M <= N that is a power of 3, M divides N.
+    For any M <= N that is not a power 3, M does not divide N.
+    The biggest power of 3 that fits into 32 bits is 3^19.
+*/
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n > 0 && ((int)pow(3, 19) % n == 0);
+    }
+};
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
