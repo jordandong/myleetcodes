111c90128d35d23bd4a945d0fef13d5f8eba0cfe
J.D <jordandong@gmail.com>
2016-05-18 13:07:17 -0700

    Update IntersectionOfTwoArrays.cpp

--- IntersectionOfTwoArrays.cpp
+++ IntersectionOfTwoArrays.cpp
@@ -9,7 +9,7 @@ The result can be in any order.
 
 Hide Tags Binary Search Hash Table Two Pointers Sort
 */
-
+//T : O(N) , S : O(N)
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -21,6 +21,31 @@ public:
             if (mp[e]) {
                 res.push_back(e);
                 mp[e] = false;
+            }
+        }
+        return res;
+    }
+};
+
+//T : O(NlogN) S : O(1)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, N1 = nums1.size();
+        int j = 0, N2 = nums2.size();
+        while (i < N1 && j < N2) {
+            if (nums1[i] == nums2[j]) {
+                if (res.size() == 0 || res.back() != nums1[i])
+                    res.push_back(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return res;
