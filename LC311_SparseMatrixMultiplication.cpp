7b7b81c7d04ec881ba20997a43e8f3f18733225c
J.D <jordandong@gmail.com>
2016-05-21 13:33:48 -0700

    Create LC311_SparseMatrixMultiplication.cpp

---
+++ LC311_SparseMatrixMultiplication.cpp
@@ -0,0 +1,35 @@
+/*
+Given two sparse matrices A and B, return the result of AB.
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+],
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+],
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+*/
+
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int m = A.size(), n = B.size(), p = B[0].size();
+        vector<vector<int>> C(m, vector<int>(p, 0));
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (A[i][j])
+                    for (int k = 0; k < p; k++)
+                        C[i][k] += A[i][j] * B[j][k];
+        return C;
+    }
+};
