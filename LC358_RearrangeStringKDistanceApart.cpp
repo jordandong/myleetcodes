22546e1929a895292f58b9907f1e91f3ef886f6c
J.D <jordandong@gmail.com>
2016-06-14 18:53:04 -0700

    Update LC358_RearrangeStringKDistanceApart.cpp

--- LC358_RearrangeStringKDistanceApart.cpp
+++ LC358_RearrangeStringKDistanceApart.cpp
@@ -31,56 +31,92 @@ Special thanks to @elmirap for adding this problem and creating all test cases.
 #include <queue>
 using namespace std;
 
-class cmp {
-public:
-    bool operator() (const pair<char, int> &a, const pair<char, int> &b) const {
-        return a.second < b.second;
-    }
-};
-
-string rearrange(int k, string s) {
+string rearrangeString(string s, int k) {
     string res = s;
-    priority_queue<pair<char, int>, vector<pair<char, int>>, cmp> q;
+    priority_queue<pair<int, char>, vector<pair<int, char>>> q;
     char mp[256] = {0};
     for(int i = 0; i < res.length(); i++) {
         mp[res[i]]++;
     }
     for(int i = 0; i < 256; i++) {
         if (mp[i] > 0)
-            q.push({i, mp[i]});
+            q.push({mp[i], i});
     }
 
-    if (k*(q.top().second - 1) + 1 > res.length())
-        return "invalid input";
+    if (k*(q.top().first - 1) + 1 > res.length())
+        return "";
 
     int start = 0, round = 0;
     while (!q.empty()) {
         auto e = q.top();
         q.pop();
-        while (e.second > 0) {
+        while (e.first > 0) {
             if (start + k*round >= res.length()) {
                 start++;
                 round = 0;
             }
-            res[start + round*k] = e.first;
-            e.second--;
+            res[start + round*k] = e.second;
+            e.first--;
             round++;
         }
     }
     return res;
 }
 
+class Solution {
+public:
+    string rearrangeString(string str, int k) {
+        if (k == 0)
+            return str;
+        string res;
+        int len = (int)str.size();
+        unordered_map<char, int> m;
+        priority_queue<pair<int, char>> q;
+        for (auto a : str)
+             ++m[a];
+        for (auto it = m.begin(); it != m.end(); ++it) {
+            q.push({it->second, it->first});
+        }
+        while (!q.empty()) {
+            vector<pair<int, int>> v;
+            int cnt = min(k, len);
+            for (int i = 0; i < cnt; ++i) {
+                if (q.empty())
+                    return "";
+                auto t = q.top();
+                q.pop();
+                res.push_back(t.second);
+                if (--t.first > 0)
+                    v.push_back(t);
+                --len;
+            }
+            for (auto a : v)
+                q.push(a);
+        }
+        return res;
+    }
+};
+
 int main() {
+    Solution sol;
     for (int i = 1; i < 8; i++) {
-        cout<<rearrange(i, "aaaaabbbbccccdddd")<<endl;
+        cout<<rearrangeString("aaaaabbbbccccdddd", i)<<endl;
+        cout<<sol.rearrangeString("aaaaabbbbccccdddd", i)<<endl;
     }
     return 0;
 }
 
-aaaaabbbbddddcccc
-adadadadacbcbcbcb
-abdabcabcadcadcbd
-abdcabdcabdcabdca
-invalid input
-invalid input
-invalid input
+aaaaaddddccccbbbb
+adcbadcbadcbadcba
+acacacacabdbdbdbd
+adcbadcbadcbadcba
+adcadbadbacbacbdc
+adcbadcbadcbadcba
+adcbadcbadcbadcba
+adcbadcbadcbadcba
+""
+""
+""
+""
+""
+""
