397a2b66ddd93a4ee5b17e34192209e6a9ba2cd4
J.D <jordandong@gmail.com>
2016-06-17 23:44:11 -0700

    Update LC360_SortTransformedArray.cpp

--- LC360_SortTransformedArray.cpp
+++ LC360_SortTransformedArray.cpp
@@ -1,15 +1,19 @@
 /*
-TBD
-Given a sorted array of integers and integer values a, b and c, apply a function of the form f(x) = ax^2+bx+c to each element x in the array.
+Given a sorted array of integers nums and integer values a, b and c.
+Apply a function of the form f(x) = ax^2 + bx + c to each element x in the array.
+The returned array must be in sorted order.
 
-The returned array should be sorted.
+Expected time complexity: O(N)
 
 Example:
+nums = [-4, -2, 2, 4], a = 1, b = 3, c = 5,
+Result: [3, 9, 15, 33]
 
-Given x = [-4 , -2 , 2 , 4] , a=1 , b=3 , c=5, output should be [3, 9, 15, 33].
-Given x = [-4 , -2 , 2 , 4] , a=-1 , b=3 , c=5, output should be [-23, -5, 1, 7].
+nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
+Result: [-23, -5, 1, 7]
 
-Expected time complexity : O(n);
+Credits:
+Special thanks to @elmirap for adding this problem and creating all test cases.
 */
 
 #include <iostream>
