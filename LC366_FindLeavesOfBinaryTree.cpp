9d05ed2df58cba2b4872eb5dd2dffea720f27aad
J.D <jordandong@gmail.com>
2016-06-25 13:47:19 -0700

    Update LC366_FindLeavesOfBinaryTree.cpp

--- LC366_FindLeavesOfBinaryTree.cpp
+++ LC366_FindLeavesOfBinaryTree.cpp
@@ -56,3 +56,29 @@ private:
         return level;
     }
 };
+
+//T : O(NlogN), S : (logN)
+class Solution {
+public:
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<vector<int>> res;
+        while (root) {
+            vector<int> leaves;
+            root = remove(root, leaves);
+            res.push_back(leaves);
+        }
+        return res;
+    }
+
+    TreeNode* remove(TreeNode *node, vector<int> &leaves) {
+        if (!node) 
+            return NULL;
+        if (!node->left && !node->right) {
+            leaves.push_back(node->val);
+            return NULL;
+        }
+        node->left = remove(node->left, leaves);
+        node->right = remove(node->right, leaves);
+        return node;
+    }
+};
