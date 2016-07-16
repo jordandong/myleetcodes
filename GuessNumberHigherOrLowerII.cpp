1131b304030697f15253ac4c1ba1eb9357ce7d51
J.D <jordandong@gmail.com>
2016-07-16 11:45:02 -0700

    Update GuessNumberHigherOrLowerII.cpp

--- GuessNumberHigherOrLowerII.cpp
+++ GuessNumberHigherOrLowerII.cpp
@@ -49,3 +49,19 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n + 2, vector<int>(n + 1, 0));
+        for (int i = n; i > 0; --i) {
+            for (int j = i + 1; j <= n; ++j) {
+                dp[i][j] = INT_MAX;
+                for (int k = i; k <= j; ++k) {
+                    dp[i][j] = min(dp[i][j], k + max(dp[i][k - 1], dp[k + 1][j]));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
