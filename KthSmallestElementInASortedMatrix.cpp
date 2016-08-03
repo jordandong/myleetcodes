8e481f63df10923f44bb0e02db7adb050d73ab12
J.D <jordandong@gmail.com>
2016-08-03 00:06:02 -0700

    Update KthSmallestElementInASortedMatrix.cpp

--- KthSmallestElementInASortedMatrix.cpp
+++ KthSmallestElementInASortedMatrix.cpp
@@ -18,22 +18,47 @@ Hide Tags Binary Search Heap
 Hide Similar Problems (M) Find K Pairs with Smallest Sums
 */
 
+//T : O(logV*N*logN), S : O(1)
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int M = matrix.size();
+        if (M == 0)
+            return 0;
+        int N = matrix[0].size();
+        if (N != M)
+            return 0;
+        int lo = matrix[0][0], hi = matrix[M - 1][M - 1];
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+			int num = 0;
+			for (int i = 0; i < M; i++)
+				num += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+			if (num < k)
+				lo = mid + 1;
+			else
+				hi = mid;
+		}
+		return lo;
+	}
+};
+
 // T:  O(k * log(min(M, N, k))) , S: O(min(M, N, K))
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int M = matrix.size();
+        if (M == 0)
+            return 0;
+        int N = matrix[0].size();
+        if (N != M)
+            return 0;
         int kth_smallest = 0;
         using P = pair<int, pair<int, int>>;
         priority_queue<P, vector<P>, greater<P>> q;
         auto push = [&matrix, &q](int i, int j) {
-            if (matrix.size() > matrix[0].size()) {
-                if (i < matrix[0].size() && j < matrix.size()) {
-                    q.emplace(matrix[j][i], make_pair(i, j));
-                }
-            } else {
-                if (i < matrix.size() && j < matrix[0].size()) {
-                    q.emplace(matrix[i][j], make_pair(i, j));
-                }
+            if (i < matrix.size() && j < matrix[0].size()) {
+                q.emplace(matrix[i][j], make_pair(i, j));
             }
         };
 
