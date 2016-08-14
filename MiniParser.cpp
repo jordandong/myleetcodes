27378820886ce862be6c9fd5f9dc19898b06b5b7
J.D <jordandong@gmail.com>
2016-08-14 13:51:07 -0700

    Update MiniParser.cpp

--- MiniParser.cpp
+++ MiniParser.cpp
@@ -80,7 +80,7 @@ private:
     }
 };
 
-// T : O(N), S : O(N)
+// T : O(N), S : O(h)
 class Solution{
 public:
     NestedInteger deserialize(string s) {
@@ -108,5 +108,35 @@ private:
             ++i;
         }
         return result;
+    }
+};
+
+// T : O(N), S : O(h)
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        if (s == "")
+            return NestedInteger();
+
+        if (s[0] != '[')
+            return NestedInteger(stoi(s));
+
+        stack<NestedInteger> stk;
+        for (int i = 0, j = 0; j < s.length(); ++j) {
+            if (s[j] == '[') {
+                stk.push(NestedInteger()); 
+                i = j + 1;
+            } else if (s[j] == ',' || s[j] == ']') {
+                if (isdigit(s[j - 1]))
+                    stk.top().add(NestedInteger(stoi(s.substr(i, j - i))));
+                if (s[j] == ']' && stk.size() > 1) {
+                    NestedInteger cur = stk.top();
+                    stk.pop();
+                    stk.top().add(cur);
+                }
+                i = j + 1;
+            }
+        }
+        return stk.top();
     }
 };
