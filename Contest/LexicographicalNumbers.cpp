328eeacdfb3dde88b506eeb66ab6868aa3e57f69
J.D <jordandong@gmail.com>
2016-08-21 16:35:32 -0700

    Update LexicographicalNumbers.cpp

--- Contest/LexicographicalNumbers.cpp
+++ Contest/LexicographicalNumbers.cpp
@@ -9,6 +9,27 @@ Please optimize your algorithm to use less time and space. The input size may be
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        
+        vector<int> res;
+        if (n <= 9) {
+            for (int i = 1; i <= n; i++)
+                res.push_back(i);
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                res.push_back(i);
+                lexicalOrderHelper(res, i, n);
+            }
+        }
+        return res;
+    }
+    
+    void lexicalOrderHelper(vector<int> &res, int s, int n) {
+        int next = s*10;
+        if (next <= n) {
+            for (int i = 0; i <= min(9, n - next); i++) {
+                int num = next + i;
+                res.push_back(num);
+                lexicalOrderHelper(res, num, n);
+            }
+        }
     }
 };
