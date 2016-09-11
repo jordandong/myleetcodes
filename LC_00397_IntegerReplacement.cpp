b8fb67daebf7f6cfd1c5a5000944b9f6ac3f2fd2
J.D <jordandong@gmail.com>
2016-09-11 15:39:56 -0700

    Update LC_00397_IntegerReplacement.cpp

--- LC_00397_IntegerReplacement.cpp
+++ LC_00397_IntegerReplacement.cpp
@@ -1,9 +1,8 @@
 /*
 Given a positive integer n and you can do operations as follow:
-
-If n is even, replace n with n/2.
-If n is odd, you can replace n with either n + 1 or n - 1.
-What is the minimum number of replacements needed for n to become 1?
+1. If n is even, replace n with n/2.
+2. If n is odd, you can replace n with either n + 1 or n - 1.
+3. What is the minimum number of replacements needed for n to become 1?
 
 Example 1:
 Input:
@@ -22,11 +21,22 @@ Explanation:
 7 -> 8 -> 4 -> 2 -> 1
 or
 7 -> 6 -> 3 -> 2 -> 1
+
+Hide Tags Math
 */
 
 class Solution {
 public:
     int integerReplacement(int n) {
-        
+        return integerReplacementHelper(n);
+    }
+    
+    int integerReplacementHelper(long long n) {
+        if (n == 1)
+            return 0;
+        if (n & 1)
+            return min(integerReplacementHelper(n - 1), integerReplacementHelper(n + 1)) + 1;
+        else
+            return integerReplacementHelper(n/2) + 1;
     }
 };
