562cf18fa44a92a47e491b03e89903b132046aeb
J.D <jordandong@gmail.com>
2016-09-19 09:41:00 -0700

    Update LC_00403_FrogJump.cpp

--- LC_00403_FrogJump.cpp
+++ LC_00403_FrogJump.cpp
@@ -33,32 +33,7 @@ There is no way to jump to the last stone as the gap between the 5th and 6th sto
 Hide Tags Dynamic Programming
 */
 
-// T : O(N), S : O(N)
-// Time:  O(n)
-// Space: O(n)
-
-class Solution {
-public:
-    bool canCross(vector<int>& stones) {
-        unordered_map<int, int> lookup;
-        for (int i = 0; i < stones.size(); ++i)
-            lookup[stones[i]] = i;
-
-        vector<bool> dp(stones.size());
-        dp[0] = true;
-        for (int i = 0; i < stones.size(); ++i) {
-            if (dp[i]) {
-                for (const auto& k : {i - 1, i, i + 1}) {
-                    const auto it = lookup.find(stones[i] + k);
-                    if (it != lookup.end()) {
-                        dp[it->second] = true;
-                    }
-                }
-            }
-        }
-        return dp.back();
-    }
-};
+// T : O(N^2), S : O(N)
 
 class Solution {
 public:
