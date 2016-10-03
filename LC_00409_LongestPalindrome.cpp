7c4bb672ddd64541139ffe189befcb3ff5ed4528
J.D <jordandong@gmail.com>
2016-10-03 12:48:22 +0800

    Update LC_00409_LongestPalindrome.cpp

--- LC_00409_LongestPalindrome.cpp
+++ LC_00409_LongestPalindrome.cpp
@@ -33,3 +33,15 @@ public:
         return res;
     }
 };
+
+//T : O(N) , S : O(1)
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int odds = 0;
+        for (auto c = 'A'; c <= 'z'; ++c) {
+            odds += count(s.cbegin(), s.cend(), c) & 1;
+        }
+        return s.length() - odds + (odds > 0);
+    }
+};
