dd31aab460154c2920cb83dddcdaf40e58f82f3a
J.D <jordandong@gmail.com>
2016-10-09 23:08:23 +0800

    Update LC_00418_SentenceScreenFitting.cpp

--- LC_00418_SentenceScreenFitting.cpp
+++ LC_00418_SentenceScreenFitting.cpp
@@ -43,3 +43,50 @@ had--
 The character '-' signifies an empty space on the screen.
 */
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int wordsTyping(vector<string> &sentence, int rows, int cols) {
+    int i = 0, j = 0, k = 0, times = 0;
+    while (i < rows && j < cols) {
+      if (sentence[k].length() > cols)
+        break;
+        int remain = cols - j;
+        if (remain == sentence[k].length() + 1 || remain == sentence[k].length()) {
+          k ++;
+          j = 0;
+          i++;
+        } else if (remain < sentence[k].length()) {
+          j = 0;
+          i++;
+        } else {
+          j += (sentence[k].length() + 1);
+          k++;
+        }
+      if (k == sentence.size()) {
+        k = 0;
+        times++;
+      }
+    }
+    return times;
+  }
+};
+
+int main() {
+	vector<string> data = {"hello", "world"};
+	Solution s;
+	cout<<s.wordsTyping(data, 2 , 8)<<endl;
+	data = {"a", "bcd", "e"};
+	cout<<s.wordsTyping(data, 3 , 6)<<endl;
+	data = {"I", "had", "apple", "pie"};
+	cout<<s.wordsTyping(data, 4 , 5)<<endl;
+	cout<<s.wordsTyping(data, 8 , 5)<<endl;
+	cout<<s.wordsTyping(data, 9 , 5)<<endl;
+	cout<<s.wordsTyping(data, 39 , 5)<<endl;	
+	return 0;
+}
+
