07550c78cccabaed658788a9ed8930ea37e93df1
J.D <jordandong@gmail.com>
2016-10-10 20:17:56 +0800

    Update LC_00418_SentenceScreenFitting.cpp

--- LC_00418_SentenceScreenFitting.cpp
+++ LC_00418_SentenceScreenFitting.cpp
@@ -50,6 +50,39 @@ using namespace std;
 
 class Solution {
 public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        vector<int> wc(sentence.size());
+        for (int i = 0; i < sentence.size(); ++i) {
+            wc[i] = wordsFit(sentence, i, cols);
+        }
+
+        int words = 0, start = 0;
+        for (int i = 0; i < rows; ++i) {
+            words += wc[start];
+            start = (start + wc[start]) % sentence.size();
+        }
+        return words / sentence.size();
+    }
+
+private:
+    int wordsFit(const vector<string>& sentence, int start, int cols) {
+        if (sentence[start].length() > cols) {
+            return 0;
+        }
+
+        int sum = sentence[start].length(), count = 1;
+        for (int i = (start + 1) % sentence.size();
+             sum + 1 + sentence[i].length() <= cols;
+             i = (i + 1) % sentence.size()) {
+            sum += 1 + sentence[i].length();
+            ++count;
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
   int wordsTyping(vector<string> &sentence, int rows, int cols) {
     int i = 0, j = 0, k = 0, times = 0;
     while (i < rows && j < cols) {
@@ -89,4 +122,3 @@ int main() {
 	cout<<s.wordsTyping(data, 39 , 5)<<endl;	
 	return 0;
 }
-
