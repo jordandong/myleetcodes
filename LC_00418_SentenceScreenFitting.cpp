af08c9cf6c224968910acf2d1b1e699e6c6faf99
J.D <jordandong@gmail.com>
2016-10-10 20:52:24 +0800

    Update LC_00418_SentenceScreenFitting.cpp

--- LC_00418_SentenceScreenFitting.cpp
+++ LC_00418_SentenceScreenFitting.cpp
@@ -52,6 +52,48 @@ class Solution {
 public:
     int wordsTyping(vector<string>& sentence, int rows, int cols) {
         vector<int> wc(sentence.size());
+        unordered_map<int, int> dp; //start idx -> row id
+        for (int i = 0; i < sentence.size(); ++i) {
+            wc[i] = wordsFit(sentence, i, cols); //start with index i, the wc on this row
+        }
+
+        int words = 0, start = 0;
+        for (int i = 0; i < rows; ++i) {
+            if (dp.find(start) != dp.end()) {
+                int loops = (rows - i + 1)/(i - dp[start]);
+		words += sentence.size() * loops;
+		i += (i - dp[start]) * loops - 1;
+		dp.clear();
+            } else {
+                words += wc[start];
+                dp[start] = i;
+                start = (start + wc[start]) % sentence.size();
+            }
+        }
+        return words / sentence.size();
+    }
+
+private:
+    int wordsFit(const vector<string>& sentence, int start, int cols) {
+        if (sentence[start].length() > cols) {
+            return 0;
+        }
+
+        int sum = sentence[start].length(), count = 1;
+        for (int i = (start + 1) % sentence.size();
+             sum + 1 + sentence[i].length() <= cols;
+             i = (i + 1) % sentence.size()) {
+            sum += 1 + sentence[i].length();
+            ++count;
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        vector<int> wc(sentence.size());
         for (int i = 0; i < sentence.size(); ++i) {
             wc[i] = wordsFit(sentence, i, cols);
         }
