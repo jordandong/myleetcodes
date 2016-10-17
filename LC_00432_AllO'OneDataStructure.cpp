1522de9a013d64913ae171626f9d8c64b041cad6
J.D <jordandong@gmail.com>
2016-10-17 16:42:53 +0800

    Create LC_00432_AllO'OneDataStructure.cpp

---
+++ LC_00432_AllO'OneDataStructure.cpp
@@ -0,0 +1,46 @@
+/*
+Implement a data structure supporting the following operations:
+1. Inc(Key) - inserts a new key with value 1. Or increments an existing key by 1.
+2. Dec(Key) - decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
+3. GetMaxKey() - returns one of the keys with maximal value.
+4. GetMinKey() - returns one of the keys with minimal value.
+
+Challenge: Perform all these in O(1) time complexity.
+*/
+
+class AllOne {
+public:
+    /** Initialize your data structure here. */
+    AllOne() {
+        
+    }
+    
+    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+    void inc(string key) {
+        
+    }
+    
+    /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+    void dec(string key) {
+        
+    }
+    
+    /** Returns one of the keys with maximal value. */
+    string getMaxKey() {
+        
+    }
+    
+    /** Returns one of the keys with Minimal value. */
+    string getMinKey() {
+        
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * string param_3 = obj.getMaxKey();
+ * string param_4 = obj.getMinKey();
+ */
