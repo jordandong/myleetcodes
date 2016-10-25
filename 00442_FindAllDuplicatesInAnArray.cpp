c714915ba0b1d3bd4f6e85271c8f9448e918b022
J.D <jordandong@gmail.com>
2016-10-26 01:34:09 +0800

    Update 00442_FindAllDuplicatesInAnArray.cpp

--- 00442_FindAllDuplicatesInAnArray.cpp
+++ 00442_FindAllDuplicatesInAnArray.cpp
@@ -14,6 +14,20 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
         for (int i = 0; i < nums.size(); i++) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] < 0)
+                res.push_back(idx + 1);
+            nums[idx] = -nums[idx];
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
             int idx = nums[i] - 1;
             if (nums[i] == 0 || idx == i)
                 continue;
