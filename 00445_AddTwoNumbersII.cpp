472f2d00e8b6df214d55078c8cdee9cbeb66bd96
J.D <jordandong@gmail.com>
2016-10-29 05:34:43 +0800

    Create 00445_AddTwoNumbersII.cpp

---
+++ 00445_AddTwoNumbersII.cpp
@@ -0,0 +1,70 @@
+/*
+You are given two linked lists representing two non-negative numbers. The most significant digit comes first and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+Follow up:
+What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+
+Example:
+Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 8 -> 0 -> 7
+
+Hide Tags Linked List
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int len1 = 0, len2 = 0, carry = 0;
+        ListNode *t = l1, *res;
+        while (t) {
+            len1++;
+            t= t->next;
+        }
+        t = l2;
+        while (t) {
+            len2++;
+            t= t->next;
+        }
+        
+        if (len1 < len2)
+            t = addTwoNumbersHelper(l2, l1, len2, len1, carry);
+        else
+            t = addTwoNumbersHelper(l1, l2, len1, len2, carry);
+        
+        if (carry) {
+            res = new ListNode(1);
+            res->next = t;
+        } else {
+            res = t;
+        }
+        return res;
+    }
+    
+    ListNode* addTwoNumbersHelper(ListNode* l1, ListNode* l2, int len1, int len2, int &carry) {
+        if (len1 == 0 && len2 == 0)
+            return NULL;
+
+        ListNode *t, *res;
+        if (len1 != len2) {
+            t = addTwoNumbersHelper(l1->next, l2, --len1, len2, carry);
+            res = new ListNode((carry + l1->val) % 10);
+            res->next = t;
+            carry = (carry + l1->val) / 10;
+        } else {
+            t = addTwoNumbersHelper(l1->next, l2->next, --len1, --len2, carry);
+            res = new ListNode((carry + l1->val + l2->val) % 10);
+            res->next = t;
+            carry = (carry + l1->val + l2->val) / 10;
+        }
+        return res;
+    }
+};
