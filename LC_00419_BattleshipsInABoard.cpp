2a9463fd325dd6b43d8ddc999a9b656ddf07cd88
J.D <jordandong@gmail.com>
2016-10-31 07:51:02 -0700

    Update and rename 00419_BattleshipsInABoard.cpp to LC_00419_BattleshipsInABoard.cpp

--- LC_00419_BattleshipsInABoard.cpp
+++ LC_00419_BattleshipsInABoard.cpp
@@ -1,9 +1,9 @@
 /*
-Given an 2D board, count how many different battleships are in it.
-The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+Given an 2D board, count how many different battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s.
+You may assume the following rules:
 1. You receive a valid board, made of only battleships or empty slots.
 2. Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
-At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+3. At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
 
 Example:
 X..X
@@ -15,9 +15,10 @@ Invalid Example:
 ...X
 XXXX
 ...X
-This is not a valid board - as battleships will always have a cell separating between them.
+This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
 
-Your algorithm should not modify the value of the board.
+Follow up:
+Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
 */
 
 class Solution {
