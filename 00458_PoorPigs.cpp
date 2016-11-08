ff77600a33e6b1d5d5cf753bf0fc1c9da7aa03cf
J.D <jordandong@gmail.com>
2016-11-08 00:45:01 -0800

    Create 00458_PoorPigs.cpp

---
+++ 00458_PoorPigs.cpp
@@ -0,0 +1,25 @@
+/*
+There are 1000 buckets, one and only one of them contains poison, the rest are filled with water. They all look the same. If a pig drinks that poison it will die within 15 minutes. What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
+Answer this question, and write an algorithm for the follow-up general case.
+
+Follow-up:
+If there are n buckets and a pig drinking poison will die within m minutes, how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
+*/
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        if (buckets <= 0)
+            return -1;
+        if (buckets-- == 1)
+            return 0;
+        int states = minutesToTest / minutesToDie + 1;
+        //states ^ count >= buckets, mini count
+        int count = 0;
+        while (buckets > 0){
+            buckets /= states;
+            count++;
+        }
+        return count;
+    }
+};
