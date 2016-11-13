09a61b9fcbf5557fa81a507d8cc5cd9452dab785
J.D <jordandong@gmail.com>
2016-11-13 14:44:10 -0800

    Update 00456_132Pattern.cpp

--- 00456_132Pattern.cpp
+++ 00456_132Pattern.cpp
@@ -23,9 +23,23 @@ Output: True
 Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
 */
 
+//T : O(N ^ 2), S : O(1)
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        
+        int N = nums.size();
+        if (N < 3)
+            return false;
+        int mi = nums[0];
+        for (int j = 1; j < N; j++) {
+            mi = min(nums[j], mi);
+            if (mi == nums[j])
+                continue;
+            for (int k = j + 1; k < N; k++) {
+                if (mi < nums[k] && nums[k] < nums[j])
+                    return true;
+            }
+        }
+        return false;
     }
 };
