ba1a5a59df78db118994ee77cb662716a662c173
J.D <jordandong@gmail.com>
2016-11-13 14:23:26 -0800

    Update 00454_4SumII.cpp

--- 00454_4SumII.cpp
+++ 00454_4SumII.cpp
@@ -23,6 +23,18 @@ The two tuples are:
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        
+        int res = 0;
+        unordered_map<int, int> mp;
+        for (int &a : A) {
+            for (int &b : B) {
+                mp[a + b] += 1;
+            }
+        }
+        for (int &c : C) {
+            for (int &d : D) {
+                res += mp[ -(c + d)];
+            }
+        }
+        return res;
     }
 };
