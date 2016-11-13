c4403149fa52d77b1bb65c9de009842786cf7091
J.D <jordandong@gmail.com>
2016-11-13 15:45:01 -0800

    Update 00456_132Pattern.cpp

--- 00456_132Pattern.cpp
+++ 00456_132Pattern.cpp
@@ -43,3 +43,28 @@ public:
         return false;
     }
 };
+
+//T : O(N), S : O(N)
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int N = nums.size();
+        if (N < 3)
+            return false;
+        vector<int> mi = nums;
+        for (int i = 1; i < N; i++)
+            mi[i] = min(nums[i - 1], mi[i - 1]);
+            
+        for (int k = N - 1, j = N; k > 0; k--) {
+            if (nums[k] <= mi[k]) //ak < ai, pass
+                continue;
+            while (j < N && mi[j] <= mi[k]) //find current smallest one which are bigger than ai on the right part
+                j++;
+            if (j < N && nums[k] > mi[j]) //found
+                return true;
+            if (nums[k] > mi[k - 1]) //use the right side of mi array as a stack to store the values bigger than mi[k - 1]
+                mi[--j] = nums[k];
+        }
+        return false;
+    }
+};
