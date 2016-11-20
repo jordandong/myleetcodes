ff3978ceddf6dda4475b64399fca1a03f64f0363
J.D <jordandong@gmail.com>
2016-11-21 05:14:39 +0800

    Update LC_00463_IslandPerimeter.cpp

--- LC_00463_IslandPerimeter.cpp
+++ LC_00463_IslandPerimeter.cpp
@@ -20,8 +20,32 @@ Explanation: The perimeter is the 16 yellow stripes in the image below:
 Hide Tags Hash Table
 */
 
-
 //T : O(M*N), S : O(1)
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int M = grid.size();
+        if (M == 0)
+            return 0;
+        int N = grid[0].size();
+        if (N == 0)
+            return 0;
+        int res = 0;
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (grid[i][j]) {
+                    res += 4;
+                    if (i > 0 && grid[i - 1][j])
+                        res -= 2;
+                    if (j > 0 && grid[i][j  - 1])
+                        res -= 2;
+                }
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
