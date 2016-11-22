1ae50c2dcd4cee554fabd1cc7234792644e78e54
J.D <jordandong@gmail.com>
2016-11-22 16:04:00 +0800

    Update 00465_OptimalAccountBalancing.cpp

--- 00465_OptimalAccountBalancing.cpp
+++ 00465_OptimalAccountBalancing.cpp
@@ -1,60 +1,61 @@
-
 #include <iostream>
-#include <tuple>
 #include <vector>
+#include <list>
 #include <climits>
 #include <unordered_map>
 using namespace std;
 class Solution {
-	public:
-    int minTransfers(vector<tuple<int, int, int>> transactions) {
-	unordered_map<int, int> loan;
-	int x, y, z;
-	vector<int> rich, poor;
+public:
+    int minTransfers(vector<vector<int>> transactions) {
+        unordered_map<int, int> loan;
+        list<int> rich, poor;
         for (auto &e : transactions) {
-		tie(x, y, z) = e;			
-		loan[x] += z;
-		loan[y] -= z;
-	}
-	for ( auto it = loan.begin(); it != loan.end(); ++it) {
-		if (it->second > 0) {
-			rich.push_back(it->second);
-		} else if (it->second < 0) {
-			poor.push_back(-it->second);
-		}
-	}
-        return solve(rich, poor);
+            loan[e[0]] += e[2];
+            loan[e[1]] -= e[2];
+        }
+        for ( auto it = loan.begin(); it != loan.end(); ++it) {
+            if (it->second > 0)
+                rich.push_back(it->second);
+            else if (it->second < 0)
+                poor.push_back(-it->second);
+        }
+        return minTransfersHelper(rich, poor);
     }
-
-    int solve(vector<int> rich, vector<int> poor) {
-            int rlen = rich.size(), plen = poor.size(); 
-            if (min(rlen, plen) <= 1)
-                return max(rlen, plen);
-            int ans = INT_MAX;
-            for (int ri = 0;  ri < rlen; ri++) {
-                for (int pi = 0; pi < plen; pi++) {
-                    vector<int> nrich = rich;
-		    nrich.erase(nrich.begin() + ri);
-                    vector<int> npoor = poor;
-		    npoor.erase(npoor.begin() + pi);
-
-                    if (rich[ri] < poor[pi]) {
-                        npoor.push_back(poor[pi] - rich[ri]);
-		    } else if (rich[ri] > poor[pi]) {
-                        nrich.push_back(rich[ri] - poor[pi]);
-		    }
-                    ans = min(solve(nrich, npoor) + 1, ans);
-		}
-	    }
-            return ans;
+    
+    int minTransfersHelper (list<int> &rich, list<int> &poor) {
+        int ans = INT_MAX;
+        for (auto ri = rich.begin();  ri != rich.end(); ri++) {
+            int r = *ri;
+            if (r == 0)
+                continue;
+            for (auto pi = poor.begin(); pi != poor.end(); pi++) {
+                int p = *pi;
+                if (p == 0)
+                    continue;
+                if (r < p) {
+                    *pi = p - r;
+                    *ri = 0;
+                } else if (r > p) {
+                    *ri = r - p;
+                    *pi = 0;
+                } else {
+                    *ri = 0;
+                    *pi = 0;
+                }
+                ans = min(minTransfersHelper(rich, poor) + 1, ans);
+                *ri = r;
+                *pi = p;
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
     }
 };
 
 int main() {
-	// your code goes here
-	Solution s;
-	//vector<tuple<int, int, int>> data = {make_tuple(0,1,10), make_tuple(2,0,5)};
-	vector<tuple<int, int, int>> data = {make_tuple(0,1,10), make_tuple(0,1,10), make_tuple(1,0,1), make_tuple(1,2,5), make_tuple(2,0,5)};
-	cout<<s.minTransfers(data)<<endl;;
-	return 0;
+    // your code goes here
+    Solution s;
+    vector<vector<int>> data = {{0,1,10}, {2,0,5}};
+    data = {{0,1,10}, {0,1,10}, {1,0,1}, {1,2,5}, {2,0,5}};
+    cout<<s.minTransfers(data)<<endl;;
+    return 0;
 }
