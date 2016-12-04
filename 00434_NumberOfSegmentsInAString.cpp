1ad1fdb896cabf76a9017c3e5dddd8821af7a9f6
J.D <jordandong@gmail.com>
2016-12-03 23:28:27 -0800

    Create 00434_NumberOfSegmentsInAString.cpp

---
+++ 00434_NumberOfSegmentsInAString.cpp
@@ -0,0 +1,16 @@
+/*
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+For example,
+
+Input: "Hello, my name is John"
+
+Output: 5
+*/
+
+class Solution {
+public:
+    int countSegments(string s) {
+        
+    }
+};
