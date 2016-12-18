8ad4d9ea8ec3bac106b2ad4dce850cb8502dfe9c
J.D <jordandong@gmail.com>
2016-12-18 14:39:48 -0800

    Update and rename 00472_ConcatenatedWords.cpp to LC_00472_ConcatenatedWords.cpp

--- LC_00472_ConcatenatedWords.cpp
+++ LC_00472_ConcatenatedWords.cpp
@@ -1,20 +1,21 @@
 /*
 Given a list of words, please write a program that returns all concatenated words in the given list of words.
-
 A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
 
 Example:
 Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
-
 Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
-
 Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
  "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
 "ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
 Note:
-The number of elements of the given array will not exceed 10,000
-The length sum of elements in the given array will not exceed 600,000.
-The returned elements order does not matter.
+1. The number of elements of the given array will not exceed 10,000
+2. The length sum of elements in the given array will not exceed 600,000.
+3. All the input string will only include lower case letters.
+4. The returned elements order does not matter.
+
+Hide Tags Dynamic Programming Trie Depth-first Search
 */
 
 class Solution {
