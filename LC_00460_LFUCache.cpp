a4eab68660bc3a825bace117c1dfa0c3a5999be5
J.D <jordandong@gmail.com>
2017-01-03 13:39:29 -0800

    Update and rename 00460_LFUCache.cpp to LC_00460_LFUCache.cpp

--- LC_00460_LFUCache.cpp
+++ LC_00460_LFUCache.cpp
@@ -1,31 +1,28 @@
 /*
-Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and set.
+Design and implement a data structure for Least Frequently Used (LFU) cache.
+It should support the following operations: get and put.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-set(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
 
 Follow up:
 Could you do both operations in O(1) time complexity?
 
 Example:
-
 LFUCache cache = new LFUCache( 2 /# capacity #/ );
 
-cache.set(1, 1);
-cache.set(2, 2);
+cache.put(1, 1);
+cache.put(2, 2);
 cache.get(1);       // returns 1
-cache.set(3, 3);    // evicts key 2
+cache.put(3, 3);    // evicts key 2
 cache.get(2);       // returns -1 (not found)
 cache.get(3);       // returns 3.
-cache.set(4, 4);    // evicts key 1.
+cache.put(4, 4);    // evicts key 1.
 cache.get(1);       // returns -1 (not found)
 cache.get(3);       // returns 3
 cache.get(4);       // returns 4
-Subscribe to see which companies asked this question
 
 Hide Tags Design
-Hide Similar Problems (H) LRU Cache
-Have you met this question in a real interview? Yes
 */
 
 class LFUCache {
@@ -51,11 +48,11 @@ public:
         auto row = keymap[key].first;
         auto col = keymap[key].second;
         int val = col->second;
-        set(key, val);
+        put(key, val);
         return val;
     }
     
-    void set(int key, int value) {
+    void put(int key, int value) {
         if (keymap.find(key) == keymap.end()) {
             if (max_size == 0)
                 return;
@@ -110,5 +107,5 @@ public:
  * Your LFUCache object will be instantiated and called as such:
  * LFUCache obj = new LFUCache(capacity);
  * int param_1 = obj.get(key);
- * obj.set(key,value);
+ * obj.put(key,value);
  */
