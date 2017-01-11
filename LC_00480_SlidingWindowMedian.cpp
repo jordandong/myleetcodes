41a88071153f1ab8321ac520f4e704b68d604f0d
J.D <jordandong@gmail.com>
2017-01-10 22:45:03 -0800

    Update LC_00480_SlidingWindowMedian.cpp

--- LC_00480_SlidingWindowMedian.cpp
+++ LC_00480_SlidingWindowMedian.cpp
@@ -71,3 +71,30 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        multiset<int> window(nums.begin(), nums.begin() + k);
+        auto mid = next(window.begin(), k / 2);
+        vector<double> medians;
+        for (int i = k; ; i++) {
+            // Push the current median.
+            medians.push_back((double(*mid) + *next(mid, k % 2 - 1)) / 2);
+
+            // If all done, return.
+            if (i == nums.size())
+                return medians;
+            
+            // Insert nums[i].
+            window.insert(nums[i]); //log(k)
+            if (nums[i] < *mid)
+                mid--;
+            // Erase nums[i - k].
+            if (nums[i - k] <= *mid)
+                mid++;
+            window.erase(window.lower_bound(nums[i - k]));
+        }
+        return medians;
+    }
+};
