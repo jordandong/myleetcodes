168246bcce96c6ee3a4fba9e576b10e9612b1ef5
J.D <jordandong@gmail.com>
2017-01-27 01:29:46 -0800

    Update and rename 00483_SmallestGoodBase.cpp to LC_00483_SmallestGoodBase.cpp

--- LC_00483_SmallestGoodBase.cpp
+++ LC_00483_SmallestGoodBase.cpp
@@ -1,5 +1,5 @@
 /*
-For an integer n, we call k >= 2 a good base of n, if all digits of n base k are 1.
+For an integer n, we call k>=2 a good base of n, if all digits of n base k are 1.
 
 Now given a string representing n, you should return the smallest good base of n in string format. 
 
@@ -25,9 +25,26 @@ Note:
 Hide Tags Binary Search Math
 */
 
+/*
+1 + k + k^2 + ... + k^m = n
+n > k ^ m       --> k < n ** 1/m
+n < (k + 1) ^ m --> k > n ** 1/m - 1
+k = int(n ** 1/m)
+verify 1 + k + k^2 + ... + k^m == n
+*/
+
 class Solution {
 public:
     string smallestGoodBase(string n) {
-        
+        typedef unsigned long long ll;
+        ll num = stol(n);
+        for (ll m = log2(num); m >= 2; --m) {
+            ll k = pow(num, 1.0 / m), sum = 0, f = 1;
+            for (int i = 0; i < m + 1; i++, f *= k)
+                sum += f;
+            if (sum == num)
+                return to_string(k);
+        }
+        return to_string(num - 1);
     }
 };
