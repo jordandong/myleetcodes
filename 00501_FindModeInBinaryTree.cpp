427371837c0b8a837e5ac5771e8d07c9c92ce275
J.D <jordandong@gmail.com>
2017-01-28 19:11:22 -0800

    Create 00501_FindModeInBinaryTree.cpp

---
+++ 00501_FindModeInBinaryTree.cpp
@@ -0,0 +1,30 @@
+/*
+Given a binary search tree with duplicates. You have to find all the mode(s) in given binary tree.
+
+For example:
+Given binary tree [1,null,2,2],
+   1
+    \
+     2
+    /
+   2
+return [2].
+
+Note: If a tree has more than one mode, you can return them in any order.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        
+    }
+};
