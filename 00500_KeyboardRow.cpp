85afcb0638f0e24f02c688283f987074787142d0
J.D <jordandong@gmail.com>
2017-02-04 23:16:37 -0800

    Update 00500_KeyboardRow.cpp

--- 00500_KeyboardRow.cpp
+++ 00500_KeyboardRow.cpp
@@ -16,25 +16,25 @@ You may assume the input string will only contain letters of alphabet.
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-  		vector<int> mp = {2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
-		vector<string> ans;
-		for (auto & w : words) {
-		    int last_row = mp[idx(w[0])], i = 1;
-			while (i < w.size()) {
-				if (mp[idx(w[i])] != last_row)
-					break;
-				i++;
-			}
-			if (i == w.size())
-				ans.push_back(w);
-		}
-		return ans;
-	}
+        vector<int> mp = {2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
+        vector<string> ans;
+        for (auto & w : words) {
+            int last_row = mp[idx(w[0])], i = 1;
+            while (i < w.size()) {
+                if (mp[idx(w[i])] != last_row)
+                    break;
+                i++;
+            }
+            if (i == w.size())
+                ans.push_back(w);
+        }
+        return ans;
+    }
 private:
-	int idx(char a) {
-		if (a >= 'a' && a <= 'z')
-			return a - 'a';
-		else
-		    return a - 'A';
-	}
+    int idx(char a) {
+        if (a >= 'a' && a <= 'z')
+            return a - 'a';
+        else
+            return a - 'A';
+    }
 };
