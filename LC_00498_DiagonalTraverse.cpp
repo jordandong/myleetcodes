027f7850708ab77bc058ecba9d34015af6db76ce
J.D <jordandong@gmail.com>
2017-02-06 23:59:01 -0800

    Update and rename 00498_DiagonalTraverse.cpp to LC_00498_DiagonalTraverse.cpp

--- LC_00498_DiagonalTraverse.cpp
+++ LC_00498_DiagonalTraverse.cpp
@@ -1,7 +1,7 @@
 /*
-Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order.
+Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order as shown in the below image.
 
-Example 1:
+Example:
 Input:
 [
  [ 1, 2, 3 ],
@@ -9,7 +9,11 @@ Input:
  [ 7, 8, 9 ]
 ]
 Output:  [1,2,4,7,5,3,6,8,9]
+
 Explanation:
+1-  2*  3-
+4*  5-  6*
+7-  8*  9-
 
 Note:
 The total number of elements of the given matrix will not exceed 10,000.
