6b8ae464b6e811afa08d0942dd6b8eab8fb77c61
J.D <jordandong@gmail.com>
2017-02-06 23:56:27 -0800

    Update and rename 00500_KeyboardRow.cpp to LC_00500_KeyboardRow.cpp

--- LC_00500_KeyboardRow.cpp
+++ LC_00500_KeyboardRow.cpp
@@ -1,6 +1,5 @@
 /*
-Given a List of words,
-return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
 
 American keyboard
 
@@ -9,8 +8,10 @@ Input: ["Hello", "Alaska", "Dad", "Peace"]
 Output: ["Alaska", "Dad"]
 
 Note:
-You may use one character in the keyboard more than once.
-You may assume the input string will only contain letters of alphabet.
+1. You may use one character in the keyboard more than once.
+2. You may assume the input string will only contain letters of alphabet.
+
+Hide Tags Hash Table
 */
 
 class Solution {
