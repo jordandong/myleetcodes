7ea1eaa49a8f021c5dbab4b265e772965d5971b7
J.D <jordandong@gmail.com>
2017-02-09 22:00:22 -0800

    Update 00490_TheMaze.cpp

--- 00490_TheMaze.cpp
+++ 00490_TheMaze.cpp
@@ -41,3 +41,51 @@ Note:
 4. The maze contains at least 2 empty spaces, and both the width and height of the maze won't exceed 100.
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        if (maze.empty() || maze[0].empty())
+            return true;
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return hasPathHelper(maze, dp, start[0], start[1], destination[0], destination[1]);
+    }
+    bool hasPathHelper(vector<vector<int>>& maze, vector<vector<int>>& dp, int i, int j, int di, int dj) {
+        if (i == di && j == dj)
+            return true;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        bool res = false;
+        int m = maze.size(), n = maze[0].size();
+        vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+        maze[i][j] = -1;
+        for (auto dir : dirs) {
+            int x = i, y = j;
+            while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] != 1) {
+                x += dir[0];
+                y += dir[1];
+            }
+            x -= dir[0];
+            y -= dir[1];
+            if (maze[x][y] != -1) {
+                res |= hasPathHelper(maze, dp, x, y, di, dj);
+            }
+        }
+        maze[i][j] = 0;
+        dp[i][j] = res;
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> maze = {{0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 0}};
+    vector<int> start = {0, 4};
+    vector<int> destination = {3, 2};
+    cout<<s.hasPath(maze, start, destination)<<endl;
+    return 0;
+}
