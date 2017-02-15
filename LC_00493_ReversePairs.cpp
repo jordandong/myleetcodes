6b51d74c501e576df15dabbabee03a19993ad729
J.D <jordandong@gmail.com>
2017-02-14 17:48:51 -0800

    Update and rename 00493_ReversePairs.cpp to LC_00493_ReversePairs.cpp

--- LC_00493_ReversePairs.cpp
+++ LC_00493_ReversePairs.cpp
@@ -1,19 +1,20 @@
 /*
 Given an array nums, we call (i, j) an important reverse pair if i < j and nums[i] > 2*nums[j].
-
 You need to return the number of important reverse pairs in the given array.
 
 Example1:
-
 Input: [1,3,2,3,1]
 Output: 2
-Example2:
 
+Example2:
 Input: [2,4,3,5,1]
 Output: 3
+
 Note:
-The length of the given array will not exceed 50,000.
-All the numbers in the input array are in the range of 32-bit integer.
+1. The length of the given array will not exceed 50,000.
+2. All the numbers in the input array are in the range of 32-bit integer.
+
+Hide Tags Binary Indexed Tree Segment Tree Binary Search Tree Divide and Conquer
 */
 
 class Solution {
@@ -56,5 +57,28 @@ public:
         while (j < r.size())
             rtn.push_back(r[j++]);
         return rtn;
+    }
+};
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        return sortCount(nums.begin(), nums.end());
+    }
+private:
+    // count reverse pairs in range [b,e) and then sort
+    int sortCount(vector<int>::iterator b, vector<int>::iterator e) {
+        if (e - b <= 1)
+            return 0;
+        auto m = b + (e - b) / 2;
+        int count = sortCount(b, m) + sortCount(m, e);
+        for (auto i = b, j = m; i < m && j <= e; (*i /2. > *j) ? ++j : (count += (j - m), ++i))
+            if (j == e) {
+                count += (m - i) * (e - m);
+                break;
+        }
+        // inplace_merge: merge two sorted segments
+        inplace_merge(b, m, e);
+        return count;
     }
 };
