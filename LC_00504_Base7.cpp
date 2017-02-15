d0b5ccc9f8207acee719b34736ee9c0b57f6b904
J.D <jordandong@gmail.com>
2017-02-14 17:54:49 -0800

    Update and rename 00504_Base7.cpp to LC_00504_Base7.cpp

--- LC_00504_Base7.cpp
+++ LC_00504_Base7.cpp
@@ -4,15 +4,17 @@ Given an integer, return its base 7 string representation.
 Example 1:
 Input: 100
 Output: "202"
+
 Example 2:
 Input: -7
 Output: "-10"
+
 Note: The input will be in range of [-1e7, 1e7].
 */
 
 class Solution {
 public:
-    string convertTo7(int num) {
+    string convertToBase7(int num) {
         bool neg = false;
         if (num < 0) {
             neg = true;
