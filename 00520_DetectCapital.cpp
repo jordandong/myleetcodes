9677d88a2bb65d5c61eb516d8f419b9313e21560
J.D <jordandong@gmail.com>
2017-02-18 22:15:06 -0800

    Create 00520_DetectCapital.cpp

---
+++ 00520_DetectCapital.cpp
@@ -0,0 +1,31 @@
+/*
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+1. All letters in this word are capitals, like "USA".
+2. All letters in this word are not capitals, like "leetcode".
+3. Only the first letter in this word is capital if it has more than one letter, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+Example 1:
+Input: "USA"
+Output: True
+
+Example 2:
+Input: "FlaG"
+Output: False
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+*/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int capital_cnt = 0;
+        for (auto c : word) {
+            if ('A' <= c && c <= 'Z')
+                capital_cnt++;
+        }
+        return (capital_cnt == 0) || (capital_cnt == word.length()) || (capital_cnt == 1 && word[0] < 'Z');
+    }
+};
