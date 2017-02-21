b8e12ef7e00d06b9fedf8013026820f8d214c16a
J.D <jordandong@gmail.com>
2017-02-21 01:44:12 -0800

    Update and rename 00520_DetectCapital.cpp to LC_00520_DetectCapital.cpp

--- LC_00520_DetectCapital.cpp
+++ LC_00520_DetectCapital.cpp
@@ -1,6 +1,5 @@
 /*
 Given a word, you need to judge whether the usage of capitals in it is right or not.
-
 We define the usage of capitals in a word to be right when one of the following cases holds:
 1. All letters in this word are capitals, like "USA".
 2. All letters in this word are not capitals, like "leetcode".
@@ -16,6 +15,8 @@ Input: "FlaG"
 Output: False
 
 Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+Hide Tags String
 */
 
 class Solution {
