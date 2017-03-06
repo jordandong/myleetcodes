b10c2cd73132e0f8dfcad84d20ba84081bf6e3ee
J.D <jordandong@gmail.com>
2017-03-06 00:54:23 -0800

    Update 00534_DesignTinyURL.cpp

--- 00534_DesignTinyURL.cpp
+++ 00534_DesignTinyURL.cpp
@@ -27,8 +27,45 @@ Questions:
 */
 
 // You can type code here and execute it.
-int main() {
-    cout << "Hello world!" << endl;
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        // Map to store 62 possible characters
+        string db = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        string shortUrl;
+        hash<string> str_hash;
+        size_t id = str_hash(longUrl);
+        id %= (size_t)pow(62, 6);
+        mp[id] = longUrl;
+        while (id) {
+            shortUrl.push_back(db[id % 62]);
+            id /= 62;
+        }
+        reverse(shortUrl.begin(), shortUrl.end());
+        return shortUrl;
+    }
 
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        int id = 0; // initialize result
+        for (int i = 0; i < shortUrl.length(); i++) {
+            if ('a' <= shortUrl[i] && shortUrl[i] <= 'z')
+                id = id*62 + shortUrl[i] - 'a';
+            if ('A' <= shortUrl[i] && shortUrl[i] <= 'Z')
+                id = id*62 + shortUrl[i] - 'A' + 26;
+            if ('0' <= shortUrl[i] && shortUrl[i] <= '9')
+                id = id*62 + shortUrl[i] - '0' + 52;
+        }
+        return mp[id];
+    }
+private:
+    unordered_map<int, string> mp;
+};
+
+
+int main() {
+    Solution s;
+    cout<<s.encode("https://leetcode.com/problems/design-tinyurl")<<endl;;
     return 0;
 }
