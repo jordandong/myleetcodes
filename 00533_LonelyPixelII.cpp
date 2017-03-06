5f0b6997f8cde504378d2e070794a5033f25df48
J.D <jordandong@gmail.com>
2017-03-05 18:12:20 -0800

    Update 00533_LonelyPixelII.cpp

--- 00533_LonelyPixelII.cpp
+++ 00533_LonelyPixelII.cpp
@@ -38,6 +38,36 @@ Have you met this question in a real interview? Yes
 class Solution {
 public:
     int findBlackPixel(vector<vector<char>>& picture, int N) {
-        
+        int m = picture.size();
+        if (!m)
+            return 0;
+        int n = picture[0].size();
+        if (!n)
+            return 0;
+        vector<int> rows(m, 0), cols(n, 0);
+        unordered_map<string,int> mp;
+        vector<string> rows_str;
+        for (int i = 0; i < m; ++i) {
+            string s;
+            for (int j = 0; j < n; ++j) {
+                if (picture[i][j] == 'B') {
+                    rows[i]++;
+                    cols[j]++;
+                }
+                s.push_back(picture[i][j]);
+            }
+            mp[s]++;
+            rows_str.push_back(s);
+        }
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            if (rows[i] == N && mp[rows_str[i]] == N) {
+                for (int j = 0; j < n; ++j) {
+                     if (picture[i][j] == 'B' && cols[j] == N)
+                        ++res;
+                }
+            }
+        }
+        return res;
     }
 };
