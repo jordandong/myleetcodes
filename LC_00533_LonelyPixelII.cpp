f6771f8fb9c14ec4f6767470458c77afc33cfdde
J.D <jordandong@gmail.com>
2017-03-06 19:08:52 -0800

    Update and rename 00533_LonelyPixelII.cpp to LC_00533_LonelyPixelII.cpp

--- LC_00533_LonelyPixelII.cpp
+++ LC_00533_LonelyPixelII.cpp
@@ -1,18 +1,18 @@
 /*
-Given a picture consisting of black and white pixels, and a positive integer N, find the number of black pixels located at some specific row R and column C that align with all the following rules:
+Given a picture consisting of black and white pixels, and a positive integer N,
+find the number of black pixels located at some specific row R and column C that align with all the following rules:
+1. Row R and column C both contain exactly N black pixels.
+2. For all rows that have a black pixel at column C, they should be exactly the same as row R
 
-Row R and column C both contain exactly N black pixels.
-For all rows that have a black pixel at column C, they should be exactly the same as row R
 The picture is represented by a 2D char array consisting of 'B' and 'W', which means black and white pixels respectively.
-
 Example:
 Input:                                            
 [['W', 'B', 'W', 'B', 'B', 'W'],    
  ['W', 'B', 'W', 'B', 'B', 'W'],    
  ['W', 'B', 'W', 'B', 'B', 'W'],    
  ['W', 'W', 'B', 'W', 'B', 'W']] 
-
 N = 3
+
 Output: 6
 Explanation: All the bold 'B' are the black pixels we need (all 'B's at column 1 and 3).
         0    1    2    3    4    5         column index                                            
@@ -23,16 +23,13 @@ Explanation: All the bold 'B' are the black pixels we need (all 'B's at column 1
 row index
 
 Take 'B' at row R = 0 and column C = 1 as an example:
-Rule 1, row R = 0 and column C = 1 both have exactly N = 3 black pixels. 
-Rule 2, the rows have black pixel at column C = 1 are row 0, row 1 and row 2. They are exactly the same as row R = 0.
+1. Rule 1, row R = 0 and column C = 1 both have exactly N = 3 black pixels. 
+2. Rule 2, the rows have black pixel at column C = 1 are row 0, row 1 and row 2. They are exactly the same as row R = 0.
 
 Note:
 The range of width and height of the input 2D array is [1,200].
-Subscribe to see which companies asked this question.
 
 Hide Tags Array Depth-first Search
-Hide Similar Problems (M) Lonely Pixel II
-Have you met this question in a real interview? Yes  
 */
 
 class Solution {
