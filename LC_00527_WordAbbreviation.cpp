5232c670d7563c8638fb29c8e997bdab5e2e413f
J.D <jordandong@gmail.com>
2017-03-12 15:00:39 -0700

    Update and rename 00527_WordAbbreviation.cpp to LC_00527_WordAbbreviation.cpp

--- LC_00527_WordAbbreviation.cpp
+++ LC_00527_WordAbbreviation.cpp
@@ -1,8 +1,8 @@
 /*
 Given an array of n distinct non-empty strings, you need to generate minimal possible abbreviations for every word following rules below.
-
 1. Begin with the first character and then the number of characters abbreviated, which followed by the last character.
-2. If there are any conflict, that is more than one words share the same abbreviation, a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique. In other words, a final abbreviation cannot map to more than one original words.
+2. If there are any conflict, that is more than one words share the same abbreviation, a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique.
+   In other words, a final abbreviation cannot map to more than one original words.
 3. If the abbreviation doesn't make the word shorter, then keep it as original.
 
 Example:
@@ -14,6 +14,8 @@ Note:
 2. The length of each word is greater than 1.
 3. The words consist of lowercase English letters only.
 4. The return answers should be in the same order as the original array.
+
+Hide Tags Sort String
 */
 
 class Solution {
