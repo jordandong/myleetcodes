20efb13094d00815704e014bd46c22755509042e
J.D <jordandong@gmail.com>
2017-03-19 16:26:05 -0700

    Update and rename 00542_01Matrix.cpp to LC_00542_01Matrix.cpp

--- LC_00542_01Matrix.cpp
+++ LC_00542_01Matrix.cpp
@@ -1,10 +1,9 @@
 /*
 Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
-
 The distance between two adjacent cells is 1.
+
 Example 1: 
 Input:
-
 0 0 0
 0 1 0
 0 0 0
@@ -12,9 +11,9 @@ Output:
 0 0 0
 0 1 0
 0 0 0
+
 Example 2: 
 Input:
-
 0 0 0
 0 1 0
 1 1 1
@@ -22,10 +21,13 @@ Output:
 0 0 0
 0 1 0
 1 2 1
+
 Note:
-The number of elements of the given matrix will not exceed 10,000.
-There are at least one 0 in the given matrix.
-The cells are adjacent in only four directions: up, down, left and right.
+1. The number of elements of the given matrix will not exceed 10,000.
+2. There are at least one 0 in the given matrix.
+3. The cells are adjacent in only four directions: up, down, left and right.
+
+Hide Tags Depth-first Search Breadth-first Search
 */
 
 class Solution {
