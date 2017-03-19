978c2f6153bc333014c31c7f21d62eb681e77417
J.D <jordandong@gmail.com>
2017-03-18 20:53:39 -0700

    Update 00544_OutputContestMatches.cpp

--- 00544_OutputContestMatches.cpp
+++ 00544_OutputContestMatches.cpp
@@ -33,6 +33,15 @@ We ensure that the input n can be converted into the form 2k, where k is a posit
 class Solution {
 public:
     string findContestMatch(int n) {
-        
+        vector<string> m(n);
+        for (int i = 0; i < n; i++)
+            m[i] = to_string(i + 1);
+
+        while (n > 1) {
+            for (int i = 0; i < n / 2; i++)
+                m[i] = "(" + m[i] + "," + m[n - 1 - i] + ")";
+            n /= 2;
+        }
+        return m[0];
     }
 };
