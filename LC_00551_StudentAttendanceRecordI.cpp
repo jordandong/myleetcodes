3e43f6bb3d06e8c41d2e6af39e8aa6e054a84cea
J.D <jordandong@gmail.com>
2017-04-18 00:05:59 -0700

    Update and rename 00551_StudentAttendanceRecordI.cpp to LC_00551_StudentAttendanceRecordI.cpp

--- LC_00551_StudentAttendanceRecordI.cpp
+++ LC_00551_StudentAttendanceRecordI.cpp
@@ -1,9 +1,10 @@
 /*
-You are given a string representing an attendance record for a student. The record only contains the following three characters:
-
+You are given a string representing an attendance record for a student.
+The record only contains the following three characters:
 'A' : Absent.
 'L' : Late.
 'P' : Present.
+
 A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
 
 You need to return whether the student could be rewarded according to his attendance record.
@@ -11,9 +12,12 @@ You need to return whether the student could be rewarded according to his attend
 Example 1:
 Input: "PPALLP"
 Output: True
+
 Example 2:
 Input: "PPALLL"
 Output: False
+
+Hide Tags String
 */
 
 class Solution {
