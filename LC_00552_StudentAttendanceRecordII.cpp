cd24677f9a3fe4760705ba24e920ccc6111b63f7
J.D <jordandong@gmail.com>
2017-04-18 00:14:45 -0700

    Update and rename 00552_StudentAttendanceRecordII.cpp to LC_00552_StudentAttendanceRecordII.cpp

--- LC_00552_StudentAttendanceRecordII.cpp
+++ LC_00552_StudentAttendanceRecordII.cpp
@@ -1,8 +1,8 @@
 /*
-Given a positive integer n, return the number of all possible attendance records with length n, which will be regarded as rewardable. The answer may be very large, return it after mod 109 + 7.
+Given a positive integer n, return the number of all possible attendance records with length n, which will be regarded as rewardable.
+The answer may be very large, return it after mod 10^9 + 7.
 
 A student attendance record is a string that only contains the following three characters:
-
 'A' : Absent.
 'L' : Late.
 'P' : Present.
@@ -15,7 +15,10 @@ Explanation:
 There are 8 records with length 2 will be regarded as rewardable:
 "PP" , "AP", "PA", "LP", "PL", "AL", "LA", "LL"
 Only "AA" won't be regarded as rewardable owing to more than one absent times. 
+
 Note: The value of n won't exceed 100,000.
+
+Hide Tags Dynamic Programming
 */
 
 class Solution {
