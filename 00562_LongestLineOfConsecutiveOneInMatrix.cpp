59a377a297fbff9f5095adcd8eca5da439c25637
J.D <jordandong@gmail.com>
2017-04-22 22:18:13 -0700

    Update 00562_LongestLineOfConsecutiveOneInMatrix.cpp

--- 00562_LongestLineOfConsecutiveOneInMatrix.cpp
+++ 00562_LongestLineOfConsecutiveOneInMatrix.cpp
@@ -13,6 +13,38 @@ Hint: The number of elements in the given matrix will not exceed 10,000.
 class Solution {
 public:
     int longestLine(vector<vector<int>>& M) {
-        
+        int R = M.size();
+        if (R == 0)
+            return 0;
+        int C = M[0].size();
+        if (C == 0)
+            return 0;
+        vector<int> col(C, 0), diag(C, 0), anti_diag(C, 0);
+        int row = 0, mx = 0;
+        for (int i = 0 ; i < R; i++) {
+            row = 0;
+            for (int j = 0; j < C; j++) {
+                if (M[i][j] == 0) {
+                    row = 0;
+                    col[j] = 0;
+                    anti_diag[j] = 0;
+                } else {
+                    row++;
+                    col[j]++;
+                    anti_diag[j] = ((j == (C - 1)) ? 1 : anti_diag[j + 1] + 1);
+                }
+                mx = max(mx, max(row, max(col[j], anti_diag[j])));
+            }
+            for (int j = C - 1; j >= 0; j--) {
+                if (M[i][j] == 0) {
+                    diag[j] = 0;
+                } else {
+                    diag[j] = ((j == 0) ? 1 : diag[j - 1] + 1);
+                }
+                mx = max(mx, diag[j]);
+            }
+
+        }
+        return mx;
     }
 };
