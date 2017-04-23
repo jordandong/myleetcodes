5aa5a9cc150b8b640ab4101c0502bb9a409aa3cb
J.D <jordandong@gmail.com>
2017-04-23 00:29:15 -0700

    Update 00564_FindTheClosestPalindrome.cpp

--- 00564_FindTheClosestPalindrome.cpp
+++ 00564_FindTheClosestPalindrome.cpp
@@ -15,74 +15,73 @@ class Solution {
 public:
     string nearestPalindromic(string n) {
         string res = n;
+        long long num = atoll(n.c_str());
         int N = n.length();
         if (N == 0)
-            return "";
+            return res;
         if (N == 1) {
-            if (n == "0") {
-                res = "1";
-            } else {
-                res[0] -= 1;
-            }
+            res[0] = (n == "0") ? '1': res[0] - 1;
             return res;
         }
             
-            
-        vector<long long> cand;
-        long long num = atoll(n.c_str());
-        cand.push_back((long long)pow(10.0, N*1.0) + 1);
-        cand.push_back((long long)pow(10.0, (N - 1)*1.0) - 1);
-        string half = res.substr(0, (N + 1)/2);
-        string tmp;
+        vector<long long> candidates;
+        int half_size = (N + 1)/2;
+        string half = n.substr(0, half_size);
+        string tmp, rev_tmp, candidate;
+        
+        candidates.push_back((long long)pow(10.0, N*1.0) + 1); //10000...00001
+        candidates.push_back((long long)pow(10.0, (N - 1)*1.0) - 1); //999...999
+        
         if (N % 2) {
             tmp = half.substr(0, half.length() - 1);
-            string c;
-            if (half.back() != '0') {
-               c = tmp + to_string(half.back() - '0' - 1) + rev_str(tmp);
-               cand.push_back(atoll(c.c_str()));
+            rev_tmp = rev_str(tmp);
+            candidate = tmp + half.substr(half_size - 1) + rev_tmp;
+        
+            candidates.push_back(atoll(candidate.c_str())); //same
+            
+            if (candidate[half_size - 1] != '9') {
+                candidate[half_size - 1] += 1;
+                candidates.push_back(atoll(candidate.c_str())); //bigger
+                candidate[half_size - 1] -= 1;
             }
             
-            tmp = half.substr(0, half.length() - 1);
-            c = tmp + to_string(half.back() - '0' + 1) + rev_str(tmp);
-            cand.push_back(atoll(c.c_str()));
-            
-            tmp = half.substr(0, half.length() - 1);
-            c = tmp + to_string(half.back() - '0') + rev_str(tmp);
-            cand.push_back(atoll(c.c_str()));
+            if (candidate[half_size - 1] != '0') {
+                candidate[half_size - 1] -= 1;
+                candidates.push_back(atoll(candidate.c_str())); //smaller
+            }
+        
         } else {
             tmp = half;
-            string c;
-            if (tmp.back() != '0') {
-               tmp.back() = tmp.back() - 1;
-               c = tmp + rev_str(tmp);
-               cand.push_back(atoll(c.c_str()));
+            rev_tmp = rev_str(tmp);
+            candidate = tmp + rev_tmp;
+        
+            candidates.push_back(atoll(candidate.c_str())); //same
+            
+            if (candidate[half_size - 1] != '9') {
+                candidate[half_size - 1] += 1;
+                candidate[half_size ] += 1;
+                candidates.push_back(atoll(candidate.c_str())); //bigger
+                candidate[half_size - 1] -= 1;
+                candidate[half_size ] -= 1;
             }
-            
-            tmp = half;
-            tmp.back() = tmp.back() + 1;
-            c = tmp + rev_str(tmp);
-            cand.push_back(atoll(c.c_str()));
-            
-            tmp = half;
-            tmp.back() = tmp.back();
-            c = tmp + rev_str(tmp);
-            cand.push_back(atoll(c.c_str()));
+                
+            if (candidate[half_size - 1] != '0') {
+                candidate[half_size - 1] -= 1;
+                candidate[half_size] -= 1;
+                candidates.push_back(atoll(candidate.c_str())); //smaller
+            }
         }
         
-        long long delta = num;
-        long long mi = num;
-        for (int i = 0; i < cand.size(); i++) {
-            if (cand[i] != num) {
-                if (abs(cand[i] - num) < delta) {
-                    res = to_string(cand[i]);
-                    mi = cand[i];
-                    delta = abs(cand[i] - num);
-                } else if (abs(cand[i] - num) == delta && cand[i] < mi) {
-                    res = to_string(cand[i]);
-                    mi = cand[i];
+        long long delta = num, mi = num;
+        for (int i = 0; i < candidates.size(); i++) {
+            if (candidates[i] != num) {
+                long long diff = abs(candidates[i] - num);
+                if (diff < delta || (diff == delta && candidates[i] < mi)) {
+                    res = to_string(candidates[i]);
+                    mi = candidates[i];
+                    delta = diff;
                 }
             }
-            cout<<cand[i]<<endl;
         }
         return res;
     }
