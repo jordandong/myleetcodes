13903a7ad74e3aa9a7496831622d2d90a48a754f
J.D <jordandong@gmail.com>
2017-04-22 20:41:25 -0700

    Update 00561_ArrayPartitionI.cpp

--- 00561_ArrayPartitionI.cpp
+++ 00561_ArrayPartitionI.cpp
@@ -14,6 +14,12 @@ All the integers in the array will be in the range of [-10000, 10000].
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if ((i % 2) == 0)
+                res += nums[i];
+        }
+        return res;
     }
 };
