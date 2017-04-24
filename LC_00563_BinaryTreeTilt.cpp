2bb93e2e560b71394672b21e8a79550de230c446
J.D <jordandong@gmail.com>
2017-04-24 12:25:05 +0800

    Update and rename 00563_BinaryTreeTilt.cpp to LC_00563_BinaryTreeTilt.cpp

--- LC_00563_BinaryTreeTilt.cpp
+++ LC_00563_BinaryTreeTilt.cpp
@@ -1,7 +1,8 @@
 /*
 Given a binary tree, return the tilt of the whole tree.
 
-The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values.
+Null node has tilt 0.
 
 The tilt of the whole tree is defined as the sum of all nodes' tilt.
 
@@ -16,10 +17,12 @@ Tilt of node 2 : 0
 Tilt of node 3 : 0
 Tilt of node 1 : |2-3| = 1
 Tilt of binary tree : 0 + 0 + 1 = 1
-Note:
 
-The sum of node values in any subtree won't exceed the range of 32-bit integer.
-All the tilt values won't exceed the range of 32-bit integer.
+Note:
+1. The sum of node values in any subtree won't exceed the range of 32-bit integer.
+2. All the tilt values won't exceed the range of 32-bit integer.
+
+Hide Tags Tree
 */
 
 /**
