84c20dadbc1e09539c30760140844f030fb49c51
J.D <jordandong@gmail.com>
2017-04-30 14:31:55 -0700

    Update and rename 00560_SubarraySumEqualsK.cpp to LC_00560_SubarraySumEqualsK.cpp

--- LC_00560_SubarraySumEqualsK.cpp
+++ LC_00560_SubarraySumEqualsK.cpp
@@ -4,16 +4,18 @@ Given an array of integers and an integer k, you need to find the total number o
 Example 1:
 Input:nums = [1,1,1], k = 2
 Output: 2
+
 Note:
-The length of the array is in range [1, 20,000].
-The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+1. The length of the array is in range [1, 20,000].
+2. The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+Hide Tags Array
 */
 
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int res = 0;
-        int s = 0;
+        int res = 0, s = 0;
         unordered_map<int, int> s_cnt;
         for (auto e : nums) {
             s_cnt[s]++;
