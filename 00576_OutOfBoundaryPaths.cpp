da6bc1d1defa53bc2693e519650585758abe7791
J.D <jordandong@gmail.com>
2017-05-07 01:09:07 -0700

    Update 00576_OutOfBoundaryPaths.cpp

--- 00576_OutOfBoundaryPaths.cpp
+++ 00576_OutOfBoundaryPaths.cpp
@@ -20,6 +20,18 @@ N is in range [0,50].
 class Solution {
 public:
     int findPaths(int m, int n, int N, int i, int j) {
-        
+        int dp[51][50][50] = {};
+        for (auto Ni = 1; Ni <= N; ++ Ni) {
+            for (auto mi = 0; mi < m; ++mi) {
+                for (auto ni = 0; ni < n; ++ni) {
+                    dp[Ni][mi][ni] = ((long long)(mi == 0 ? 1 : dp[Ni - 1][mi - 1][ni]) + //up
+                                                 (mi == m - 1? 1 : dp[Ni - 1][mi + 1][ni]) + //down
+                                                 (ni == 0 ? 1 : dp[Ni - 1][mi][ni - 1]) + //left
+                                                 (ni == n - 1 ? 1 : dp[Ni - 1][mi][ni + 1])) //right
+                                                 % 1000000007;
+                }
+            }
+        }
+        return dp[N][i][j];
     }
 };
