effd9717773e45939fbc66fd2a89ef80b79e0926
J.D <jordandong@gmail.com>
2017-05-08 17:29:10 -0700

    Update and rename 00573_SquirrelSimulation.cpp to LC_00573_SquirrelSimulation.cpp

--- LC_00573_SquirrelSimulation.cpp
+++ LC_00573_SquirrelSimulation.cpp
@@ -1,23 +1,34 @@
 /*
-There's a tree, a squirrel, and several nuts. Positions are represented by the cells in a 2D grid. Your goal is to find the minimal distance for the squirrel to collect all the nuts and put them under the tree one by one. The squirrel can only take at most one nut at one time and can move in four directions - up, down, left and right, to the adjacent cell. The distance is represented by the number of moves.
+There's a tree, a squirrel, and several nuts. Positions are represented by the cells in a 2D grid.
+Your goal is to find the minimal distance for the squirrel to collect all the nuts and put them under the tree one by one.
+The squirrel can only take at most one nut at one time and can move in four directions - up, down, left and right, to the adjacent cell.
+The distance is represented by the number of moves.
 
 Example 1:
-Input: 
+Input:
+
 Height : 5
 Width : 7
 Tree position : [2,2]
 Squirrel : [4,4]
 Nuts : [[3,0], [2,5]]
+
 Output: 12
+
 Explanation:
+  0 1 2 3 4 5 6
+0 - - - - - - -
+1 - - - - - - -
+2 * * T * * N -
+3 N * * - - * -
+4 - - - - S * -
 
 Note:
-All given positions won't overlap.
-The squirrel can take at most one nut at one time.
-The given positions of nuts have no order.
-Height and width are positive integers. 3 <= height * width <= 10,000.
-The given positions contain at least one nut, only one tree and one squirrel.
-Discuss
+1. All given positions won't overlap.
+2. The squirrel can take at most one nut at one time.
+3. The given positions of nuts have no order.
+4. Height and width are positive integers. 3 <= height * width <= 10,000.
+5. The given positions contain at least one nut, only one tree and one squirrel.
 */
 
 class Solution {
