3101ee0a249bfe443a81de7f325aee8e381665f9
J.D <jordandong@gmail.com>
2017-05-10 12:44:31 -0700

    Create 00584_FindCustomerReferee.sql

---
+++ 00584_FindCustomerReferee.sql
@@ -0,0 +1,30 @@
+/*
+Description
+
+Given these contents of the customer table:
+Input
++------+------+-----------+
+| id   | name | referee_id|
++------+------+-----------+
+|    1 | Will |      NULL |
+|    2 | Jane |      NULL |
+|    3 | Alex |         2 |
+|    4 | Bill |      NULL |
+|    5 | Zack |         1 |
+|    6 | Mark |         2 |
++------+------+-----------+
+
+Write a query to return the list of customers not referred by Jane.
+
+Output
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+*/
+
+# Write your MySQL query statement below
