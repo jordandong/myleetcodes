696d9a5f9e32bddfb9e553490210c7dac89ad511
J.D <jordandong@gmail.com>
2017-05-15 18:58:05 -0700

    Update and rename 00582_KillProcess.cpp to LC_00582_KillProcess.cpp

--- LC_00582_KillProcess.cpp
+++ LC_00582_KillProcess.cpp
@@ -31,9 +31,48 @@ Note:
 Hide Tags Tree Queue
 */
 
+//BFS
 class Solution {
 public:
     vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
-        
+        vector<int> killed;
+        map<int, set<int>> children;
+        for (int i = 0; i < pid.size(); i++) {
+            children[ppid[i]].insert(pid[i]);
+        }
+
+        queue<int> q;
+        q.push(kill);
+        while (!q.empty()) {
+            int p = q.front();
+            q.pop();
+            killed.push_back(p);
+            for (int child : children[p]) {
+                q.push(child);
+            }
+        }
+        return killed;
+    }
+};
+
+//DFS
+class Solution {
+public:
+    vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
+        vector<int> killed;
+        map<int, set<int>> children;
+        for (int i = 0; i < pid.size(); i++) {
+            children[ppid[i]].insert(pid[i]);
+        }
+        killAll(kill, children, killed);
+        return killed;
+    }
+
+private:
+    void killAll(int pid, map<int, set<int>>& children, vector<int>& killed) {
+        killed.push_back(pid);
+        for (int child : children[pid]) {
+            killAll(child, children, killed);
+        }
     }
 };
