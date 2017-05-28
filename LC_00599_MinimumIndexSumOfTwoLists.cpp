4dcd316aeffa4c7381a963eacf34dbd3546c9603
J.D <jordandong@gmail.com>
2017-05-28 16:47:21 -0700

    Update and rename 00599_MinimumIndexSumOfTwoLists.cpp to LC_00599_MinimumIndexSumOfTwoLists.cpp

--- LC_00599_MinimumIndexSumOfTwoLists.cpp
+++ LC_00599_MinimumIndexSumOfTwoLists.cpp
@@ -1,7 +1,9 @@
 /*
 Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
 
-You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+You need to help them find out their common interest with the least list index sum.
+If there is a choice tie between answers, output all of them with no order requirement.
+You could assume there always exists an answer.
 
 Example 1:
 Input:
@@ -9,17 +11,21 @@ Input:
 ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
 Output: ["Shogun"]
 Explanation: The only restaurant they both like is "Shogun".
+
 Example 2:
 Input:
 ["Shogun", "Tapioca Express", "Burger King", "KFC"]
 ["KFC", "Shogun", "Burger King"]
 Output: ["Shogun"]
 Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+
 Note:
-The length of both lists will be in the range of [1, 1000].
-The length of strings in both lists will be in the range of [1, 30].
-The index is starting from 0 to the list length minus 1.
-No duplicates in both lists.
+1. The length of both lists will be in the range of [1, 1000].
+2. The length of strings in both lists will be in the range of [1, 30].
+3. The index is starting from 0 to the list length minus 1.
+4. No duplicates in both lists.
+
+Hide Tags Hash Table
 */
 
 class Solution {
