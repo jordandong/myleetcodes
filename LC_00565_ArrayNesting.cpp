0ae9c8478837a345de9d973bce5deaa02ca178fb
J.D <jordandong@gmail.com>
2017-05-28 16:37:35 -0700

    Update and rename 00565_ArrayNesting.cpp to LC_00565_ArrayNesting.cpp

--- LC_00565_ArrayNesting.cpp
+++ LC_00565_ArrayNesting.cpp
@@ -2,7 +2,6 @@
 A zero-indexed array A consisting of N different integers is given. The array contains all integers in the range [0, N - 1].
 
 Sets S[K] for 0 <= K < N are defined as follows:
-
 S[K] = { A[K], A[A[K]], A[A[A[K]]], ... }.
 
 Sets S[K] are finite for each K and should NOT contain duplicates.
@@ -14,13 +13,15 @@ Input: A = [5,4,0,3,1,6,2]
 Output: 4
 Explanation: 
 A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4] = 1, A[5] = 6, A[6] = 2.
-
 One of the longest S[K]:
 S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
+
 Note:
-N is an integer within the range [1, 20,000].
-The elements of A are all distinct.
-Each element of array A is an integer within the range [0, N-1].
+1. N is an integer within the range [1, 20,000].
+2. The elements of A are all distinct.
+3. Each element of array A is an integer within the range [0, N-1].
+
+Hide Tags Array
 */
 
 class Solution {
