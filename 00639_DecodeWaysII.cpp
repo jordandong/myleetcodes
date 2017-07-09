f647bbf04c527e9e32cbfff231cdf30f3ebce249
J.D <jordandong@gmail.com>
2017-07-09 16:16:41 -0700

    Update 00639_DecodeWaysII.cpp

--- 00639_DecodeWaysII.cpp
+++ 00639_DecodeWaysII.cpp
@@ -26,6 +26,23 @@ The input string will only contain the character '*' and digits '0' - '9'.
 class Solution {
 public:
     int numDecodings(string s) {
-        
+        int MOD = 1000000007;
+        long e_any = 1, e_1 = 0, e_2 = 0;
+        long f_any, f_1, f_2;
+        for (auto  c : s) {
+            if (c == '*') {
+                f_any = 9 * e_any + 9 * e_1 + 6 * e_2; //use * as one digit + 1* + 2*
+                f_1 = e_any; //xxx1, 1 is not used
+                f_2 = e_any; //xxx2, 2 is not used
+            } else {
+                f_any = (c > '0') * e_any + e_1 + (c <= '6') * e_2; //user num as one digit + 1* + 2*
+                f_1 = (c == '1') * e_any; //xxx1, 1 is not used
+                f_2 = (c == '2') * e_any; //xxx2, 2 is not used
+            }
+            e_any = f_any % MOD;
+            e_1 = f_1;
+            e_2 = f_2;
+        }
+        return e_any;
     }
 };
