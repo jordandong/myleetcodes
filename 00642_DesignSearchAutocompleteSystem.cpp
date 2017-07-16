e9d7d136bc9087254eccbfe9fd985c978f4bd4d7
J.D <jordandong@gmail.com>
2017-07-15 20:22:26 -0700

    Update 00642_DesignSearchAutocompleteSystem.cpp

--- 00642_DesignSearchAutocompleteSystem.cpp
+++ 00642_DesignSearchAutocompleteSystem.cpp
@@ -55,12 +55,42 @@ Please remember to RESET your class variables declared in class AutocompleteSyst
 class AutocompleteSystem {
 public:
     AutocompleteSystem(vector<string> sentences, vector<int> times) {
-        
+        int n = sentences.size();
+        for (int i = 0; i < n; i++)
+            mp[sentences[i]] = times[i];
     }
     
     vector<string> input(char c) {
+        if (c == '#') {
+            if (!buffer.empty()) {
+                mp[buffer]++;
+                buffer = "";
+            }
+            return {};
+        }
         
+        buffer += c;
+        vector<pair<int, string> > res;
+        auto pa = mp.lower_bound(buffer);
+        auto pb = mp.upper_bound(buffer + "|");
+        for (auto it = pa; it != pb; ++it)
+            res.push_back({-it->second, it->first});
+        sort(res.begin(), res.end());
+        
+        vector<string> ans;
+        int cnt = 0;
+        for (const auto& item: res) {
+            ans.push_back(item.second);
+            cnt++;
+            if (cnt >= 3) {
+                break;
+            }
+        }
+        return ans;
     }
+private:
+    string buffer;
+    map<string, int> mp;
 };
 
 /**
