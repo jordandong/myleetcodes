8d4aef9f6e5fd3fba8f883b32d801e9106bb4532
J.D <jordandong@gmail.com>
2017-07-16 16:24:45 -0700

    Update and rename 00644_MaximumAverageSubarrayII.cpp to LC_00644_MaximumAverageSubarrayII.cpp

--- LC_00644_MaximumAverageSubarrayII.cpp
+++ LC_00644_MaximumAverageSubarrayII.cpp
@@ -1,5 +1,6 @@
 /*
-Given an array consisting of n integers, find the contiguous subarray whose length is greater than or equal to k that has the maximum average value. And you need to output the maximum average value.
+Given an array consisting of n integers, find the contiguous subarray whose length is greater than or equal to k that has the maximum average value.
+And you need to output the maximum average value.
 
 Example 1:
 Input: [1,12,-5,-6,50,3], k = 4
@@ -8,10 +9,11 @@ Explanation:
 when length is 5, maximum average value is 10.8,
 when length is 6, maximum average value is 9.16667.
 Thus return 12.75.
+
 Note:
-1 <= k <= n <= 10,000.
-Elements of the given array will be in range [-10,000, 10,000].
-The answer with the calculation error less than 10-5 will be accepted.
+1. 1 <= k <= n <= 10,000.
+2. Elements of the given array will be in range [-10,000, 10,000].
+3. The answer with the calculation error less than 10-5 will be accepted.
 */
 
 class Solution {
@@ -29,7 +31,7 @@ public:
         vector<double> sum(N + 1, 0);
         while (r - l >= 1e-6) {
             double mid = (l + r) / 2.0; // binary search
-            double min_pre = 0; //
+            double min_pre = 0;
             bool check = false;
             for (int i = 1; i <= N; ++i) {
                 sum[i] = sum[i - 1] + nums[i - 1] - mid;
