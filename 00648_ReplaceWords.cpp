dbe69d1da31f4cdb8ad8782550a26a7f19252ce1
J.D <jordandong@gmail.com>
2017-07-22 19:34:55 -0700

    Create 00648_ReplaceWords.cpp

---
+++ 00648_ReplaceWords.cpp
@@ -0,0 +1,46 @@
+/*
+In English, we have a concept called root, which can be followed by some other words to form another longer word - let's call this word successor. For example, the root an, followed by other, which can form another word another.
+
+Now, given a dictionary consisting of many roots and a sentence. You need to replace all the successor in the sentence with the root forming it. If a successor has many roots can form it, replace it with the root with the shortest length.
+
+You need to output the sentence after the replacement.
+
+Example 1:
+Input: dict = ["cat", "bat", "rat"]
+sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+Note:
+The input will only have lower-case letters.
+1 <= dict words number <= 1000
+1 <= sentence words number <= 1000
+1 <= root length <= 100
+1 <= sentence words length <= 1000
+*/
+
+class Solution {
+public:
+    string replaceWords(vector<string>& dict, string sentence) {
+        unordered_map<string, bool> mp;
+        for (auto &d : dict)
+            mp[d] = true;
+        string res;
+        for (int i = 0, j = 0; j <= sentence.length(); j++) {
+            if (j == sentence.length() || sentence[j] == ' ') {
+                res += sentence.substr(i, j - i + 1);
+                i = j + 1;
+                continue;
+            }
+            string h = sentence.substr(i, j - i + 1);
+            if (mp.find(h) != mp.end()) {
+                res += h;
+                while (j < sentence.length() && sentence[j] != ' ') {
+                    j++;
+                }
+                if (j != sentence.length())
+                    res += " ";
+                i = j + 1;
+            }
+        }
+        return res;
+    }
+};
