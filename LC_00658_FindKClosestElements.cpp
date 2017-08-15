ddc1100a3eb95781e1a2ecf36b2853956d7f6019
J.D <jordandong@gmail.com>
2017-08-14 17:26:01 -0700

    Update LC_00658_FindKClosestElements.cpp

--- LC_00658_FindKClosestElements.cpp
+++ LC_00658_FindKClosestElements.cpp
@@ -31,7 +31,7 @@ public:
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-	    int lo = 0, hi = arr.size() - k;
+        int lo = 0, hi = arr.size() - k;
         while (lo < hi) {
             int mid = (lo + hi) / 2;
             if (x - arr[mid] > arr[mid + k] - x)
