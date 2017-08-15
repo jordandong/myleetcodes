7ceaeceaf26ac5e3b3cb0b49e3a44dcc11ad0c02
J.D <jordandong@gmail.com>
2017-08-14 18:00:23 -0700

    Update and rename 00659_SplitArrayIntoConsecutiveSubsequences.cpp to LC_00659_SplitArrayIntoConsecutiveSubsequences.cpp

--- LC_00659_SplitArrayIntoConsecutiveSubsequences.cpp
+++ LC_00659_SplitArrayIntoConsecutiveSubsequences.cpp
@@ -1,5 +1,7 @@
 /*
-You are given an integer array sorted in ascending order (may contain duplicates), you need to split them into several subsequences, where each subsequences consist of at least 3 consecutive integers. Return whether you can make such a split.
+You are given an integer array sorted in ascending order (may contain duplicates),
+you need to split them into several subsequences, where each subsequences consist of at least 3 consecutive integers.
+Return whether you can make such a split.
 
 Example 1:
 Input: [1,2,3,3,4,5]
@@ -8,6 +10,7 @@ Explanation:
 You can split them into two consecutive subsequences : 
 1, 2, 3
 3, 4, 5
+
 Example 2:
 Input: [1,2,3,3,4,4,5,5]
 Output: True
@@ -15,13 +18,16 @@ Explanation:
 You can split them into two consecutive subsequences : 
 1, 2, 3, 4, 5
 3, 4, 5
+
 Example 3:
 Input: [1,2,3,4,4,5]
 Output: False
+
 Note:
-The length of the input is in range of [1, 10000]
+1. The length of the input is in range of [1, 10000]
 */
 
+//T:O(N^2) , S:O(N)
 class Solution {
 public:
     bool isPossible(vector<int>& nums) {
@@ -45,6 +51,32 @@ public:
         for (int j = 0; j < tbl.size(); j++) {
             if (tbl[j].second < 3)
                 return false;
+        }
+        return true;
+    }
+};
+
+//T:O(N) , S:O(N)
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> cnt, ends;
+        for (auto e : nums)
+            cnt[e]++;
+        for (auto e : nums) {
+            if (cnt[e] == 0)
+                continue;
+            if (ends[e-1]) { //append to the end
+                ends[e-1]--;
+                ends[e]++;
+            } else if (cnt[e+1] && cnt[e+2]) { //add new subseq
+                cnt[e+1]--;
+                cnt[e+2]--;
+                ends[e+2]++;
+            } else {
+                return false;
+            }
+            cnt[e]--;
         }
         return true;
     }
