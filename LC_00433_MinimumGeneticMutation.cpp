813192d1a931e5d6982aafe8ce0ec4886c1101c2
J.D <jordandong@gmail.com>
2017-08-22 00:40:48 -0700

    Update and rename 00433_MinimumGeneticMutation.cpp to LC_00433_MinimumGeneticMutation.cpp

--- LC_00433_MinimumGeneticMutation.cpp
+++ LC_00433_MinimumGeneticMutation.cpp
@@ -1,22 +1,37 @@
 /*
-A gene string can be represented by an 8-character long string, with choices from "A","C","G","T". 
-Suppose we need to investigate about a mutation (mutation from "start" to "end"), where ONE mutation is defined as ONE single character changed in the gene string. 
-For example, "AACCGGTT" -> "AACCGGTA" is 1 mutation. 
+A gene string can be represented by an 8-character long string, with choices from "A", "C", "G", "T".
+
+Suppose we need to investigate about a mutation (mutation from "start" to "end"),
+where ONE mutation is defined as ONE single character changed in the gene string.
+
+For example, "AACCGGTT" -> "AACCGGTA" is 1 mutation.
+
 Also, there is a given gene "bank", which records all the valid gene mutations.
-A gene must be in the bank to make it a valid gene string. 
+A gene must be in the bank to make it a valid gene string.
 
-Now, given 3 things - start, end, bank, your task is to determine what is the minimum number of mutations needed to mutate from "start" to "end". 
-If there is no such a mutation, return -1. 
+Now, given 3 things - start, end, bank, your task is to determine what is the minimum number of mutations needed to mutate from "start" to "end". If there is no such a mutation, return -1.
 
-For example, 
-bank: "AACCGGTA" 
-start: "AACCGGTT" 
-end: "AACCGGTA" 
+Note:
+1. Starting point is assumed to be valid, so it might not be included in the bank.
+2. If multiple mutations are needed, all mutations during in the sequence must be valid.
+3. You may assume start and end string is not the same.
+
+Example 1:
+start: "AACCGGTT"
+end:   "AACCGGTA"
+bank: ["AACCGGTA"]
 return: 1
 
-bank: "AACCGGTA", "AACCGCTA", "AAACGGTA"
+Example 2:
 start: "AACCGGTT"
-end: "AAACGGTA"
+end:   "AAACGGTA"
+bank: ["AACCGGTA", "AACCGCTA", "AAACGGTA"]
+return: 2
+
+Example 3:
+start: "AAAAACCC"
+end:   "AACCCCCC"
+bank: ["AAAACCCC", "AAACCCCC", "AACCCCCC"]
 return: 3
 */
 
