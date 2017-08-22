3fbd32b672756d39fc68895a481ff1c0819a1bf1
J.D <jordandong@gmail.com>
2017-08-21 23:47:10 -0700

    Update and rename 00662_MaximumWidthOfBinaryTree.cpp to LC_00662_MaximumWidthOfBinaryTree.cpp

--- LC_00662_MaximumWidthOfBinaryTree.cpp
+++ LC_00662_MaximumWidthOfBinaryTree.cpp
@@ -1,11 +1,12 @@
 /*
-Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels.
+The binary tree has the same structure as a full binary tree, but some nodes are null.
 
-The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+The width of one level is defined as the length between the end-nodes
+(the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation).
 
 Example 1:
 Input: 
-
            1
          /   \
         3     2
@@ -14,9 +15,9 @@ Input:
 
 Output: 4
 Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+
 Example 2:
 Input: 
-
           1
          /  
         3    
@@ -25,9 +26,9 @@ Input:
 
 Output: 2
 Explanation: The maximum width existing in the third level with the length 2 (5,3).
+
 Example 3:
 Input: 
-
           1
          / \
         3   2 
@@ -36,9 +37,9 @@ Input:
 
 Output: 2
 Explanation: The maximum width existing in the second level with the length 2 (3,2).
+
 Example 4:
 Input: 
-
           1
          / \
         3   2
@@ -48,7 +49,6 @@ Input:
     6           7
 Output: 8
 Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
-
 
 Note: Answer will in the range of 32-bit signed integer.
 */
@@ -66,16 +66,14 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         unordered_map<int, int> l, r;
-        widthOfBinaryTreeHelper(root, 1, 1, l, r);
         int res = 0;
-        for (auto p : l) {
-            res = max(res, r[p.first] - p.second + 1);
-        }
+        widthOfBinaryTreeHelper(root, 1, 1, l, r, res);
         return res;
     }
-    
+
+private:
     void widthOfBinaryTreeHelper(TreeNode* root, int lv, int idx, 
-                                unordered_map<int, int> &l, unordered_map<int, int> &r) {
+                                unordered_map<int, int> &l, unordered_map<int, int> &r, int &res) {
         if (!root)
             return;
         
@@ -91,35 +89,11 @@ public:
             r[lv] = idx;    
         }
         
-        widthOfBinaryTreeHelper(root->left, lv + 1, idx * 2, l, r);
-        widthOfBinaryTreeHelper(root->right, lv + 1, idx * 2 + 1, l, r);
-    }  
-};
-
-class Solution {
-public:
-    int widthOfBinaryTree(TreeNode* root) {
-        int res = 0, idx = 0, lo = INT_MAX;
-        queue<pair<TreeNode*, int>> q[2];
+        res = max(res, r[lv] - l[lv] + 1);
         
-        q[idx % 2].push({root, 1});
-        while (!q[idx % 2].empty()) {
-            TreeNode* t = q[idx % 2].front().first;
-            int seq = q[idx % 2].front().second;
-            q[idx % 2].pop();
-            if (t->left)
-                q[(idx + 1) % 2].push({t->left, 2 * seq });
-            if (t->right)
-                q[(idx + 1) % 2].push({t->right, 2 * seq + 1});
-            lo = min(lo, seq);
-            res = max(res, seq - lo + 1);
-            if (q[idx % 2].empty()) {
-                idx++;
-                lo = INT_MAX;
-            }
-        }
-        return res;
-    }    
+        widthOfBinaryTreeHelper(root->left, lv + 1, idx * 2, l, r, res);
+        widthOfBinaryTreeHelper(root->right, lv + 1, idx * 2 + 1, l, r, res);
+    }  
 };
 
 class Solution {
