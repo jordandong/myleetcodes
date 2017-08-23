9ccca68febc3f2d4bc2a8f3cf93c4c644ea44bf8
J.D <jordandong@gmail.com>
2017-08-22 20:00:27 -0700

    Update and rename 00518_CoinChange2.cpp to LC_00518_CoinChange2.cpp

--- LC_00518_CoinChange2.cpp
+++ LC_00518_CoinChange2.cpp
@@ -1,14 +1,15 @@
 /*
-You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+You are given coins of different denominations and a total amount of money.
+Write a function to compute the number of combinations that make up that amount.
+You may assume that you have infinite number of each kind of coin.
 
 Note: You can assume that
+1. 0 <= amount <= 5000
+2. 1 <= coin <= 5000
+3. the number of coins is less than 500
+4. the answer is guaranteed to fit into signed 32-bit integer
 
-0 <= amount <= 5000
-1 <= coin <= 5000
-the number of coins is less than 500
-the answer is guaranteed to fit into signed 32-bit integer
 Example 1:
-
 Input: amount = 5, coins = [1, 2, 5]
 Output: 4
 Explanation: there are four ways to make up the amount:
@@ -16,23 +17,20 @@ Explanation: there are four ways to make up the amount:
 5=2+2+1
 5=2+1+1+1
 5=1+1+1+1+1
-Example 2:
 
+Example 2:
 Input: amount = 3, coins = [2]
 Output: 0
 Explanation: the amount of 3 cannot be made up just with coins of 2.
-Example 3:
 
+Example 3:
 Input: amount = 10, coins = [10] 
 Output: 1
-Subscribe to see which companies asked this question.
 */
 
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        if (amount == 0)
-            return 1;
         vector<int> dp(amount + 1, 0);
         dp[0] = 1;
         for (int i = 0; i < coins.size(); ++i) {
