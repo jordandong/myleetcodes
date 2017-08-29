55facde795e873dde91a61af0fbc247f0908079f
J.D <jordandong@gmail.com>
2017-08-28 22:00:56 -0700

    Update and rename 00668_KthSmallestNumberInMultiplicationTable.cpp to LC_00668_KthSmallestNumberInMultiplicationTable.cpp

--- LC_00668_KthSmallestNumberInMultiplicationTable.cpp
+++ LC_00668_KthSmallestNumberInMultiplicationTable.cpp
@@ -1,5 +1,6 @@
 /*
-Nearly every one have used the Multiplication Table. But could you find out the k-th smallest number quickly from the multiplication table?
+Nearly every one have used the Multiplication Table.
+But could you find out the k-th smallest number quickly from the multiplication table?
 
 Given the height m and the length n of a m * n Multiplication Table, and a positive integer k, you need to return the k-th smallest number in this table.
 
@@ -11,8 +12,8 @@ The Multiplication Table:
 1	2	3
 2	4	6
 3	6	9
-
 The 5-th smallest number is 3 (1, 2, 2, 3, 3).
+
 Example 2:
 Input: m = 2, n = 3, k = 6
 Output: 
@@ -20,11 +21,11 @@ Explanation:
 The Multiplication Table:
 1	2	3
 2	4	6
-
 The 6-th smallest number is 6 (1, 2, 2, 3, 4, 6).
+
 Note:
-The m and n will be in the range [1, 30000].
-The k will be in the range [1, m * n]
+1. The m and n will be in the range [1, 30000].
+2. The k will be in the range [1, m * n].
 */
 
 class Solution {
