d73dd729846c481ff5d70c73d41745bcd5675f4c
J.D <jordandong@gmail.com>
2017-09-02 23:58:03 -0700

    Update 00670_MaximumSwap.cpp

--- 00670_MaximumSwap.cpp
+++ 00670_MaximumSwap.cpp
@@ -16,6 +16,14 @@ The given number is in the range [0, 108]
 class Solution {
 public:
     int maximumSwap(int num) {
-
+        string s = to_string(num);
+        for ( int i = 0; i < s.size(); ++i) {
+            for ( int j = i + 1; j < s.size(); ++j ) {
+                swap(s[i], s[j]);
+                num = max(num, stoi(s));
+                swap(s[i], s[j]);
+            }
+        }
+        return num;   
     }
 };
