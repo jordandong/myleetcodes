27853d29384d196ea82fd84ee1756fce2440ccf6
J.D <jordandong@gmail.com>
2017-09-02 20:02:25 -0700

    Create 00671_SecondMinimumNodeInABinaryTree.cpp

---
+++ 00671_SecondMinimumNodeInABinaryTree.cpp
@@ -0,0 +1,60 @@
+/*
+Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes.
+
+Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+
+If no such second minimum value exists, output -1 instead.
+
+Example 1:
+Input: 
+    2
+   / \
+  2   5
+     / \
+    5   7
+
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+Example 2:
+Input: 
+    2
+   / \
+  2   2
+
+Output: -1
+Explanation: The smallest value is 2, but there isn't any second smallest value.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        int f = -1, s = -1;
+        findSecondMinimumValueHelper(root, f ,s);
+        return s;
+    }
+    
+    void findSecondMinimumValueHelper(TreeNode* root, int &f, int &s) {
+        if (!root)
+            return;
+        findSecondMinimumValueHelper(root->left, f, s);
+        
+        if (f == -1 || root->val <= f) {
+            if (root->val != f)
+                s = f;
+            f = root->val;
+        } else if (s == -1 || root->val < s)
+            s = root->val;
+        
+        findSecondMinimumValueHelper(root->right, f, s);
+        
+    }
+};
