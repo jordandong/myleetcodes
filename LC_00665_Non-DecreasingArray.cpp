540f82a6325d55b289c453235e273ca1a27f8c3a
J.D <jordandong@gmail.com>
2017-09-04 01:47:22 -0700

    Update LC_00665_Non-DecreasingArray.cpp

--- LC_00665_Non-DecreasingArray.cpp
+++ LC_00665_Non-DecreasingArray.cpp
@@ -36,3 +36,22 @@ public:
         return cnt <= 1;
     }
 };
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        bool mod = false;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] < nums[i - 1]) {
+                if (mod)
+                    return false;
+                mod = true;
+                if (i > 1 && nums[i] <= nums[i - 2])
+                    nums[i] = nums[i - 1];
+                //else
+                //    nums[i - 1] = nums[i];
+            }
+        }
+        return true;
+    }
+};
