f1965b2926697a84a1f54932d90ed5a6a857d3da
J.D <jordandong@gmail.com>
2017-09-10 15:57:02 -0700

    Update and rename 00674_LongestContinuousIncreasingSubsequence.cpp to LC_00674_LongestContinuousIncreasingSubsequence.cpp

--- LC_00674_LongestContinuousIncreasingSubsequence.cpp
+++ LC_00674_LongestContinuousIncreasingSubsequence.cpp
@@ -4,11 +4,14 @@ Given an unsorted array of integers, find the length of longest continuous incre
 Example 1:
 Input: [1,3,5,4,7]
 Output: 3
-Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
+Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+
 Example 2:
 Input: [2,2,2,2,2]
 Output: 1
 Explanation: The longest continuous increasing subsequence is [2], its length is 1. 
+
 Note: Length of the array will not exceed 10,000.
 */
 
@@ -18,15 +21,12 @@ public:
         int N = nums.size();
         if (N <= 0)
             return N;
-        int res = 1;
-        int l = 1;
+        int res = 1, l = 1;
         for (int i = 1; i < N; i++) {
-            if (nums[i] > nums[i - 1]) {
-                l++;
-                res = max(res, l);
-            } else {
+            if (nums[i] > nums[i - 1])
+                res = max(res, ++l);
+            else
                 l = 1;
-            }
         }
         return res;
     }
