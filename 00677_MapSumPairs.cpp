00aa3f4d6f6f9770735dba7f02fa4f61eecd8b04
J.D <jordandong@gmail.com>
2017-09-17 00:14:59 -0700

    Update 00677_MapSumPairs.cpp

--- 00677_MapSumPairs.cpp
+++ 00677_MapSumPairs.cpp
@@ -13,18 +13,58 @@ Input: sum("ap"), Output: 5
 */
 
 class MapSum {
+private:
+    struct Node {
+        Node () {
+            memset(nodes, 0, sizeof(nodes));
+            sum = 0;
+            val = 0;
+        }
+        int sum;
+        int val;
+        struct Node *nodes[26];
+    };
+    
+    struct Node* root;
+
 public:
     /** Initialize your data structure here. */
     MapSum() {
-        
+        root = new struct Node();
     }
     
     void insert(string key, int val) {
-        
+        int d = 0;
+        insertHelper(root, key, 0, val, d);
+    }
+    
+    void insertHelper(struct Node *root, string &key, int idx, int val, int &d) {
+        int N = key.size();
+        if (idx >= N)
+            return;
+        int i = key[idx] - 'a';
+        if (!root->nodes[i])
+            root->nodes[i] = new struct Node();
+        if (idx == N - 1) {
+            d = val - root->nodes[i]->val;
+            root->nodes[i]->val = val;
+        } else {
+            insertHelper(root->nodes[i], key, idx + 1, val, d);
+        }
+        root->nodes[i]->sum += d;
     }
     
     int sum(string prefix) {
-        
+        int N = prefix.length();
+        struct Node *node = root;
+        for (int i = 0; i < N; i++) {
+            int c = prefix[i] - 'a';
+            if (node->nodes[c])
+                node = node->nodes[c];
+            else
+                return 0;
+        }
+        return node->sum;
     }
 };
 
