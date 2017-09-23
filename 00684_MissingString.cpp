3468feb3a833b67a911b33dc47cf1a20a616a025
J.D <jordandong@gmail.com>
2017-09-23 12:16:51 -0700

    Create 00684_MissingString.cpp

---
+++ 00684_MissingString.cpp
@@ -0,0 +1,24 @@
+/*
+Given two strings, you have to find the missing string.
+
+Example
+Given a string str1 = This is an example
+Given another string str2 = is example
+
+Return ["This", "an"]
+
+Tags 
+Hash Table
+*/
+
+class Solution {
+public:
+    /*
+     * @param : a given string
+     * @param : another given string
+     * @return: An array of missing string
+     */
+    vector<string> missingString(string str1, string str2) {
+        // Write your code here
+    }
+};
