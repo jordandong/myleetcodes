af080b3572cd08406a3e3253fa34b5cf563b4fd2
J.D <jordandong@gmail.com>
2017-09-23 12:19:06 -0700

    Create 00688_TheNumberInWords.cpp

---
+++ 00688_TheNumberInWords.cpp
@@ -0,0 +1,26 @@
+/*
+Given a non-negative integer n, print the number in words.
+
+ Notice
+
+n <= 2147483647
+
+Example
+Given n = 125
+
+Return one hundred twenty five
+
+Tags 
+Enumeration
+*/
+
+class Solution {
+public:
+    /*
+     * @param : the number
+     * @return: the number in words
+     */
+    string convertWords(int number) {
+        // Write your code here
+    }
+};
