533a715b5ee68fbb59a6718dbe1f144314f0a4a1
J.D <jordandong@gmail.com>
2017-10-28 22:47:54 -0700

    Create 00718_MaximumLengthOfRepeatedSubarray.cpp

---
+++ 00718_MaximumLengthOfRepeatedSubarray.cpp
@@ -0,0 +1,27 @@
+/*
+Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
+
+Example 1:
+Input:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+Output: 3
+Explanation: 
+The repeated subarray with maximum length is [3, 2, 1].
+Note:
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+*/
+
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        int m = A.size(), n = B.size(), res = 0;
+        vector<int> dp(n + 1, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = n - 1; j >= 0; j--)
+                res = max(res, dp[j + 1] = (A[i] == B[j]) ? dp[j] + 1 : 0);
+        }
+        return res;
+    }
+};
