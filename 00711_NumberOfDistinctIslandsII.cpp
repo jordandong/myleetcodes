65a765db8bb30288756096abf997c7207736a41a
J.D <jordandong@gmail.com>
2017-10-29 22:22:17 -0700

    Update 00711_NumberOfDistinctIslandsII.cpp

--- 00711_NumberOfDistinctIslandsII.cpp
+++ 00711_NumberOfDistinctIslandsII.cpp
@@ -98,9 +98,156 @@ are considered same island shapes. Because if we flip the first array in the up/
 Note: The length of each dimension in the given grid does not exceed 50.
 */
 
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
 class Solution {
 public:
     int numDistinctIslands2(vector<vector<int>>& grid) {
-    
+        int m = grid.size();
+        if (m == 0) return 0;
+        int n = grid[0].size();
+        if (n == 0) return 0;
+        
+        std::unordered_set<int> signatures;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c]) {
+                    vector<pair<int, int>> pts;
+                    visit(grid, m, n, r, c, pts);
+                    int hash = compute_hash(pts);
+                    signatures.insert(hash);
+                }
+            }
+        }
+        
+        return signatures.size();
     }
-}
+    
+private:
+    int compute_hash(vector<pair<int, int>>& pts) {
+        int n = pts.size();
+        int hash = 0;
+        // int xmin = INT_MAX, ymin = INT_MAX, xmax = INT_MIN, ymax = INT_MIN;
+        std::unordered_map<int, int> stats1, stats2;
+        
+        for (int i = 0; i < n; ++i) {
+            const auto& pt1 = pts[i];
+            // xmin = std::min(xmin, pt1.first);
+            // ymin = std::min(ymin, pt1.second);
+            // xmax = std::max(xmax, pt1.first);
+            // ymax = std::max(ymax, pt1.second);
+            stats1[pt1.first]++;
+            stats2[pt1.second]++;
+            for (int j = i + 1; j < n; ++j) {
+                const auto& pt2 = pts[j];
+                int delta_x = pt1.first - pt2.first;
+                int delta_y = pt1.second - pt2.second;
+                if (delta_x == 0 || delta_y == 0) {
+                    hash += 19 * (delta_x * delta_x + delta_y * delta_y);
+                } else {
+                    hash += 31 * (delta_x * delta_x + delta_y * delta_y);
+                } 
+            }
+        }
+        
+        for (auto& kv : stats1) {
+            hash += 73 * kv.second * kv.second;
+        }
+        for (auto& kv : stats2) {
+            hash += 73 * kv.second * kv.second;
+        }
+        
+        // int delta_x = xmax - xmin, delta_y = ymax - ymin;
+        int delta_x = stats1.size(), delta_y = stats2.size();
+        hash += 193 * (delta_x * delta_x + delta_y * delta_y) + 97 * delta_x * delta_y;
+        return hash;
+    }
+    
+    void visit(vector<vector<int>>& grid, int m, int n, int r, int c,
+               vector<pair<int, int>>& pts) {
+        bool outOfBound = (r < 0 || r >= m || c < 0 || c >= n);
+        if (outOfBound || grid[r][c] == 0) return;
+        
+        grid[r][c] = 0;
+        pts.emplace_back(r, c);
+        visit(grid, m, n, r - 1, c, pts);
+        visit(grid, m, n, r + 1, c, pts);
+        visit(grid, m, n, r, c - 1, pts);
+        visit(grid, m, n, r, c + 1, pts);
+    }    
+};
