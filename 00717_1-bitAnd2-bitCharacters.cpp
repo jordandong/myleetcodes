af399e4b28d22aee3f267310d879c2d2ed045591
J.D <jordandong@gmail.com>
2017-10-29 20:08:57 -0700

    Update 00717_1-bitAnd2-bitCharacters.cpp

--- 00717_1-bitAnd2-bitCharacters.cpp
+++ 00717_1-bitAnd2-bitCharacters.cpp
@@ -9,6 +9,7 @@ bits = [1, 0, 0]
 Output: True
 Explanation: 
 The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+
 Example 2:
 Input: 
 bits = [1, 1, 1, 0]
