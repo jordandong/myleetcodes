c17fb1db96a8a8f6e9a6e668e3bb6bff111d7062
J.D <jordandong@gmail.com>
2017-11-21 07:26:18 -0800

    Update 00730_CountDifferentPalindromicSubsequences.cpp

--- 00730_CountDifferentPalindromicSubsequences.cpp
+++ 00730_CountDifferentPalindromicSubsequences.cpp
@@ -29,6 +29,97 @@ Each character S[i] will be in the set {'a', 'b', 'c', 'd'}.
 class Solution {
 public:
     int countPalindromicSubsequences(string S) {
+        int n = S.length();
+        vector<vector<int>> next(n, vector<int>(4, -1));
+        vector<vector<int>> prev(n, vector<int>(4, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < 4; k++) {
+                if (S[i] == ('a' + k))
+                    prev[i][k] = i;
+                else if (i > 0)
+                    prev[i][k] = prev[i - 1][k];
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int k = 0; k < 4; k++) {
+                if (S[i] == ('a' + k))
+                    next[i][k] = i;
+                else if (i < n - 1)
+                    next[i][k] = next[i + 1][k];
+            }
+        }
+        return countPalindromicSubsequencesHelper(0, n - 1, prev, next, dp) - 1;
+    }
+
+private:
+    int countPalindromicSubsequencesHelper(int i, int j, vector<vector<int>> &prev, vector<vector<int>> &next, vector<vector<int>> &dp) {
+        if (dp[i][j])
+            return dp[i][j];
+        int ans = 1; //empty str
+        if (i <= j) {
+            for (int k = 0; k < 4; k++) {
+                int ii = next[i][k], jj = prev[j][k];
+                if (i <= ii && ii <= j)
+                    ans += 1; //S[ii]
+                if (-1 < ii && ii < jj) //S[ii] + xxx + S[jj]
+                    ans += countPalindromicSubsequencesHelper(ii + 1, jj - 1, prev, next, dp);
+                    ans %= 1000000007;
+            }
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+};
+
+class Solution {
+public:
+    int countPalindromicSubsequences(string S) {
+        int n = S.length();
+        vector<vector<int>> next(n, vector<int>(4, -1));
+        vector<vector<int>> prev(n, vector<int>(4, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < 4; k++) {
+                if (S[i] == ('a' + k))
+                    prev[i][k] = i;
+                else if (i > 0)
+                    prev[i][k] = prev[i - 1][k];
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int k = 0; k < 4; k++) {
+                if (S[i] == ('a' + k))
+                    next[i][k] = i;
+                else if (i < n - 1)
+                    next[i][k] = next[i + 1][k];
+            }
+        }
+        return countPalindromicSubsequencesHelper(0, n - 1, prev, next, dp);
+    }
+
+private:
+    int countPalindromicSubsequencesHelper(int i, int j, vector<vector<int>> &prev, vector<vector<int>> &next, vector<vector<int>> &dp) {
+        if (i > j)
+            return 0;
+        if (dp[i][j])
+            return dp[i][j];
+        int ans = 0;
+        for (int k = 0; k < 4; k++) {
+            int ii = next[i][k], jj = prev[j][k];
+            if (ii < 0) //no this char
+                continue;
+            if (ii < jj) //S[ii] + S[jj], no middle chars
+                ans++;
+            if (ii <= j) {
+                ans++; //S[ii] only
+                ans += countPalindromicSubsequencesHelper(ii + 1, jj - 1, prev, next, dp); //S[ii] + dp(ii + 1, jj - 1) + S[jj]
+                ans %= 1000000007;
+            }
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
     }
 };
