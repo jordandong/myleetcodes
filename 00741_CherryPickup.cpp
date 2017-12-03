9a57637c5f3b7a90649ec102f36d4363305dc29c
J.D <jordandong@gmail.com>
2017-12-03 12:00:53 -0800

    Update 00741_CherryPickup.cpp

--- 00741_CherryPickup.cpp
+++ 00741_CherryPickup.cpp
@@ -69,3 +69,46 @@ public:
         return dp1[N - 1][N - 1] < 0 ? 0 : dp1[N - 1][N - 1];
     }
 };
+
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int d = grid.size(), p_len = 2 * d - 1;
+        vector<vector<int>> state(d, vector<int>(d, -1));
+        
+        state[0][0] = grid[0][0];
+        for (int p = 2; p <= p_len; ++p) {
+            for (int x1 = min(p,d) - 1; x1 >= 0; --x1) {
+                for (int x2 = min(p,d) - 1; x2 >= x1; --x2) {
+                    int y1 = p - x1 - 1, y2 = p - x2 - 1;
+                    if (y1 < 0 || y2 < 0 || y1 >= d || y2 >= d)
+                        continue;
+                    if (grid[y1][x1] == -1 || grid[y2][x2] == -1) {
+                        state[x1][x2] = -1;
+                        continue;
+                    }
+                    
+                    int gain = (x1 == x2) ? grid[y1][x1] : grid[y1][x1] + grid[y2][x2];
+                    int value = -1;
+                    if (x1 - 1 >= 0 && state[x1 - 1][x2] >= 0) {
+                        value = max(state[x1-1][x2] + gain, value);
+                    }
+                    
+                    if (x2 - 1 >= 0 && state[x1][x2 - 1] >= 0) {
+                        value = max(state[x1][x2-1] + gain, value);
+                    }
+                    
+                    if (x1 - 1 >= 0 && x2 - 1 >= 0 && state[x1 - 1][x2 - 1] >= 0) {
+                        value = max(state[x1 - 1][x2 - 1] + gain, value);
+                    }
+                    
+                    if (state[x1][x2] >= 0) {
+                        value = max(state[x1][x2] + gain, value);
+                    }
+                    state[x1][x2] = value;
+                }
+            }
+        }
+        return max(state[d - 1][d - 1], 0);
+    }
+};
