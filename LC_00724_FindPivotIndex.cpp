30d92b4458c8a3e962606c3457d79117ec400ade
J.D <jordandong@gmail.com>
2017-12-12 23:52:38 -0800

    Update and rename 00724_FindPivotIndex.cpp to LC_00724_FindPivotIndex.cpp

--- LC_00724_FindPivotIndex.cpp
+++ LC_00724_FindPivotIndex.cpp
@@ -3,7 +3,8 @@ Given an array of integers nums, write a method that returns the "pivot" index o
 
 We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
 
-If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+If no such index exists, we should return -1.
+If there are multiple pivot indexes, you should return the left-most pivot index.
 
 Example 1:
 Input: 
@@ -21,9 +22,25 @@ Explanation:
 There is no index that satisfies the conditions in the problem statement.
 
 Note:
-The length of nums will be in the range [0, 10000].
-Each element nums[i] will be an integer in the range [-1000, 1000].
+1. The length of nums will be in the range [0, 10000].
+2. Each element nums[i] will be an integer in the range [-1000, 1000].
 */
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size(), cur = 0, sum = 0;
+        for(int num: nums)
+            sum += num;
+        for (int i = 0; i < n; i++) { 
+            if (cur == sum - nums[i])
+                return i;
+            cur += nums[i];
+            sum -= nums[i];
+        }
+        return -1;
+    }
+};
 
 class Solution {
 public:
