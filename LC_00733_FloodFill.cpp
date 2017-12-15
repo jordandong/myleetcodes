b12e4bb728c4a7ad73b339b2ccbe04d0a0c63a36
J.D <jordandong@gmail.com>
2017-12-14 22:44:11 -0800

    Update and rename 00733_FloodFill.cpp to LC_00733_FloodFill.cpp

--- LC_00733_FloodFill.cpp
+++ LC_00733_FloodFill.cpp
@@ -1,9 +1,13 @@
 /*
 An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
 
-Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill,
+and a pixel value newColor, "flood fill" the image.
 
-To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+To perform a "flood fill", consider the starting pixel,
+plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel),
+and so on. Replace the color of all of the aforementioned pixels with the newColor.
 
 At the end, return the modified image.
 
@@ -13,15 +17,14 @@ image = [[1,1,1],[1,1,0],[1,0,1]]
 sr = 1, sc = 1, newColor = 2
 Output: [[2,2,2],[2,2,0],[2,0,1]]
 Explanation: 
-From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
-by a path of the same color as the starting pixel are colored with the new color.
-Note the bottom corner is not colored 2, because it is not 4-directionally connected
-to the starting pixel.
+From the center of the image (with position (sr, sc) = (1, 1)),
+all pixels connected by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 
 Note:
-The length of image and image[0] will be in the range [1, 50].
-The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
-The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+1. The length of image and image[0] will be in the range [1, 50].
+2. The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+3. The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
 */
 
 class Solution {
@@ -38,6 +41,7 @@ public:
     }
 private:
     void floodFillHelper(vector<vector<int>>& image, int sr, int sc, int newColor, int oldColor) {
+        //avoid looping via checking newColor == oldColor
         if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != oldColor || newColor == oldColor)
             return;
         image[sr][sc] = newColor;
