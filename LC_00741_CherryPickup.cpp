f54fc0fc3ec14a7130d9ef3fcc19345c8ccc692c
J.D <jordandong@gmail.com>
2017-12-21 23:12:21 -0800

    Update and rename 00741_CherryPickup.cpp to LC_00741_CherryPickup.cpp

--- LC_00741_CherryPickup.cpp
+++ LC_00741_CherryPickup.cpp
@@ -1,15 +1,15 @@
 /*
 In a N x N grid representing a field of cherries, each cell is one of three possible integers.
+1. 0 means the cell is empty, so you can pass through;
+2. 1 means the cell contains a cherry, that you can pick up and pass through;
+3. -1 means the cell contains a thorn that blocks your way.
 
-0 means the cell is empty, so you can pass through;
-1 means the cell contains a cherry, that you can pick up and pass through;
--1 means the cell contains a thorn that blocks your way.
 Your task is to collect maximum number of cherries possible by following the rules below:
+1. Starting at the position (0, 0) and reaching (N-1, N-1) by moving right or down through valid path cells (cells with value 0 or 1);
+2. After reaching (N-1, N-1), returning to (0, 0) by moving left or up through valid path cells;
+3. When passing through a path cell containing a cherry, you pick it up and the cell becomes an empty cell (0);
+4. If there is no valid path between (0, 0) and (N-1, N-1), then no cherries can be collected.
 
-Starting at the position (0, 0) and reaching (N-1, N-1) by moving right or down through valid path cells (cells with value 0 or 1);
-After reaching (N-1, N-1), returning to (0, 0) by moving left or up through valid path cells;
-When passing through a path cell containing a cherry, you pick it up and the cell becomes an empty cell (0);
-If there is no valid path between (0, 0) and (N-1, N-1), then no cherries can be collected.
 Example 1:
 Input: grid =
 [[0, 1, -1],
@@ -21,12 +21,11 @@ The player started at (0, 0) and went down, down, right right to reach (2, 2).
 4 cherries were picked up during this single trip, and the matrix becomes [[0,1,-1],[0,0,-1],[0,0,0]].
 Then, the player went left, up, up, left to return home, picking up one more cherry.
 The total number of cherries picked up is 5, and this is the maximum possible.
-Note:
 
-grid is an N by N 2D array, with 1 <= N <= 50.
-Each grid[i][j] is an integer in the set {-1, 0, 1}.
-It is guaranteed that grid[0][0] and grid[N-1][N-1] are not -1.
-Discuss
+Note:
+1. grid is an N by N 2D array, with 1 <= N <= 50.
+2. Each grid[i][j] is an integer in the set {-1, 0, 1}.
+3. It is guaranteed that grid[0][0] and grid[N-1][N-1] are not -1.
 */
 
 class Solution {
