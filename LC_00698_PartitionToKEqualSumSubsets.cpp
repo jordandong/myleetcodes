293c4f189362c5d19cb018fd08778b65f1da62b8
J.D <jordandong@gmail.com>
2017-12-22 21:54:10 -0800

    Update and rename 00698_PartitionToKEqualSumSubsets.cpp to LC_00698_PartitionToKEqualSumSubsets.cpp

--- LC_00698_PartitionToKEqualSumSubsets.cpp
+++ LC_00698_PartitionToKEqualSumSubsets.cpp
@@ -1,14 +1,15 @@
 /*
-Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
+Given an array of integers nums and a positive integer k,
+find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
 
 Example 1:
 Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
 Output: True
 Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
-Note:
 
-1 <= k <= len(nums) <= 16.
-0 < nums[i] < 10000.
+Note:
+1. 1 <= k <= len(nums) <= 16.
+2. 0 < nums[i] < 10000.
 */
 
 class Solution {
@@ -38,5 +39,4 @@ private:
         }
         return false;
     }
-    
 };
