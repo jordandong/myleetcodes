e90f088c413e7b5febf450559eb25809f3bb31d9
J.D <jordandong@gmail.com>
2017-12-23 23:11:53 -0800

    Update 00752_OpenTheLock.cpp

--- 00752_OpenTheLock.cpp
+++ 00752_OpenTheLock.cpp
@@ -14,19 +14,23 @@ Explanation:
 A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
 Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
 because the wheels of the lock become stuck after the display becomes the dead end "0102".
+
 Example 2:
 Input: deadends = ["8888"], target = "0009"
 Output: 1
 Explanation:
 We can turn the last wheel in reverse to move from "0000" -> "0009".
+
 Example 3:
 Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
 Output: -1
 Explanation:
 We can't reach the target without getting stuck.
+
 Example 4:
 Input: deadends = ["0000"], target = "8888"
 Output: -1
+
 Note:
 The length of deadends will be in the range [1, 500].
 target will not be in the list deadends.
