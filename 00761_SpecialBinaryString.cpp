cfee232b48aa44562d99ad2f9f24a0b4308661c1
J.D <jordandong@gmail.com>
2018-01-08 00:33:21 -0800

    Update 00761_SpecialBinaryString.cpp

--- 00761_SpecialBinaryString.cpp
+++ 00761_SpecialBinaryString.cpp
@@ -19,9 +19,54 @@ S has length at most 50.
 S is guaranteed to be a special binary string as defined above.
 */
 
+
+//1 + S + 0 ,recursion on middle S part
 class Solution {
 public:
     string makeLargestSpecial(string S) {
-        
+        int count = 0, i = 0;
+        vector<string> res;
+        for (int j = 0; j < S.size(); ++j) {
+            if (S[j] == '1')
+                count++;
+            else
+                count--;
+            if (count == 0) {
+                res.push_back('1' + makeLargestSpecial(S.substr(i + 1, j - i - 1)) + '0');
+                i = j + 1;
+            }
+        }
+        sort(res.begin(), res.end(), greater<string>());
+        string ans = "";
+        for (auto &s : res)
+            ans += s;
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    string makeLargestSpecial(string S) {
+        int i = 0;
+        return makeLargestSpecialHelper(S, i);
+    }
+
+private:
+    string makeLargestSpecialHelper(string &S, int &i) {
+        string res;
+        vector<string> toks;
+        while (i < S.size() && res.empty()) {
+            if (S[i++] == '1')
+                toks.push_back(makeLargestSpecialHelper(S, i));
+            else
+                res += "1";
+        }
+        bool prefix = res.size();
+        sort(toks.begin(), toks.end());
+        for (auto it = toks.rbegin(); it != toks.rend(); it++)
+            res += *it;
+        if (prefix)
+            res += '0';
+        return res;
     }
 };
