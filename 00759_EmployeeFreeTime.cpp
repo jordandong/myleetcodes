e491042d2b571e0b9d9cae3e7e3a598d236aa5f2
J.D <jordandong@gmail.com>
2018-01-07 23:40:30 -0800

    Update 00759_EmployeeFreeTime.cpp

--- 00759_EmployeeFreeTime.cpp
+++ 00759_EmployeeFreeTime.cpp
@@ -37,6 +37,24 @@ schedule and schedule[i] are lists with lengths in range [1, 50].
 class Solution {
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>>& schedule) {
-        
+        vector<Interval> res;
+        map<int, int> timeline;
+        for (int i = 0; i < schedule.size(); i++) {
+            for (int j = 0; j < schedule[i].size(); j++) {
+                timeline[schedule[i][j].start]++;
+                timeline[schedule[i][j].end]--;
+            }
+        }
+        int workers = 0;
+        for (auto p : timeline) {
+            workers += p.second;
+            if (!workers)
+                res.push_back(Interval(p.first, 0));
+            if (workers && !res.empty() && !res.back().end)
+                res.back().end = p.first;
+        }
+        if (!res.empty())
+            res.pop_back();
+        return res;
     }
 };
