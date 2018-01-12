5ad380063266f6ff9037d6b286ad555dd9907904
J.D <jordandong@gmail.com>
2018-01-12 01:02:10 -0800

    Update and rename 00727_MinimumWindowSubsequence.cpp to LC_00727_MinimumWindowSubsequence.cpp

--- LC_00727_MinimumWindowSubsequence.cpp
+++ LC_00727_MinimumWindowSubsequence.cpp
@@ -1,7 +1,7 @@
 /*
 Given strings S and T, find the minimum (contiguous) substring W of S, so that T is a subsequence of W.
-
-If there is no such window in S that covers all characters in T, return the empty string "". If there are multiple such minimum-length windows, return the one with the left-most starting index.
+If there is no such window in S that covers all characters in T, return the empty string "".
+If there are multiple such minimum-length windows, return the one with the left-most starting index.
 
 Example 1:
 Input: 
@@ -10,11 +10,11 @@ Output: "bcde"
 Explanation: 
 "bcde" is the answer because it occurs before "bdde" which has the same length.
 "deb" is not a smaller window because the elements of T in the window must occur in order.
-Note:
 
-All the strings in the input will only contain lowercase letters.
-The length of S will be in the range [1, 20000].
-The length of T will be in the range [1, 100].
+Note:
+1. All the strings in the input will only contain lowercase letters.
+2. The length of S will be in the range [1, 20000].
+3. The length of T will be in the range [1, 100].
 */
 
 class Solution {
