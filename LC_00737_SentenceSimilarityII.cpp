5512d0e1cd626de3dbbf90314343f3fc3082736f
J.D <jordandong@gmail.com>
2018-01-12 01:00:57 -0800

    Update and rename 00737_SentenceSimilarityII.cpp to LC_00737_SentenceSimilarityII.cpp

--- LC_00737_SentenceSimilarityII.cpp
+++ LC_00737_SentenceSimilarityII.cpp
@@ -1,18 +1,20 @@
 /*
 Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
 
-For example, words1 = ["great", "acting", "skills"] and words2 = ["fine", "drama", "talent"] are similar, if the similar word pairs are pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]].
+For example, words1 = ["great", "acting", "skills"] and words2 = ["fine", "drama", "talent"] are similar,
+if the similar word pairs are pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]].
 
-Note that the similarity relation is transitive. For example, if "great" and "good" are similar, and "fine" and "good" are similar, then "great" and "fine" are similar.
+Note that the similarity relation is transitive.
+For example, if "great" and "good" are similar, and "fine" and "good" are similar, then "great" and "fine" are similar.
 
-Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
+Also, a word is always similar with itself.
+For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
 
 Note:
-
-The length of words1 and words2 will not exceed 1000.
-The length of pairs will not exceed 2000.
-The length of each pairs[i] will be 2.
-The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+1. The length of words1 and words2 will not exceed 1000.
+2. The length of pairs will not exceed 2000.
+3. The length of each pairs[i] will be 2.
+4. The length of each words[i] and pairs[i][j] will be in the range [1, 20].
 */
 
 class Solution {
