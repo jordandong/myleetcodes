75739392cfa0957cb7dd8b297d014d6ef3d03729
J.D <jordandong@gmail.com>
2018-01-15 01:29:15 -0800

    Update and rename 00763_PartitionLabels.cpp to LC_00763_PartitionLabels.cpp

--- LC_00763_PartitionLabels.cpp
+++ LC_00763_PartitionLabels.cpp
@@ -1,5 +1,6 @@
 /*
-A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+A string S of lowercase letters is given.
+We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
 
 Example 1:
 Input: S = "ababcbacadefegdehijhklij"
@@ -8,10 +9,10 @@ Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
 This is a partition so that each letter appears in at most one part.
 A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
-Note:
 
-S will have length in range [1, 500].
-S will consist of lowercase letters ('a' to 'z') only.
+Note:
+1. S will have length in range [1, 500].
+2. S will consist of lowercase letters ('a' to 'z') only.
 */
 
 class Solution {
