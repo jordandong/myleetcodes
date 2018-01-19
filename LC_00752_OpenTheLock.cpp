c5520ceaaea105aeaaf88b464a632232d8fec5f6
J.D <jordandong@gmail.com>
2018-01-18 23:59:23 -0800

    Update and rename 00752_OpenTheLock.cpp to LC_00752_OpenTheLock.cpp

--- LC_00752_OpenTheLock.cpp
+++ LC_00752_OpenTheLock.cpp
@@ -1,5 +1,8 @@
 /*
-You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+You have a lock in front of you with 4 circular wheels.
+Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'.
+The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'.
+Each move consists of turning one wheel one slot.
 
 The lock initially starts at '0000', a string representing the state of the 4 wheels.
 
@@ -32,9 +35,9 @@ Input: deadends = ["0000"], target = "8888"
 Output: -1
 
 Note:
-The length of deadends will be in the range [1, 500].
-target will not be in the list deadends.
-Every string in deadends and the string target will be a string of 4 digits from the 10,000 possibilities '0000' to '9999'.
+1. The length of deadends will be in the range [1, 500].
+2. target will not be in the list deadends.
+3. Every string in deadends and the string target will be a string of 4 digits from the 10,000 possibilities '0000' to '9999'.
 */
 
 class Solution {
