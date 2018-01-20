48ae59a33517d5c8402c72b6a0eda2a04e0b8baa
J.D <jordandong@gmail.com>
2018-01-20 00:52:53 -0800

    Update and rename 00758_BoldWordsInString.cpp to LC_00758_BoldWordsInString.cpp

--- LC_00758_BoldWordsInString.cpp
+++ LC_00758_BoldWordsInString.cpp
@@ -3,14 +3,14 @@ Given a set of keywords words and a string S, make all appearances of all keywor
 
 The returned string should use the least number of tags possible, and of course the tags should form a valid combination.
 
-For example, given that words = ["ab", "bc"] and S = "aabcd", we should return "a<b>abc</b>d". Note that returning "a<b>a<b>b</b>c</b>d" would use more tags, so it is incorrect.
+For example, given that words = ["ab", "bc"] and S = "aabcd", we should return "a<b>abc</b>d".
+Note that returning "a<b>a<b>b</b>c</b>d" would use more tags, so it is incorrect.
 
 Note:
-
-words has length in range [0, 50].
-words[i] has length in range [1, 10].
-S has length in range [0, 500].
-All characters in words[i] and S are lowercase letters.
+1. words has length in range [0, 50].
+2. words[i] has length in range [1, 10].
+3. S has length in range [0, 500].
+4. All characters in words[i] and S are lowercase letters.
 */
 
 class Solution {
@@ -34,7 +34,6 @@ public:
                 res += "</b>";
         }
         return res;
-        
     }
 };
 
