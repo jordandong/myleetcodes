2113f7a499836a7d7b831111a9605b8f2adcc86a
J.D <jordandong@gmail.com>
2018-01-21 01:23:09 -0800

    Update 00766_ToeplitzMatrix.cpp

--- 00766_ToeplitzMatrix.cpp
+++ 00766_ToeplitzMatrix.cpp
@@ -58,3 +58,22 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if (m == 0)
+            return false;
+        int n = matrix[0].size();
+        if (n == 0)
+            return false;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
