30336f6ccaaa9db7464acf287147c3087c94bfa7
J.D <jordandong@gmail.com>
2018-01-21 22:50:09 -0800

    Update 00767_ReorganizeString.cpp

--- 00767_ReorganizeString.cpp
+++ 00767_ReorganizeString.cpp
@@ -19,6 +19,32 @@ S will consist of lowercase letters and have length in range [1, 500].
 class Solution {
 public:
     string reorganizeString(string S) {
-        
+        map<int, int> cnts;
+        for (char c : S)
+            cnts[c]++;
+        priority_queue<pair<int, int>> pq;
+        for (auto p : cnts) {
+            if (p.second > (S.length() + 1) / 2)
+                return "";
+            pq.push({p.second, p.first});
+        }
+
+        string res;
+        pair<int, int> top1, top2;
+        while (!pq.empty()) {
+            top1 = pq.top();
+            pq.pop();
+            res += top1.second;
+            if (!pq.empty()) {
+                res += pq.top().second;
+                top2 = pq.top();
+                pq.pop();
+                if (top2.first > 1)
+                    pq.push({ top2.first - 1, top2.second });
+            }
+            if (top1.first > 1)
+                pq.push({ top1.first - 1, top1.second });
+        }
+        return res;        
     }
 };
