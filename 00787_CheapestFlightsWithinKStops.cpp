8c5f3a7a1343324ad6868098d6d16f898db823e3
J.D <jordandong@gmail.com>
2018-02-19 23:22:22 -0800

    Update 00787_CheapestFlightsWithinKStops.cpp

--- 00787_CheapestFlightsWithinKStops.cpp
+++ 00787_CheapestFlightsWithinKStops.cpp
@@ -37,9 +37,22 @@ k is in the range of [0, n - 1].
 There will not be any duplicated flights or self cycles.
 */
 
+//DP
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        
+        const int INF = 1e9;
+        K++;
+        vector<vector<int>> ans(n, vector<int>(K + 1, INF));
+        ans[src][0] = 0;
+        for(int i = 1; i <= K; i++) {
+            for(int j = 0; j < n; j++)
+                ans[j][i] = ans[j][i - 1];
+            for(const vector<int>& f: flights)
+                ans[f[1]][i] = min(ans[f[1]][i], ans[f[0]][i - 1] + f[2]);
+        }
+        if(ans[dst][K] == INF)
+            return -1;
+        return ans[dst][K];
     }
 };
