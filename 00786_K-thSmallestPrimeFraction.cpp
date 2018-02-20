40811ae9254e4d1725fbaa9330761eba14e1bb41
J.D <jordandong@gmail.com>
2018-02-19 23:07:43 -0800

    Update 00786_K-thSmallestPrimeFraction.cpp

--- 00786_K-thSmallestPrimeFraction.cpp
+++ 00786_K-thSmallestPrimeFraction.cpp
@@ -24,6 +24,23 @@ class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
         priority_queue<pair<double, pair<int,int>>> pq;
+        pq.push({-1.0*A[0]/A.back(), {0, A.size() - 1}});
+        while (--K > 0) {
+            pair<int,int> cur = pq.top().second;
+            pq.pop();
+            if (cur.second == A.size() - 1)
+                pq.push({-1.0*A[cur.first + 1]/A[cur.second], {cur.first + 1, cur.second}});
+            cur.second--;
+            pq.push({-1.0*A[cur.first]/A[cur.second], {cur.first, cur.second}});
+        }
+        return {A[pq.top().second.first], A[pq.top().second.second]};
+    }
+};
+
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+        priority_queue<pair<double, pair<int,int>>> pq;
         for (int i = 0; i < A.size(); i++)
             pq.push({-1.0*A[i]/A.back(), {i, A.size() - 1}});
         while (--K > 0) {
