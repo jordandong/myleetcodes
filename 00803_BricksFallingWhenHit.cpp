0b2031f5e67ec65eb98511d45c1dd419b21bf395
J.D <jordandong@gmail.com>
2018-03-22 22:57:19 -0700

    Update 00803_BricksFallingWhenHit.cpp

--- 00803_BricksFallingWhenHit.cpp
+++ 00803_BricksFallingWhenHit.cpp
@@ -32,6 +32,56 @@ An erasure may refer to a location with no brick - if it does, no bricks drop.
 class Solution {
 public:
     vector<int> hitBricks(vector<vector<int>>& grid, vector<vector<int>>& hits) {
+        int m = grid.size();
+        if (m == 0)
+            return {};
+        int n = grid[0].size();
+        if (n == 0)
+            return {};
+        int k = hits.size();
+        if (k == 0)
+            return {};
         
+        //remove the brick count if it is there or not
+        for (auto h: hits)
+            grid[h[0]][h[1]] -= 1;
+        
+        //color the stable bricks connecting to the top
+        for (int j = 0; j < n; j++)
+            hitBricksHelper(0, j, grid);
+        
+        //add hit brick back to see if we can attach more bricks to the stable bricks 
+        vector<int> ans(k, 0);
+        for (int i = k - 1; i >= 0; i--) {
+            if (grid[hits[i][0]][hits[i][1]] == -1)
+                continue; //empty at beginning
+            grid[hits[i][0]][hits[i][1]] += 1; //add brick back
+            if (!toTop(hits[i][0], hits[i][1], grid))
+                continue;
+            ans[i] = hitBricksHelper(hits[i][0], hits[i][1], grid) - 1; //don't count the hit one
+        }
+        
+        return ans;
+    }
+
+private:
+    int hitBricksHelper(int r, int c, vector<vector<int>>& grid) {
+        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] != 1)
+            return 0;
+        int cnt = 1;
+        grid[r][c] = 2; //mark stable
+        cnt += hitBricksHelper(r - 1, c, grid);
+        cnt += hitBricksHelper(r + 1, c, grid);
+        cnt += hitBricksHelper(r, c - 1, grid);
+        cnt += hitBricksHelper(r, c + 1, grid);
+        return cnt;
+    }
+    
+    bool toTop(int i, int j, vector<vector<int>>& grid) {
+        return ( (i == 0) ||
+                 (i - 1 >= 0 && grid[i - 1][j] == 2) || 
+                 (i + 1 < grid.size() && grid[i + 1][j] == 2) ||
+                 (j - 1 >= 0 && grid[i][j - 1] == 2) ||
+                 (j + 1 < grid[0].size() && grid[i][j + 1] == 2));
     }
 };
