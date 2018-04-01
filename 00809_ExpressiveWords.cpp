56a3ad28234d4c49b3ca0e86f9751b7c8d35b06c
J.D <jordandong@gmail.com>
2018-04-01 16:07:24 +0800

    Update 00809_ExpressiveWords.cpp

--- 00809_ExpressiveWords.cpp
+++ 00809_ExpressiveWords.cpp
@@ -23,7 +23,24 @@ S and all words in words consist only of lowercase letters
 
 class Solution {
 public:
-    int expressiveWords(string S, vector<string>& words) {
-        
+    int expressiveWords(string S, vector<string> &words) {
+        int res = 0;
+        for (auto &w : words)
+            if (w.size() <= S.size()) {
+                int i, j;
+                for (i = 0, j = 0; j < S.size(); j++) {
+                    if (i < w.length() && w[i] == S[j])
+                        i++;
+                    else if (j > 0 && S[j] == S[j - 1] && j + 1 < S.size() && S[j] == S[j + 1]) // last, this and next
+                        j++;
+                    else if (!(j > 1 && S[j] == S[j - 1] && S[j] == S[j - 2])) // this and last 2 chars
+                        break;
+                }
+                if (i == w.size() && j == S.size()) // both pointers reach the end
+                    res++;
+            }
+        return res;
     }
 };
+
+
