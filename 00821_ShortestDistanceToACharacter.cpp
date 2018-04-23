c86a4cc690f5f8688d7588b8138f1f8752c9cf78
J.D <jordandong@gmail.com>
2018-04-23 01:00:55 -0700

    Update 00821_ShortestDistanceToACharacter.cpp

--- 00821_ShortestDistanceToACharacter.cpp
+++ 00821_ShortestDistanceToACharacter.cpp
@@ -17,6 +17,20 @@ All letters in S and C are lowercase.
 class Solution {
 public:
     vector<int> shortestToChar(string S, char C) {
-        
+        int n = S.size();
+        vector<int> res(n, n);
+        int pos = -n;
+        for (int i = 0; i < n; ++i) {
+            if (S[i] == C)
+                pos = i;
+            res[i] = min(res[i], i - pos);
+        }
+        pos = 2*n;
+        for (int i = n - 1; i >= 0; --i) {
+            if (S[i] == C)
+                pos = i;
+            res[i] = min(res[i], pos - i);
+        }
+        return res;
     }
 };
