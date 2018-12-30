631c1506695049b817859afd84242373944d460a
J.D <jordandong@gmail.com>
2018-12-30 02:48:30 -0800

    Create 00968_BinaryTreeCameras.cpp

---
+++ 00968_BinaryTreeCameras.cpp
@@ -0,0 +1,51 @@
+/*
+Given a binary tree, we install cameras on the nodes of the tree. 
+
+Each camera at a node can monitor its parent, itself, and its immediate children.
+
+Calculate the minimum number of cameras needed to monitor all nodes of the tree.
+
+ 
+
+Example 1:
+
+     x
+     C
+  x     x   
+
+Input: [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+
+Example 2:
+       x
+      c
+     x
+    c
+      x
+
+Input: [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+
+Note:
+
+The number of nodes in the given tree will be in the range [1, 1000].
+Every node has value 0.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        
+    }
+};
