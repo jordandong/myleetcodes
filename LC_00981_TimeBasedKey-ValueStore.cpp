b1a7aded623eb451980ef600366520ed83518ccd
J.D <jordandong@gmail.com>
2021-07-31 12:10:48 -0700

    Update and rename 00981_TimeBasedKey-ValueStore.cpp to LC_00981_TimeBasedKey-ValueStore.cpp

--- LC_00981_TimeBasedKey-ValueStore.cpp
+++ LC_00981_TimeBasedKey-ValueStore.cpp
@@ -43,16 +43,39 @@ class TimeMap {
 public:
     /** Initialize your data structure here. */
     TimeMap() {
-        
     }
     
     void set(string key, string value, int timestamp) {
-        
+        int idx = BS(m_data[key], timestamp);
+        if (idx == -1)
+            m_data[key].push_back(make_pair(timestamp, value));
+        else
+            m_data[key].insert(m_data[key].begin() + idx + 1, make_pair(timestamp, value));
     }
     
     string get(string key, int timestamp) {
-        
+        int idx = BS(m_data[key], timestamp);
+        if (idx < 0)
+            return "";
+        else
+            return (m_data[key].begin() + idx)->second;
     }
+    
+    int BS(vector<pair<int,string>> &data, int timestamp) {
+        int lo = 0, hi = data.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (timestamp >= data[mid].first) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return hi;
+    }
+
+private:
+    map<string, vector<pair<int, string>>> m_data;
 };
 
 /**
