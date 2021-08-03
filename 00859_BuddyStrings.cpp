7efdd89424f2269a1229ddf3caa49579c5b1984e
J.D <jordandong@gmail.com>
2021-08-02 22:59:42 -0700

    Update 00859_BuddyStrings.cpp

--- 00859_BuddyStrings.cpp
+++ 00859_BuddyStrings.cpp
@@ -34,7 +34,32 @@ A and B consist only of lowercase letters.
 
 class Solution {
 public:
-    bool buddyStrings(string A, string B) {
-        
+    bool buddyStrings(string s, string goal) {
+        if (s.length() != goal.length())
+            return false;
+        bool dup = false;
+        set<char> exists;
+        int idx1 = -1, idx2 = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != goal[i]) {
+                if (idx1 == -1)
+                    idx1 = i;
+                else if (idx2 == -1)
+                    idx2 = i;
+                else
+                    return false;
+            }
+            if (exists.find(s[i]) == exists.end()) {
+                exists.insert(s[i]);
+            } else {
+               dup = true; 
+            }
+        }
+        if (idx1 >= 0 && idx2 >= 0) {
+            return s[idx1] == goal[idx2] && s[idx2] == goal[idx1];
+        } else if (idx1 == -1 && idx2 == -1) {
+            return dup;
+        }
+        return false;
     }
 };
