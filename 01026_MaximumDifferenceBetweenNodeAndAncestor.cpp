6e41d6c3544edd191945a7b5fb4c41f40e34776d
J.D <jordandong@gmail.com>
2021-08-09 22:39:05 -0700

    Update 01026_MaximumDifferenceBetweenNodeAndAncestor.cpp

--- 01026_MaximumDifferenceBetweenNodeAndAncestor.cpp
+++ 01026_MaximumDifferenceBetweenNodeAndAncestor.cpp
@@ -1,9 +1,7 @@
 /*
-Given the root of a binary tree, find the maximum value V for which there exists different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
+Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
 
-(A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.)
-
- 
+A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
 
 Example 1:
                8
@@ -11,23 +9,28 @@ Example 1:
 1         6                        14 
        4     7                   13
 
-
-
-Input: [8,3,10,1,6,null,14,null,null,4,7,13]
+Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
 Output: 7
-Explanation: 
-We have various ancestor-node differences, some of which are given below :
+Explanation: We have various ancestor-node differences, some of which are given below :
 |8 - 3| = 5
 |3 - 7| = 4
 |8 - 1| = 7
 |10 - 13| = 3
 Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+
+Example 2:
+
+    1
+          2
+                0
+             3
  
-
-Note:
-
-The number of nodes in the tree is between 2 and 5000.
-Each node will have value between 0 and 100000.
+Input: root = [1,null,2,null,0,3]
+Output: 3
+ 
+Constraints:
+The number of nodes in the tree is in the range [2, 5000].
+0 <= Node.val <= 105
 */
 
 /**
@@ -36,12 +39,48 @@ Each node will have value between 0 and 100000.
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int maxAncestorDiff(TreeNode* root) {
-        
+        int ans = 0;
+        if (!root) {
+            return ans;
+        }
+        maxAncestorDiffHelper(root, root->val, root->val, ans);
+        return ans;
+    }
+    
+    void maxAncestorDiffHelper(TreeNode* root, int mx, int mi, int &ans) {
+        if (!root) {
+            return;
+        }
+        int ans_temp = max(abs(mx - root->val), abs(mi - root->val));
+        ans = max(ans, ans_temp);
+        maxAncestorDiffHelper(root->left, max(mx, root->val), min(mi, root->val), ans);
+        maxAncestorDiffHelper(root->right, max(mx, root->val), min(mi, root->val), ans);
+    }
+};
+
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        return maxAncestorDiffHelper(root, root->val, root->val);
+    }
+    
+    int maxAncestorDiffHelper(TreeNode* root, int mx, int mi) {
+        if (!root) {
+            return mx - mi;
+        }
+        int ans1 = maxAncestorDiffHelper(root->left, max(mx, root->val), min(mi, root->val));
+        int ans2 = maxAncestorDiffHelper(root->right, max(mx, root->val), min(mi, root->val));
+        return max(ans1, ans2);
     }
 };
