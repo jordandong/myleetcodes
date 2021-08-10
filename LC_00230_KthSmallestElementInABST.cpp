116713bc7f12b7dcf204f3dabaafbe56826d46e9
J.D <jordandong@gmail.com>
2021-08-09 23:34:43 -0700

    Update LC_00230_KthSmallestElementInABST.cpp

--- LC_00230_KthSmallestElementInABST.cpp
+++ LC_00230_KthSmallestElementInABST.cpp
@@ -1,20 +1,37 @@
 /*
-Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
 
-Note: 
-You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+Example 1:
 
-Follow up:
-What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+      3
+ 1         4
+    2
 
-Hint:
-Try to utilize the property of a BST.
-What if you could modify the BST node's structure?
-The optimal runtime complexity is O(height of BST).
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
 
-Credits:
-Special thanks to @ts for adding this problem and creating all test cases.
+Example 2:
+
+                     5
+              3             6
+          2      4
+       1
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
 */
+
+
 
 /**
  * Definition for a binary tree node.
@@ -22,9 +39,35 @@ Special thanks to @ts for adding this problem and creating all test cases.
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int ans = -1;
+        kthSmallestHelp(root, k, ans);
+        return ans;
+    }
+    
+    void kthSmallestHelp(TreeNode* root, int &k, int &ans) {
+        if (!root) {
+            return;
+        }
+        kthSmallestHelp(root->left, k, ans);
+        if (--k <= 0) {
+            if (k == 0) {
+                ans = root->val;
+            }
+            return;
+        }
+        kthSmallestHelp(root->right, k, ans);
+    }
+};
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
