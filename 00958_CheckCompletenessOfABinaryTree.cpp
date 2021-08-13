034a59e116137722db75e7cc235f13be2be639ca
J.D <jordandong@gmail.com>
2021-08-13 00:54:05 -0700

    Update 00958_CheckCompletenessOfABinaryTree.cpp

--- 00958_CheckCompletenessOfABinaryTree.cpp
+++ 00958_CheckCompletenessOfABinaryTree.cpp
@@ -1,10 +1,8 @@
 /*
-Given a binary tree, determine if it is a complete binary tree.
+Given the root of a binary tree, determine if it is a complete binary tree.
 
-Definition of a complete binary tree from Wikipedia:
-In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
-
- 
+In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible.
+It can have between 1 and 2h nodes inclusive at the last level h.
 
 Example 1:
 
@@ -12,8 +10,7 @@ Example 1:
    2         3
 4    5     6
 
-
-Input: [1,2,3,4,5,6]
+Input: root = [1,2,3,4,5,6]
 Output: true
 Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
 
@@ -23,13 +20,14 @@ Example 2:
    2         3
 4    5          7
 
-Input: [1,2,3,4,5,null,7]
+Input: root = [1,2,3,4,5,null,7]
 Output: false
 Explanation: The node with value 7 isn't as far left as possible.
- 
-Note:
 
-The tree will have between 1 and 100 nodes.
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+1 <= Node.val <= 1000
 */
 
 /**
@@ -38,12 +36,30 @@ The tree will have between 1 and 100 nodes.
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        
+        bool node = true;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* cur = q.front();
+            q.pop();
+            if (cur) {
+                if (!node) {
+                    return false;
+                }
+                q.push(cur->left);
+                q.push(cur->right);
+            } else {
+                node = false;
+            }
+        }
+        return true;
     }
 };
