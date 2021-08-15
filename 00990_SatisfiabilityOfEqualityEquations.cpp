964eeb199483fed6dc680e45d973f72affd24375
J.D <jordandong@gmail.com>
2021-08-15 04:34:53 -0700

    Update 00990_SatisfiabilityOfEqualityEquations.cpp

--- 00990_SatisfiabilityOfEqualityEquations.cpp
+++ 00990_SatisfiabilityOfEqualityEquations.cpp
@@ -1,46 +1,82 @@
 /*
-Given an array equations of strings that represent relationships between variables, each string equations[i] has length 4 and takes one of two different forms: "a==b" or "a!=b".  Here, a and b are lowercase letters (not necessarily different) that represent one-letter variable names.
-
-Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
-
- 
+You are given an array of strings equations that represent relationships between variables where each string equations[i] is of length 4 and takes one of two different forms: "xi==yi" or "xi!=yi".
+Here, xi and yi are lowercase letters (not necessarily different) that represent one-letter variable names.
+Return true if it is possible to assign integers to variable names so as to satisfy all the given equations, or false otherwise.
 
 Example 1:
 
-Input: ["a==b","b!=a"]
+Input: equations = ["a==b","b!=a"]
 Output: false
-Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.  There is no way to assign the variables to satisfy both equations.
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+There is no way to assign the variables to satisfy both equations.
+
 Example 2:
 
-Input: ["b==a","a==b"]
+Input: equations = ["b==a","a==b"]
 Output: true
 Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+
 Example 3:
 
-Input: ["a==b","b==c","a==c"]
+Input: equations = ["a==b","b==c","a==c"]
 Output: true
+
 Example 4:
 
-Input: ["a==b","b!=c","c==a"]
+Input: equations = ["a==b","b!=c","c==a"]
 Output: false
+
 Example 5:
 
-Input: ["c==c","b==d","x!=z"]
+Input: equations = ["c==c","b==d","x!=z"]
 Output: true
  
-
-Note:
+Constraints:
 
 1 <= equations.length <= 500
 equations[i].length == 4
-equations[i][0] and equations[i][3] are lowercase letters
-equations[i][1] is either '=' or '!'
-equations[i][2] is '='
+equations[i][0] is a lowercase letter.
+equations[i][1] is either '=' or '!'.
+equations[i][2] is '='.
+equations[i][3] is a lowercase letter.
 */
 
 class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
-        
+        vector<int> parent;
+        for (int i = 0; i < 26; i++) {
+            parent.push_back(i);
+        }
+
+        for (auto eq : equations) {
+            int l = eq[0] - 'a', r = eq[3] - 'a';
+            if (eq[1] == '=') {
+                uni(parent, l, r);
+            }  
+        }
+
+        for (auto eq : equations) {
+            int l = eq[0] - 'a', r = eq[3] - 'a';
+            if (eq[1]== '!') {
+                if (find(parent, l) == find(parent, r)) {
+                    return false;
+                }
+            }  
+        }
+        return true;
+    }
+    
+    int find(vector<int> &parent, int c) {
+        if (parent[c] == c) {
+            return c;
+        }
+        return find(parent, parent[c]);
+    }
+    
+    void uni(vector<int> &parent, int p, int c) {
+        int p1 = find(parent, p);
+        int p2 = find(parent, c);
+        parent[p2] = p1;
     }
 };
