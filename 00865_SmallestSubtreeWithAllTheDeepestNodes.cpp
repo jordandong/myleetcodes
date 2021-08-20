7e8679b4ee4fca8f38c3bc8e25fcd06976334edb
J.D <jordandong@gmail.com>
2021-08-20 00:34:14 -0700

    Update 00865_SmallestSubtreeWithAllTheDeepestNodes.cpp

--- 00865_SmallestSubtreeWithAllTheDeepestNodes.cpp
+++ 00865_SmallestSubtreeWithAllTheDeepestNodes.cpp
@@ -1,35 +1,45 @@
 /*
-Given a binary tree rooted at root, the depth of each node is the shortest distance to the root.
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
 
-A node is deepest if it has the largest depth possible among any node in the entire tree.
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
 
-The subtree of a node is that node, plus the set of all descendants of that node.
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
 
-Return the node with the largest depth such that it contains all the deepest nodes in it's subtree.
+The subtree of a node is tree consisting of that node, plus the set of all descendants of that node.
 
- 
+Note: This question is the same as 1123: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+
 
 Example 1:
 
-Input: [3,5,1,6,2,0,8,null,null,7,4]
-Output: [2,7,4]
-Explanation:
             3
         5        1
-   6    2        0 .   8
+   6    2        0    8
       7   4
 
-We return the node with value 2, colored in yellow in the diagram.
-The nodes colored in blue are the deepest nodes of the tree.
-The input "[3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]" is a serialization of the given tree.
-The output "[2, 7, 4]" is a serialization of the subtree rooted at the node with value 2.
-Both the input and output have TreeNode type.
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest nodes of the tree.
+Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+
+Example 2:
+
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree.
+
+Example 3:
+
+Input: root = [0,1,3,null,2]
+Output: [2]
+Explanation: The deepest node in the tree is 2, the valid subtrees are the subtrees of nodes 2, 1 and 0 but the subtree of node 2 is the smallest.
  
+Constraints:
 
-Note:
-
-The number of nodes in the tree will be between 1 and 500.
-The values of each node are unique.
+The number of nodes in the tree will be in the range [1, 500].
+0 <= Node.val <= 500
+The values of the nodes in the tree are unique.
 */
 
 /**
@@ -38,12 +48,31 @@ The values of each node are unique.
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        
+        return subtreeWithAllDeepestHelper(root).first;        
+    }
+    
+    pair<TreeNode*, int> subtreeWithAllDeepestHelper(TreeNode* root) {
+        if (!root) {
+            return make_pair(root, 0);
+        }
+        auto l = subtreeWithAllDeepestHelper(root->left);
+        auto r = subtreeWithAllDeepestHelper(root->right);
+        if (l.second > r.second) {
+            l.second += 1;
+            return l;
+        } else if (l.second < r.second) {
+            r.second += 1;
+            return r;
+        } else {
+            return make_pair(root, l.second + 1);
+        } 
     }
 };
