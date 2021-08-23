d1090597234c912721982eb3e0f4975d08ab97eb
J.D <jordandong@gmail.com>
2021-08-22 20:43:21 -0700

    Update 00965_UnivaluedBinaryTree.cpp

--- 00965_UnivaluedBinaryTree.cpp
+++ 00965_UnivaluedBinaryTree.cpp
@@ -1,30 +1,29 @@
 /*
-A binary tree is univalued if every node in the tree has the same value.
+A binary tree is uni-valued if every node in the tree has the same value.
 
-Return true if and only if the given tree is univalued.
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
 
 Example 1:
        1
   1         1
 1    1         1
 
-Input: [1,1,1,1,1,null,1]
+Input: root = [1,1,1,1,1,null,1]
 Output: true
-
 
 Example 2:
         2
     2       2
   5   2    
 
-Input: [2,2,2,5,2]
+Input: root = [2,2,2,5,2]
 Output: false
  
 
-Note:
+Constraints:
 
-The number of nodes in the given tree will be in the range [1, 100].
-Each node's value will be an integer in the range [0, 99].
+The number of nodes in the tree is in the range [1, 100].
+0 <= Node.val < 100
 */
 
 /**
@@ -33,12 +32,24 @@ Each node's value will be an integer in the range [0, 99].
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isUnivalTree(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        if (root->left && root->left->val != root->val) {
+            return false;
+        }
         
+        if (root->right && root->right->val != root->val) {
+            return false;
+        }
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
     }
 };
