fd495b677c1828a3a1799058e50a32ea991bac3d
J.D <jordandong@gmail.com>
2021-08-28 01:54:06 -0700

    Update LC_00200_NumberOfIslands.cpp

--- LC_00200_NumberOfIslands.cpp
+++ LC_00200_NumberOfIslands.cpp
@@ -1,27 +1,75 @@
 /*
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
 
 Example 1:
-11110
-11010
-11000
-00000
-Answer: 1
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
 
 Example 2:
-11000
-11000
-00100
-00011
-Answer: 3
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
 
-Credits:
-Special thanks to @mithmatt for adding this problem and creating all test cases.
+Constraints:
 
-Hide Tags Depth-first Search Breadth-first Search
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
 */
 
 //recursion
+class Solution {
+public:
+    int numIslands(vector<vector<char> > &grid) {
+        int res = 0;
+        int M = grid.size();
+        if(M == 0)
+            return res;
+        int N = grid[0].size();
+        if(N == 0)
+            return res;
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(grid[i][j] == '1'){
+                    res++;
+                    numIslandsHelper(i, j, grid);
+                }
+            }
+        }
+        return res;
+    }
+  
+    void numIslandsHelper(int i, int j, vector<vector<char> > &grid) {
+        if (grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = 'x';
+        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, -0}};
+        for (auto d : dir) {
+            int x = i + d[0];
+            int y = j + d[1];
+            if (0 <= x && x < grid.size() &&
+                0 <= y && y < grid[0].size() &&
+                grid[x][y] == '1') {
+                numIslandsHelper(x, y, grid);
+            }
+        }
+    }
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char> > &grid) {
@@ -55,6 +103,50 @@ public:
 };
 
 //non recursion
+class Solution {
+public:
+    int numIslands(vector<vector<char> > &grid) {
+        int res = 0;
+        int M = grid.size();
+        if(M == 0)
+            return res;
+        int N = grid[0].size();
+        if(N == 0)
+            return res;
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(grid[i][j] == '1'){
+                    res++;
+                    numIslandsHelper(i, j, grid);
+                }
+            }
+        }
+        return res;
+    }
+  
+    void numIslandsHelper(int i, int j, vector<vector<char> > &grid) {
+        queue<pair<int, int> > q;
+        q.push(make_pair(i, j));
+        grid[i][j] = 'x';
+        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, -0}};
+        while (q.size()) {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for (auto d : dir) {
+                int x = i + d[0];
+                int y = j + d[1];
+                if (0 <= x && x < grid.size() &&
+                    0 <= y && y < grid[0].size() &&
+                    grid[x][y] == '1') {
+                    grid[x][y] = 'x';
+                    q.push(make_pair(x, y));
+                }
+            }
+        }
+    }
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char> > &grid) {
